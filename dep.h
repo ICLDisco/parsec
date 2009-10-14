65ce6977b8e46fb08ee9d1deb4e6e626c288592e
herault@thomas-heraults-macbook-pro-5.local <herault@thomas-heraults-macbook-pro-5.local>
2009-10-14 16:03:16 -0400

    As suggested by Anthony, remove ternary conditional, to use guarded rules approach

--- dep.h
+++ dep.h
@@ -7,15 +7,10 @@
 #define MAX_CALL_PARAM_COUNT    MAX_PARAM_COUNT
 
 typedef struct {
+    expr_t    *cond;
     dplasma_t *t;
     expr_t    *call_params[MAX_CALL_PARAM_COUNT];
     char      *sym_name;
-} rec_dep_t;
-
-typedef struct {
-    expr_t *cond;
-    rec_dep_t iftrue;
-    rec_dep_t iffalse;
 } dep_t;
 
 #endif
