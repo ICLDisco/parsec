61a8f46599d7c1a5fa31deedf0479f5251f200e3
George Bosilca <bosilca@eecs.utk.edu>
2009-10-29 17:47:19 -0400

    Extract the code to compute the initial values in its own function.

--- dplasma.c
+++ dplasma.c
@@ -171,14 +171,11 @@ int dplasma_dependency_activate( dplasma_execution_context_t* context, int arg_i
 #define DEBUG(ARG)
 #endif
 
-int dplasma_unroll( const dplasma_t* object )
+int dplasma_set_initial_execution_context( dplasma_execution_context_t* exec_context )
 {
-    dplasma_execution_context_t* exec_context = (dplasma_execution_context_t*)malloc(sizeof(dplasma_execution_context_t));
-    int i, nb_locals, rc, actual_loop;
-    dplasma_loop_values_t* last_loop = NULL;
+    int i, nb_locals, rc;
+    const dplasma_t* object = exec_context->function;
     const expr_t** predicates;
-
-    exec_context->function = object;
 
     /* Compute the number of local values */
     for( i = nb_locals = 0; (NULL != object->locals[i]) && (i < MAX_LOCAL_COUNT); i++, nb_locals++ );
@@ -186,40 +183,6 @@ int dplasma_unroll( const dplasma_t* object )
         /* special case for the IN/OUT obejcts */
         return 0;
     }
-    printf( "Function %s (loops %d)\n", object->name, nb_locals );
-
-#if 0
-    /**
-     * This section of code walk through the tree and printout the local and global
-     * minimum and maximum values for all local variables.
-     */
-    for( i = 0; i < nb_locals; i++ ) {
-        dplasma_loop_values_t* loop;
-        int abs_min, min, abs_max, max;
-        exec_context->locals[i].sym = object->locals[i];
-        dplasma_symbol_get_first_value(object->locals[i], (const expr_t**)object->preds,
-                                       exec_context->locals, &min);
-        exec_context->locals[i].value = min;
-        dplasma_symbol_get_last_value(object->locals[i], (const expr_t**)object->preds,
-                                      exec_context->locals, &max);
-
-        dplasma_symbol_get_absolute_minimum_value(object->locals[i], &abs_min);
-        dplasma_symbol_get_absolute_maximum_value(object->locals[i], &abs_max);
-
-        printf( "Range for local symbol %s is [%d..%d] (global range [%d..%d]) %s\n",
-                object->locals[i]->name, min, max, abs_min, abs_max,
-                (0 == dplasma_symbol_is_standalone(object->locals[i]) ? "[standalone]" : "[dependent]") );
-
-        loop = dplasma_dependency_create_loop( DPLASMA_LOOP_FINAL, exec_context->locals[i].sym, min, max );
-        if( NULL != last_loop ) {
-            last_loop->type = DPLASMA_LOOP_NEXT;
-            last_loop->generic_next = loop;
-        } else {
-            exec_context->loops = loop;
-        }
-        last_loop = loop;
-    }
-#endif
 
     /**
      * This section of the code generate all possible executions for a specific
@@ -242,7 +205,7 @@ int dplasma_unroll( const dplasma_t* object )
             i--;
             if( i < 0 ) {
                 printf( "Impossible to find initial values. Giving up\n" );
-                break;
+                return -1;
             }
             rc = dplasma_symbol_get_next_value(object->locals[i], predicates,
                                                exec_context->locals, &min );
@@ -250,6 +213,191 @@ int dplasma_unroll( const dplasma_t* object )
                 goto initial_values_one_loop_up;
             }
         }
+    }
+    return 0;
+}
+
+int plasma_show_ranges( const dplasma_t* object )
+{
+    dplasma_execution_context_t* exec_context = (dplasma_execution_context_t*)malloc(sizeof(dplasma_execution_context_t));
+    dplasma_loop_values_t* last_loop = NULL;
+    const expr_t** predicates;
+    int i, nb_locals;
+
+    exec_context->function = object;
+
+    /* Compute the number of local values */
+    for( i = nb_locals = 0; (NULL != object->locals[i]) && (i < MAX_LOCAL_COUNT); i++, nb_locals++ );
+    if( 0 == nb_locals ) {
+        /* special case for the IN/OUT obejcts */
+        return 0;
+    }
+    printf( "Function %s (loops %d)\n", object->name, nb_locals );
+
+    /**
+     * If the global execution is true (!= 0) then it generate
+     * the values globally, otherwise it will generate them locally (using
+     * the predicates).
+     */
+    predicates = NULL;
+    if( !global_execution ) {
+        predicates = (const expr_t**)object->preds;
+    }
+
+    /**
+     * This section of code walk through the tree and printout the local and global
+     * minimum and maximum values for all local variables.
+     */
+    for( i = 0; i < nb_locals; i++ ) {
+        dplasma_loop_values_t* loop;
+        int abs_min, min, abs_max, max;
+        exec_context->locals[i].sym = object->locals[i];
+        dplasma_symbol_get_first_value(object->locals[i], predicates,
+                                       exec_context->locals, &min);
+        exec_context->locals[i].value = min;
+        dplasma_symbol_get_last_value(object->locals[i], predicates,
+                                      exec_context->locals, &max);
+
+        dplasma_symbol_get_absolute_minimum_value(object->locals[i], &abs_min);
+        dplasma_symbol_get_absolute_maximum_value(object->locals[i], &abs_max);
+
+        printf( "Range for local symbol %s is [%d..%d] (global range [%d..%d]) %s\n",
+                object->locals[i]->name, min, max, abs_min, abs_max,
+                (0 == dplasma_symbol_is_standalone(object->locals[i]) ? "[standalone]" : "[dependent]") );
+
+        loop = dplasma_dependency_create_loop( DPLASMA_LOOP_FINAL, exec_context->locals[i].sym, min, max );
+        if( NULL != last_loop ) {
+            last_loop->type = DPLASMA_LOOP_NEXT;
+            last_loop->generic_next = (dplasma_loop_union_t*)loop;
+        } else {
+            exec_context->loops = loop;
+        }
+        last_loop = loop;
+    }
+    return 0;
+}
+
+int dplasma_show_tasks( const dplasma_t* object )
+{
+    dplasma_execution_context_t* exec_context = (dplasma_execution_context_t*)malloc(sizeof(dplasma_execution_context_t));
+    int i, nb_locals, rc, actual_loop;
+    const expr_t** predicates;
+
+    exec_context->function = object;
+
+    /* Compute the number of local values */
+    for( i = nb_locals = 0; (NULL != object->locals[i]) && (i < MAX_LOCAL_COUNT); i++, nb_locals++ );
+    if( 0 == nb_locals ) {
+        /* special case for the IN/OUT obejcts */
+        return 0;
+    }
+    printf( "Function %s (loops %d)\n", object->name, nb_locals );
+
+    /**
+     * This section of the code generate all possible executions for a specific
+     * dplasma object. If the global execution is true (!= 0) then it generate
+     * the instances globally, otherwise it will generate them locally (using
+     * the predicates).
+     */
+    predicates = NULL;
+    if( !global_execution ) {
+        predicates = (const expr_t**)object->preds;
+    }
+
+    if( 0 != dplasma_set_initial_execution_context(exec_context) ) {
+        /* if we can't initialize the execution context then there is no reason to
+         * continue.
+         */
+        return -1;
+    }
+
+    actual_loop = nb_locals - 1;
+    while(1) {
+        int value;
+
+        /* Do whatever we have to do for this context */
+        printf( "Execute %s with ", object->name );
+        for( i = 0; i <= actual_loop; i++ ) {
+            printf( "(%s = %d)", object->locals[i]->name,
+                    exec_context->locals[i].value );
+        }
+        printf( "\n" );
+
+        /* Go to the next valid value for this loop context */
+        rc = dplasma_symbol_get_next_value( object->locals[actual_loop], predicates,
+                                            exec_context->locals, &value );
+
+        /* If no more valid values, go to the previous loop,
+         * compute the next valid value and redo and reinitialize all other loops.
+         */
+        if( rc != EXPR_SUCCESS ) {
+            int current_loop = actual_loop;
+        one_loop_up:
+            DEBUG(("Loop index %d based on %s failed to get next value. Going up ...\n",
+                   actual_loop, object->locals[actual_loop]->name));
+            if( 0 == actual_loop ) {  /* we're done */
+                goto end_of_all_loops;
+            }
+            actual_loop--;  /* one level up */
+            rc = dplasma_symbol_get_next_value( object->locals[actual_loop], predicates,
+                                                exec_context->locals, &value );
+            if( rc != EXPR_SUCCESS ) {
+                goto one_loop_up;
+            }
+            DEBUG(("Keep going on the loop level %d (symbol %s value %d)\n", actual_loop,
+                   object->locals[actual_loop]->name, exec_context->locals[actual_loop].value));
+            for( actual_loop++; actual_loop <= current_loop; actual_loop++ ) {
+                rc = dplasma_symbol_get_first_value(object->locals[actual_loop], predicates,
+                                                    exec_context->locals, &value );
+                if( rc != EXPR_SUCCESS ) {  /* no values for this symbol in this context */
+                    goto one_loop_up;
+                }
+                DEBUG(("Loop index %d based on %s get first value %d\n", actual_loop,
+                       object->locals[actual_loop]->name, exec_context->locals[actual_loop].value));
+            }
+            actual_loop = current_loop;  /* go back to the original loop */
+        } else {
+            DEBUG(("Loop index %d based on %s get next value %d\n", actual_loop,
+                   object->locals[actual_loop]->name, exec_context->locals[actual_loop].value));
+        }
+    }
+ end_of_all_loops:
+
+    return 0;
+}
+
+int dplasma_unroll( const dplasma_t* object )
+{
+    dplasma_execution_context_t* exec_context = (dplasma_execution_context_t*)malloc(sizeof(dplasma_execution_context_t));
+    int i, nb_locals, rc, actual_loop;
+    const expr_t** predicates;
+
+    exec_context->function = object;
+
+    /* Compute the number of local values */
+    for( i = nb_locals = 0; (NULL != object->locals[i]) && (i < MAX_LOCAL_COUNT); i++, nb_locals++ );
+    if( 0 == nb_locals ) {
+        /* special case for the IN/OUT obejcts */
+        return 0;
+    }
+    printf( "Function %s (loops %d)\n", object->name, nb_locals );
+
+    /**
+     * This section of the code generate all possible executions for a specific
+     * dplasma object. If the global execution is true (!= 0) then it generate
+     * the instances globally, otherwise it will generate them locally (using
+     * the predicates).
+     */
+    predicates = NULL;
+    if( !global_execution ) {
+        predicates = (const expr_t**)object->preds;
+    }
+
+    if( 0 != dplasma_set_initial_execution_context(exec_context) ) {
+        /* if we can't initialize the execution context then there is no reason to
+         * continue.
+         */
+        return -1;
     }
 
     actual_loop = nb_locals - 1;
