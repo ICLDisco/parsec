4948cdbce6371f94009e01c8ac9ac84ab4f87e87
George Bosilca <bosilca@eecs.utk.edu>
2009-11-04 00:27:08 -0500

    Add the parser code.

---
+++ parser.c
@@ -0,0 +1,45 @@
+#include <dplasma.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern int yyparse();
+extern int dplasma_lineno;
+
+static int generic_hook(const dplasma_execution_context_t* exec_context)
+{
+    char tmp[128];
+
+    printf("Execute %s\n", dplasma_service_to_string(exec_context, tmp, 128));
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    dplasma_lineno = 1;
+	yyparse();
+
+    /*symbol_dump_all("");*/
+    /*dplasma_dump_all();*/
+    {
+        /* Setup generic hook for all services */
+        dplasma_t* object;
+        int i;
+        for( i = 0; NULL != (object = (dplasma_t*)dplasma_element_at(i)); i++ ) {
+            object->hook = generic_hook;
+        }
+    }
+
+    {
+        dplasma_execution_context_t exec_context;
+        /* I know what I'm doing ;) */
+        exec_context.function = (dplasma_t*)dplasma_find("POTRF");
+        if( NULL == exec_context.function ) {
+            printf("Unable to find the expected function. Giving up.\n");
+            exit(-1);
+        }
+        dplasma_set_initial_execution_context(&exec_context);
+        dplasma_execute(&exec_context);
+    }
+
+	return 0;
+}
