66f69fd7bfa175ce693a31c5aa2310e34cf57025
George Bosilca <bosilca@eecs.utk.edu>
2009-12-18 19:11:41 -0500

    Add atomic operations for MAC OS X, X86 and X86_64.

---
+++ atomic-x86_32.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2009      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+static inline int dplasma_atomic_bor_32b( volatile uint32_t* location,
+                                          uint32_t value )
+{
+    return OSAtomicOr32( value, location );
+}
+
+static inline int dplasma_atomic_cas_32b( volatile uint32_t* location,
+                                          uint32_t old_value,
+                                          uint32_t new_value )
+{
+    unsigned char ret;
+    __asm__ __volatile__ (
+                          "lock; cmpxchgl %3,%4   \n\t"
+                          "sete     %0      \n\t"
+                          : "=qm" (ret), "+a" (oldval), "+m" (*addr)
+                          : "q"(newval)
+                          : "memory", "cc");
+
+    return (int)ret;
+}
+
+static inline int dplasma_atomic_cas_64b( volatile uint64_t* location,
+                                          uint64_t old_value,
+                                          uint64_t new_value )
+{
+   /*
+    * Compare EDX:EAX with m64. If equal, set ZF and load ECX:EBX into
+    * m64. Else, clear ZF and load m64 into EDX:EAX.
+    */
+    unsigned char ret;
+
+    __asm__ __volatile__(
+                    "push %%ebx            \n\t"
+                    "movl %4, %%ebx        \n\t"
+                    SMPLOCK "cmpxchg8b (%1)  \n\t"
+                    "sete %0               \n\t"
+                    "pop %%ebx             \n\t"
+                    : "=qm"(ret)
+                    : "D"(addr), "a"(ll_low(oldval)), "d"(ll_high(oldval)),
+                      "r"(ll_low(newval)), "c"(ll_high(newval))
+                    : "cc", "memory", "ebx");
+    return (int) ret;
+}
