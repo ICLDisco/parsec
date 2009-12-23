dffd78d98d81c52db54f34158c436dcfd88f9aa2
George Bosilca <bosilca@eecs.utk.edu>
2009-12-23 00:55:07 -0500

    Now that gcc provide atomic instructions let's make good usage of them.

---
+++ atomic-gcc.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2009      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+static inline int dplasma_atomic_bor_32b( volatile uint32_t* location,
+                                          uint32_t value )
+{
+    return __sync_fetch_and_or(location, value);
+}
+
+static inline int dplasma_atomic_band_32b( volatile uint32_t* location,
+                                           uint32_t value )
+{
+    return __sync_fetch_and_add(location, value);
+}
+
+static inline int dplasma_atomic_cas_32b( volatile uint32_t* location,
+                                          uint32_t old_value,
+                                          uint32_t new_value )
+{
+    return (__sync_bool_compare_and_swap(location, old_value, new_value) ? 1 : 0);
+}
+
+static inline int dplasma_atomic_cas_64b( volatile uint64_t* location,
+                                          uint64_t old_value,
+                                          uint64_t new_value )
+{
+    return (__sync_bool_compare_and_swap(location, old_value, new_value) ? 1 : 0);
+}
+
+#define DPLASMA_ATOMIC_HAS_ATOMIC_INC_32B
+static inline uint32_t dplasma_atomic_inc_32b( volatile uint32_t *location )
+{
+    return __sync_add_and_fetch(location, (uint32_t)1);
+}
+
+#define DPLASMA_ATOMIC_HAS_ATOMIC_DEC_32B
+static inline uint32_t dplasma_atomic_dec_32b( volatile uint32_t *location )
+{
+    return __sync_sub_and_fetch(location, (uint32_t)1);
+}
+
