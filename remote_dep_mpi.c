1d33e31a937528982f81463ea40311d19b6e8338
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2010-03-15 17:24:04 -0400

    Newer versions of open MPI are better, and more asynchronous. As a proof, misassumptions about fifo ordering of testany dont yield anymore. This is a dirty fix for this condition. Better one coming next.

--- remote_dep_mpi.c
+++ remote_dep_mpi.c
@@ -273,7 +273,7 @@ static int remote_dep_mpi_progress(dplasma_execution_unit_t* eu_context)
             }
             else 
             {
-                assert(REMOTE_DEP_PUT_DATA_TAG == status.MPI_TAG);
+                //assert(REMOTE_DEP_PUT_DATA_TAG == status.MPI_TAG);
                 i -= DEP_NB_CONCURENT * 2;
                 assert(i >= 0);
                 if(i < DEP_NB_CONCURENT)
@@ -302,13 +302,14 @@ static int remote_dep_mpi_progress(dplasma_execution_unit_t* eu_context)
     return ret;
 }
 
+#define PTR_TO_TAG(ptr) (((int) ptr) > 0 ? (int) ptr : - (int) ptr)
 
 static void remote_dep_mpi_put_data(void* data, int to, int i)
 {
     assert(dep_enabled);
     DEBUG(("TO\t%d\tPut data\tunknown \tj=%d\twith data at %p\n", to, i, data));
     TAKE_TIME(MPIsnd_prof[i], MPI_Data_plds_sk, i);
-    MPI_Isend(data, TILE_SIZE, MPI_DOUBLE, to, REMOTE_DEP_PUT_DATA_TAG, dep_comm, &dep_put_snd_req[i]);
+    MPI_Isend(data, TILE_SIZE, MPI_DOUBLE, to, PTR_TO_TAG(data), dep_comm, &dep_put_snd_req[i]);
 }
 
 static int get = 1;
@@ -325,7 +326,7 @@ static void remote_dep_mpi_get_data(dplasma_execution_context_t* task, int from,
     DEBUG(("TO\t%d\tGet data\t%s\ti=%d\twith data at %p\n", from, dplasma_service_to_string(task, tmp, 128), i, datakey));
     TAKE_TIME(MPIrcv_prof[i], MPI_Data_pldr_sk, i);
     MPI_Irecv(task->list_item.cache_friendly_emptiness, TILE_SIZE, 
-              MPI_DOUBLE, from, REMOTE_DEP_PUT_DATA_TAG, dep_comm, &dep_put_rcv_req[i]);
+              MPI_DOUBLE, from, PTR_TO_TAG(datakey), dep_comm, &dep_put_rcv_req[i]);
 
     TAKE_TIME(MPIctl_prof, MPI_Data_ctl_sk, get);
     MPI_Send(&datakey, 1, datakey_dtt, from, REMOTE_DEP_GET_DATA_TAG, dep_comm);
