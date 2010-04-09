990a2de4a27b43d0a92fab083ce43f356b48997b
George Bosilca <bosilca@eecs.utk.edu>
2010-04-09 19:20:22 -0400

    If we detect XLC then switch to their atomic built-ins.

--- atomic-ppc.h
+++ atomic-ppc.h
@@ -9,45 +9,52 @@
 #endif  /* __ PPC */
 
 static inline int dplasma_atomic_bor_32b( volatile uint32_t* location,
-                                          uint32_t value )
+                                          uint32_t mask )
 {
-   int32_t t;
+#if !defined(__IBMC__)
+   int32_t old, t;
 
    __asm__ __volatile__(
-                        "1:   lwarx   %0, 0, %3    \n\t"
-                        "     add.    %0, %2, %0   \n\t"
-                        "     stwcx.  %0, 0, %3    \n\t"
+                        "1:   lwarx   %0,  0, %3   \n\t"
+                        "     or      %1, %0, %2   \n\t"
+                        "     stwcx.  %1,  0, %3   \n\t"
                         "     bne-    1b           \n\t"
-                        "     mr      %3, %0       \n\t"
-                        : "=&r" (t), "=m" (*location)
-                        : "r" (value), "r" (location), "m" (*location)
-                        : "cr0");
+                        : "=&r" (old), "=&r" (t)
+                        : "r" (mask), "r" (location)
+                        : "cc", "memory");
 
    return t;
+#else
+   return mask | __fetch_and_or(location, mask);
+#endif  /* !defined(__IBMC__) */
 }
 
 static inline int dplasma_atomic_band_32b( volatile uint32_t* location,
-                                          uint32_t value )
+                                          uint32_t mask )
 {
-   int32_t t;
+#if !defined(__IBMC__)
+   int32_t old, t;
 
    __asm__ __volatile__(
-                        "1:   lwarx   %0, 0, %3    \n\t"
-                        "     or.     %0, %2, %0   \n\t"
-                        "     stwcx.  %0, 0, %3    \n\t"
+                        "1:   lwarx   %0,  0, %3   \n\t"
+                        "     andc    %1, %0, %2   \n\t"
+                        "     stwcx.  %1,  0, %3   \n\t"
                         "     bne-    1b           \n\t"
-                        "     mr      %3, %0       \n\t"
-                        : "=&r" (t), "=m" (*location)
-                        : "r" (value), "r" (location), "m" (*location)
-                        : "cr0");
+                        : "=&r" (old), "=&r" (t)
+                        : "r" (mask), "r" (location)
+                        : "cc", "memory");
 
    return t;
+#else
+   return mask & __fetch_and_and(location, mask);
+#endif  /* !defined(__IBMC__) */
 }
 
 static inline int dplasma_atomic_cas_32b( volatile uint32_t* location,
                                           uint32_t old_value,
                                           uint32_t new_value )
 {
+#if !defined(__IBMC__)
    int32_t ret;
 
    __asm__ __volatile__ (
@@ -62,12 +69,16 @@ static inline int dplasma_atomic_cas_32b( volatile uint32_t* location,
                          : "cr0", "memory");
 
    return (ret == old_value);
+#else
+   return __compare_and_swap((volatile int*)location, (int*)&old_value, (int)new_value);
+#endif  /* !defined(__IBMC__) */
 }
 
 static inline int dplasma_atomic_cas_64b( volatile uint64_t* location,
                                           uint64_t old_value,
                                           uint64_t new_value )
 {
+#if !defined(__IBMC__)
    int64_t ret;
 
    __asm__ __volatile__ (
@@ -82,41 +93,50 @@ static inline int dplasma_atomic_cas_64b( volatile uint64_t* location,
                          : "cr0", "memory");
 
    return (ret == old_value);
+#else
+   return __compare_and_swaplp((volatile long*)location, (long*)&old_value, (long)new_value);
+#endif  /* !defined(__IBMC__) */
 }
 
 #define DPLASMA_ATOMIC_HAS_ATOMIC_INC_32B
 static inline uint32_t dplasma_atomic_inc_32b( volatile uint32_t *location )
 {
-   int32_t t, inc = 1;
+#if !defined(__IBMC__)
+   int32_t t;
 
    __asm__ __volatile__(
-                        "1:   lwarx   %0, 0, %3    \n\t"
-                        "     add     %0, %2, %0   \n\t"
-                        "     stwcx.  %0, 0, %3    \n\t"
+                        "1:   lwarx   %0, 0, %1    \n\t"
+                        "     addic   %0, %0, 1    \n\t"
+                        "     stwcx.  %0, 0, %1    \n\t"
                         "     bne-    1b           \n\t"
-                        "     mr      %3, %0       \n\t"
-                        : "=&r" (t), "=m" (*location)
-                        : "r" (inc), "r" (location), "m" (*location)
-                        : "cr0");
+                        : "=&r" (t)
+                        : "r" (location)
+                        : "cc", "memory");
 
    return t;
+#else
+   return 1 + __fetch_and_add( (volatile int*)location, 1);
+#endif  /* !defined(__IBMC__) */
 }
 
 #define DPLASMA_ATOMIC_HAS_ATOMIC_DEC_32B
 static inline uint32_t dplasma_atomic_dec_32b( volatile uint32_t *location )
 {
-   int32_t t, dec = 1;
+#if !defined(__IBMC__)
+   int32_t t;
 
    __asm__ __volatile__(
-                        "1:   lwarx   %0,0,%3      \n\t"
-                        "     subf    %0,%2,%0     \n\t"
-                        "     stwcx.  %0,0,%3      \n\t"
+                        "1:   lwarx   %0, 0,%1     \n\t"
+                        "     addic   %0,%0,-1     \n\t"
+                        "     stwcx.  %0,0,%1      \n\t"
                         "     bne-    1b           \n\t"
-                        "     mr      %3, %0       \n\t"
-                        : "=&r" (t), "=m" (*location)
-                        : "r" (dec), "r" (location), "m" (*location)
-                        : "cr0");
+                        : "=&r" (t)
+                        : "r" (location)
+                        : "cc", "memory");
 
    return t;
-}
+#else
+   return __fetch_and_add( (volatile int*)location, -1) - 1;
+#endif  /* !defined(__IBMC__) */
+}}
 
