15f07e16f98aa46cea7a1a6b299a8999686a02ae
George Bosilca <bosilca@eecs.utk.edu>
2010-04-27 15:54:44 -0400

    Update the perf script.

--- tests/pdpotrf/exe_chol.py
+++ tests/pdpotrf/exe_chol.py
@@ -1,33 +1,44 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sys
 import commands
 
-EXE = ["./dposv_ll"]
+EXE = ["./dposv_rl"]
+repeat = 10
 
 def get_nb(exe):
   n = 500
   cores = 1
-  cmd = "env GOTO_NUM_THREADS=1 OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 %s %d %d %d 1 %d" % (exe, cores, n, n, n)
+  cmd = "env GOTO_NUM_THREADS=1 OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 %s -c %d %d" % (exe, cores, n)
+  print "####", cmd
   st, out = commands.getstatusoutput(cmd)
-  perf_line = find_perf_line(out)
-  nb = int(perf_line.split()[4])
+  perf_line = find_block_size(out)
+  nb = int(perf_line.split()[8])
   return nb
 
+def find_block_size(txt):
+  s = "Dplasma initialization:"
+  for l in txt.split("\n"):
+    if l.find(s) >= 0:
+      return l
+  raise ValueError, "Cannot find '%s' in this:\n%s" % (s, txt)
+
 def find_perf_line(txt):
-  s = "PLASMA DPOTRF"
+  s = "Dplasma computation:"
   for l in txt.split("\n"):
     if l.find(s) >= 0:
       return l
   raise ValueError, "Cannot find '%s' in this:\n%s" % (s, txt)
 
 def run(n, exe):
-  for cores in [1, 2, 4, 8]:
-    cmd = "env GOTO_NUM_THREADS=1 OMP_NUM_THREADS=1 MKL_NUM_THREADS=1  %s %d %d %d 1 %d" % (exe, cores, n, n, n)
+  for cores in [1, 2, 4]:
+    cmd = "env GOTO_NUM_THREADS=1 OMP_NUM_THREADS=1 MKL_NUM_THREADS=1  %s -c %d %d" % (exe, cores, n)
     print "####", cmd
-    st, out = commands.getstatusoutput(cmd)
-    print find_perf_line(out)
-    sys.stdout.flush()
+    for count in range(0, repeat, 1):
+      st, out = commands.getstatusoutput(cmd)
+      print cores, "x", n, " ", find_perf_line(out)
+      sys.stdout.flush()
 
 def test_exe(fname):
   try:
