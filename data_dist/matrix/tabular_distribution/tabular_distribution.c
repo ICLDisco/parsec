4b10a2db38046ffe37418cba55c79141786877c1
Pierre Lemarinier <none@none>
2010-08-18 14:55:41 -0400

    new distribution for testing different tiles/process association
    --HG--
    branch : DAGuE

---
+++ data_dist/matrix/tabular_distribution/tabular_distribution.c
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2009      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+#ifdef USE_MPI
+#include <mpi.h>
+#endif /* USE_MPI */
+
+
+#include "tabular_distribution.h"
+#include "data_distribution.h"
+#include "matrix.h"
+
+
+static uint32_t td_get_rank_for_tile(dague_ddesc_t * desc, ...)
+{
+    int m, n, res;
+    va_list ap;
+    tabular_distribution_t * Ddesc;
+    Ddesc = (tabular_distribution_t *)desc;
+    va_start(ap, desc);
+    m = va_arg(ap, int);
+    n = va_arg(ap, int);
+    va_end(ap);
+
+    res = (Ddesc->super.lmt * n) + m;
+    return Ddesc->tiles_table[res].rank;
+}
+
+
+
+static void * td_get_local_tile(dague_ddesc_t * desc, ...)
+{
+    int res, m, n;
+    tabular_distribution_t * Ddesc;
+    va_list ap;
+    Ddesc = (tabular_distribution_t *)desc;
+    va_start(ap, desc);
+    m = va_arg(ap, int);
+    n = va_arg(ap, int);
+    va_end(ap);
+#ifdef DISTRIBUTED
+    assert(desc->myrank == td_get_rank_for_tile(desc, m, n));    
+#endif /* DISTRIBUTED */
+
+    res = (Ddesc->super.lmt * n) + m;
+    
+    return  Ddesc->tiles_table[res].tile;
+}
+
+
+void tabular_distribution_init(tabular_distribution_t * Ddesc, enum matrix_type mtype, int nodes, int cores, int myrank, int mb, int nb, int ib, int lm, int ln, int i, int j, int m, int n, uint32_t * table )
+{
+    int res;
+    
+
+    // Filling matrix description woth user parameter
+    Ddesc->super.super.nodes = nodes ;
+    Ddesc->super.super.cores = cores ;
+    Ddesc->super.super.myrank = myrank ;
+    Ddesc->super.mtype = mtype;
+    Ddesc->super.mb = mb;
+    Ddesc->super.nb = nb;
+    Ddesc->super.ib = ib;
+    Ddesc->super.lm = lm;
+    Ddesc->super.ln = ln;
+    Ddesc->super.i = i;
+    Ddesc->super.j = j;
+    Ddesc->super.m = m;
+    Ddesc->super.n = n;
+
+    // Matrix derived parameters
+    Ddesc->super.lmt = ((Ddesc->super.lm)%(Ddesc->super.mb)==0) ? ((Ddesc->super.lm)/(Ddesc->super.mb)) : ((Ddesc->super.lm)/(Ddesc->super.mb) + 1);
+    Ddesc->super.lnt = ((Ddesc->super.ln)%(Ddesc->super.nb)==0) ? ((Ddesc->super.ln)/(Ddesc->super.nb)) : ((Ddesc->super.ln)/(Ddesc->super.nb) + 1);
+    Ddesc->super.bsiz =  Ddesc->super.mb * Ddesc->super.nb;
+
+    // Submatrix parameters    
+    Ddesc->super.mt = ((Ddesc->super.m)%(Ddesc->super.mb)==0) ? ((Ddesc->super.m)/(Ddesc->super.nb)) : ((Ddesc->super.m)/(Ddesc->super.nb) + 1);
+    Ddesc->super.nt = ((Ddesc->super.n)%(Ddesc->super.nb)==0) ? ((Ddesc->super.n)/(Ddesc->super.nb)) : ((Ddesc->super.n)/(Ddesc->super.nb) + 1);
+    
+
+    /* allocate the table*/
+    Ddesc->tiles_table = malloc((Ddesc->super.lmt) * (Ddesc->super.lnt) * sizeof(tile_elem_t));
+
+    /* affecting corresponding ranks and allocating tile memory */
+    
+    for (res = 0 ; res < (Ddesc->super.lmt) * (Ddesc->super.lnt) ; res++)
+        {
+            Ddesc->tiles_table[res].rank = table[res];
+            if(table[res] == myrank) /* this tile belongs to me, allocating memory*/
+                {
+                    Ddesc->tiles_table[res].tile = dague_allocate_matrix( Ddesc->super.bsiz * Ddesc->super.mtype);
+                    if (Ddesc->tiles_table[res].tile == NULL)
+                        {
+                            perror("matrix memory allocation failed\n");
+                            exit(-1);
+                        }
+                    
+                }
+            else
+                Ddesc->tiles_table[res].tile = NULL;
+        }
+
+    /*
+
+    Ddesc->mat = dague_allocate_matrix(Ddesc->nb_elem_r * Ddesc->nb_elem_c * Ddesc->super.bsiz * Ddesc->super.mtype);
+    if (Ddesc->mat == NULL)
+        {
+            perror("matrix memory allocation failed\n");
+            exit(-1);
+        }
+
+    */
+    Ddesc->super.super.rank_of =  td_get_rank_for_tile;
+    Ddesc->super.super.data_of =  td_get_local_tile;
+    
+}
+
+
---
+++ data_dist/matrix/tabular_distribution/tabular_distribution.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2009      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+#ifndef __TABULAR_DISTRIBUTION_H__
+#define __TABULAR_DISTRIBUTION_H__
+
+#ifdef USE_MPI
+#include <mpi.h>
+#endif /* USE_MPI */
+
+#include "../../data_distribution.h"
+#include "../matrix.h"
+
+/*
+ * General distribution of data. Suppose exists a matrix in process of mpi rank 0
+ */
+
+
+/*******************************************************************
+ * distributed data structure and basic functionalities
+ *******************************************************************/
+
+typedef struct tile_elem {
+    uint32_t rank;
+    void * tile;
+} tile_elem_t;
+
+/* structure equivalent to PLASMA_desc, but for distributed matrix data
+ */
+typedef struct tabular_distribution {
+    tiled_matrix_desc_t super;
+    tile_elem_t * tiles_table;
+} tabular_distribution_t;
+
+
+
+/************************************************
+ *   mpi ranks distribution in the process grid
+ *         ------------------------------------------
+ * tile    | 0,0 | 1,0 | 2,0 | 0,1 | 1,1 | 2,1 | ... |
+ *         |-----------------------------------------|
+ * process |  4  |  2  |  0  |  4  |  3  |  0  | ... | <-- input table for init
+ *         ------------------------------------------
+ ************************************************/
+
+
+
+/**
+ * Initialize the description of a  2-D block cyclic distributed matrix.
+ * @param Ddesc matrix description structure, already allocated, that will be initialize
+ * @param nodes number of nodes
+ * @param cores number of cores per node
+ * @param myrank rank of the local node (as of mpi rank)
+ * @param mb number of row in a tile
+ * @param nb number of column in a tile
+ * @param ib number of column in an inner block
+ * @param lm number of rows of the entire matrix
+ * @param ln number of column of the entire matrix
+ * @param i starting row index for the computation on a submatrix
+ * @param j starting column index for the computation on a submatrix
+ * @param m number of rows of the entire submatrix
+ * @param n numbr of column of the entire submatrix
+ * @param table mpi rank for each tile, column major ordering
+ */
+
+void tabular_distribution_init(tabular_distribution_t * Ddesc, enum matrix_type mtype, int nodes, int cores, int myrank, int mb, int nb, int ib, int lm, int ln, int i, int j, int m, int n, uint32_t * table );
+
+#endif /* __TABULAR_DISTRIBUTION_H__ */
