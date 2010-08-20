2223dd458feb517b75fe5cac3d225c4d9b0253ee
Mathieu Faverge <mfaverge@eecs.utk.edu>
2010-08-20 19:20:25 -0400

    Now everything compile, is it working is another question
    --HG--
    branch : DAGuE

--- tests/dtrsm/dtrsm.c
+++ tests/dtrsm/dtrsm.c
@@ -5,13 +5,13 @@
 #include "dtrsm.h"
 
 #include "trsm_LLN.h"
-/* #include "trsm_LLT.h" */
-/* #include "trsm_LUN.h" */
-/* #include "trsm_LUT.h" */
-/* #include "trsm_RLN.h" */
-/* #include "trsm_RLT.h" */
-/* #include "trsm_RUN.h" */
-/* #include "trsm_RUT.h" */
+#include "trsm_LLT.h"
+#include "trsm_LUN.h"
+#include "trsm_LUT.h"
+#include "trsm_RLN.h"
+#include "trsm_RLT.h"
+#include "trsm_RUN.h"
+#include "trsm_RUT.h"
 
 dague_object_t * 
 DAGUE_dtrsm_getObject(PLASMA_enum side, PLASMA_enum uplo, PLASMA_enum trans, PLASMA_enum diag,
@@ -57,43 +57,43 @@ DAGUE_dtrsm_getObject(PLASMA_enum side, PLASMA_enum uplo, PLASMA_enum trans, PLA
 	    }
 	}
     }
-    /* else /\* side == PlasmaRight *\/ */
-    /* { */
-    /* 	if ( uplo == PlasmaLower ) */
-    /* 	{ */
-    /* 	    if ( trans == PlasmaNoTrans ) */
-    /* 	    { */
-    /* 		dague_trsm = (dague_object_t*)dague_trsm_RLN_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,  */
-    /* 								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,  */
-    /* 								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n, */
-    /* 								  trans, diag, alpha); */
-    /* 	    } */
-    /* 	    else /\* trans =! PlasmaNoTrans *\/ */
-    /* 	    { */
-    /* 		dague_trsm = (dague_object_t*)dague_trsm_RLT_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,  */
-    /* 								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,  */
-    /* 								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n, */
-    /* 								  trans, diag, alpha); */
-    /* 	    } */
-    /* 	} */
-    /* 	else /\* uplo = PlasmaUpper *\/ */
-    /* 	{ */
-    /* 	    if ( trans == PlasmaNoTrans ) */
-    /* 	    { */
-    /* 		dague_trsm = (dague_object_t*)dague_trsm_RUN_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,  */
-    /* 								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,  */
-    /* 								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n, */
-    /* 								  trans, diag, alpha); */
-    /* 	    } */
-    /* 	    else /\* trans =! PlasmaNoTrans *\/ */
-    /* 	    { */
-    /* 		dague_trsm = (dague_object_t*)dague_trsm_RUT_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,  */
-    /* 								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,  */
-    /* 								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n, */
-    /* 								  trans, diag, alpha); */
-    /* 	    } */
-    /* 	} */
-    /* } */
+    else /* side == PlasmaRight */
+    {
+    	if ( uplo == PlasmaLower )
+    	{
+    	    if ( trans == PlasmaNoTrans )
+    	    {
+    		dague_trsm = (dague_object_t*)dague_trsm_RLN_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,
+    								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,
+    								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n,
+    								  trans, diag, alpha);
+    	    }
+    	    else /* trans =! PlasmaNoTrans */
+    	    {
+    		dague_trsm = (dague_object_t*)dague_trsm_RLT_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,
+    								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,
+    								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n,
+    								  trans, diag, alpha);
+    	    }
+    	}
+    	else /* uplo = PlasmaUpper */
+    	{
+    	    if ( trans == PlasmaNoTrans )
+    	    {
+    		dague_trsm = (dague_object_t*)dague_trsm_RUN_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,
+    								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,
+    								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n,
+    								  trans, diag, alpha);
+    	    }
+    	    else /* trans =! PlasmaNoTrans */
+    	    {
+    		dague_trsm = (dague_object_t*)dague_trsm_RUT_new( (dague_ddesc_t*)A, (dague_ddesc_t*)B,
+    								  B->super.mb, B->super.nb, B->super.mt, B->super.nt, B->super.m, B->super.n,
+    								  A->super.mb, A->super.nb, A->super.mt, A->super.nt, A->super.m, A->super.n,
+    								  trans, diag, alpha);
+    	    }
+    	}
+    }
 
     printf("TRSM A:%dx%d (%dx%d) and B:%dx%d (%dx%d) has %d tasks to run.\n", 
 	   A->super.m, A->super.n, A->super.mt, A->super.nt, 
