9a8266dc648c1fe059f9febfb3aa719fa95ce5af
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2010-08-24 12:47:18 -0400

    upgrade pdgemm to the new plasma.
    --HG--
    branch : DAGuE

--- tests/pdgemm/pdgemm.c
+++ tests/pdgemm/pdgemm.c
@@ -20,10 +20,9 @@
 #include <cblas.h>
 #include <math.h>
 #include <plasma.h>
-#include <../src/common.h>
-#include <../src/lapack.h>
-#include <../src/context.h>
-#include <../src/allocate.h>
+#include <control/common.h>
+#include <control/context.h>
+#include <control/allocate.h>
 #include <sys/time.h>
 
 #include "scheduling.h"
