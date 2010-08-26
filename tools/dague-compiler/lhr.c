4bb78ec831d35eb1a282d31a51466d2965c8d7b7
George Bosilca <bosilca@eecs.utk.edu>
2010-08-25 22:08:25 -0400

    Update the example to match with the new features of the dague compiler.
    --HG--
    branch : DAGuE

--- tools/dague-compiler/lhr.c
+++ tools/dague-compiler/lhr.c
@@ -1,5 +1,6 @@
 #include "dague.h"
 #include "cholesky.h"
+#include "data_dist/data_distribution.h"
 
 static dague_ontask_iterate_t printit(struct dague_execution_unit_t *eu, 
                                       dague_execution_context_t *newcontext, 
@@ -8,7 +9,6 @@ static dague_ontask_iterate_t printit(struct dague_execution_unit_t *eu,
                                       int rank_src, int rank_dst,
                                       void *param)
 {
-    (void)eu;
     (void)oldcontext;
     (void)param_index;
     (void)outdep_index;
@@ -36,26 +36,49 @@ static dague_ontask_iterate_t printit(struct dague_execution_unit_t *eu,
                newcontext->locals[2].value);
     }
 
-    newcontext->function->iterate_successors(NULL, newcontext, printit, NULL);
+    newcontext->function->iterate_successors(eu, newcontext, printit, NULL);
 
     return DAGUE_ITERATE_CONTINUE;
 }
 
+static uint32_t rank_of( struct dague_ddesc* mat, ... )
+{
+    (void)mat;
+    return 0;  /* Always me */
+}
+
+static void* data_of( struct dague_ddesc* mat, ... )
+{
+    (void)mat;
+    return NULL;  /* Nothing relevant */
+}
+
 int main(int argc, char *argv[])
 {
-    dague_object_t *o = (dague_object_t*)dague_cholesky_new(NULL, 4, 10, 1.0);
+    dague_ddesc_t empty_data;
+    dague_object_t *o;
     dague_execution_context_t init;
+    dague_context_t *dague;
 
     (void)argc;
     (void)argv;
+
+    dague = dague_init(1, &argc, &argv, 10);
+
+    empty_data.myrank = 0;
+    empty_data.cores = 1;
+    empty_data.nodes = 1;
+    empty_data.rank_of = &rank_of;
+    empty_data.data_of = &data_of;
+    o = (dague_object_t*)dague_cholesky_new(&empty_data, 10, 4, 1.0);
 
     init.dague_object = o;
     init.function = (dague_t*)o->functions_array[3];
     init.locals[0].value = 0;
 
-    printit(NULL, &init, NULL, -1, -1, -1, -1, NULL);
+    printit(dague->execution_units[0], &init, NULL, -1, -1, -1, -1, NULL);
     
-    o->functions_array[3]->iterate_successors(NULL, &init, printit, NULL);
+    o->functions_array[3]->iterate_successors(dague->execution_units[0], &init, printit, NULL);
 
     return 0;
 }
