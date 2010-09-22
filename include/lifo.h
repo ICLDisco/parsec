3c4b9035d3a028269a98408dc0e0cad2b4c66680
thomas herault <herault@eecs.utk.edu>
2010-09-22 18:21:28 -0400

    At last, a FIFO implementation that we can more or less agree on :)

--- include/lifo.h
+++ include/lifo.h
@@ -9,6 +9,8 @@
 
 #include "atomic.h"
 
+#if defined(DAGUE_LIFO_USE_LOCKS)
+
 typedef struct dague_list_item_t {
     volatile struct dague_list_item_t* list_next;
     void* cache_friendly_emptiness;
@@ -29,9 +31,6 @@ static inline dague_list_item_t* dague_list_item_singleton(dague_list_item_t* it
     item->list_prev = item;
     return item;
 }
-
-
-#if defined(DAGUE_LIFO_USE_LOCKS)
 
 typedef struct dague_atomic_lifo_t {
     volatile dague_list_item_t* lifo_head;
@@ -111,10 +110,37 @@ static inline void dague_atomic_lifo_construct( dague_atomic_lifo_t* lifo )
 
 #else
 
+typedef struct dague_list_item_t {
+    volatile struct dague_list_item_t* list_next;
+    void* cache_friendly_emptiness;
+    volatile struct dague_list_item_t* list_prev;
+#ifdef DAGUE_DEBUG
+    volatile int32_t refcount;
+    volatile struct dague_list_t* belong_to_list;
+#endif  /* DAGUE_DEBUG */
+} dague_list_item_t;
+
+/* Make a well formed singleton list with a list item so that it can be 
+ * puhsed. 
+ */
+#define DAGUE_LIST_ITEM_SINGLETON(item) dague_list_item_singleton((dague_list_item_t*) item)
+static inline dague_list_item_t* dague_list_item_singleton(dague_list_item_t* item)
+{
+    item->list_next = item;
+    item->list_prev = item;
+    return item;
+}
+
+#define DAGUE_LIFO_ALIGNMENT 8
+#define DAGUE_LIFO_CNTMASK  (DAGUE_LIFO_ALIGNMENT-1)
+#define DAGUE_LIFO_PTRMASK  (~(DAGUE_LIFO_CNTMASK))
+#define DAGUE_LIFO_CNT( v )   ( (uintptr_t) ( (uintptr_t)(v) & DAGUE_LIFO_CNTMASK ) )
+#define DAGUE_LIFO_PTR( v )   ( (dague_list_item_t *) ( (uintptr_t)(v) & DAGUE_LIFO_PTRMASK ) )
+#define DAGUE_LIFO_VAL( p, c) ( (dague_list_item_t *) ( ((uintptr_t)DAGUE_LIFO_PTR(p)) | DAGUE_LIFO_CNT(c) ) )
 
 typedef struct dague_atomic_lifo_t {
-    volatile dague_list_item_t* lifo_head;
-    dague_list_item_t  lifo_ghost;
+    dague_list_item_t *lifo_head;
+    dague_list_item_t *lifo_ghost;
 } dague_atomic_lifo_t;
 
 /* The ghost pointer will never change. The head will change via an
@@ -123,7 +149,7 @@ typedef struct dague_atomic_lifo_t {
  */
 static inline int dague_atomic_lifo_is_empty( dague_atomic_lifo_t* lifo )
 {
-    return (lifo->lifo_head == &(lifo->lifo_ghost) ? 1 : 0);
+    return ( (DAGUE_LIFO_PTR(lifo->lifo_head) == lifo->lifo_ghost) ? 1 : 0);
 }
 
 /* Add double-linked element to the LIFO. We will return the last head
@@ -132,25 +158,20 @@ static inline int dague_atomic_lifo_is_empty( dague_atomic_lifo_t* lifo )
  * operation).
  */
 static inline dague_list_item_t* dague_atomic_lifo_push( dague_atomic_lifo_t* lifo,
-                                                             dague_list_item_t* items )
+                                                         dague_list_item_t* items )
 {
     dague_list_item_t* tail = (dague_list_item_t*)items->list_prev;
-#ifdef DAGUE_DEBUG
-    {
-        dague_list_item_t* item = items;
-        do {
-            item->refcount++;
-            item->belong_to_list = (struct dague_list_t*)lifo;
-            item = (dague_list_item_t*)item->list_next;
-        } while (item != tail);
-    }
-#endif  /* DAGUE_DEBUG */
+    dague_list_item_t* tp;
+#if defined(DAGUE_DEBUG)
+    assert(  (uintptr_t)items % DAGUE_LIFO_ALIGNMENT == 0 );
+#endif
+
     do {
         tail->list_next = lifo->lifo_head;
-        dague_mfence();
+        tp = DAGUE_LIFO_VAL( items, DAGUE_LIFO_CNT(lifo->lifo_head) );
         if( dague_atomic_cas( &(lifo->lifo_head),
-                                (uintptr_t) tail->list_next,
-                                (uintptr_t) items ) ) {            
+                              (uintptr_t) tail->list_next,
+                              (uintptr_t) tp ) ) {
             return (dague_list_item_t*)tail->list_next;
         }
         /* DO some kind of pause to release the bus */
@@ -163,30 +184,30 @@ static inline dague_list_item_t* dague_atomic_lifo_push( dague_atomic_lifo_t* li
 static inline dague_list_item_t* dague_atomic_lifo_pop( dague_atomic_lifo_t* lifo )
 {
     dague_list_item_t* item;
+    uintptr_t cnt;
 
-    while((item = (dague_list_item_t*)lifo->lifo_head) != &(lifo->lifo_ghost))
+    while(DAGUE_LIFO_PTR((item = lifo->lifo_head)) != lifo->lifo_ghost)
     {
+        cnt = (DAGUE_LIFO_CNT(item) + 1) % DAGUE_LIFO_ALIGNMENT;
         if( dague_atomic_cas( &(lifo->lifo_head),
-                                (uintptr_t) item,
-                                (uintptr_t) item->list_next ) )
+                              (uintptr_t) item,
+                              (uintptr_t) DAGUE_LIFO_VAL( DAGUE_LIFO_PTR(item)->list_next, cnt) ) )
             break;
         /* Do some kind of pause to release the bus */
     }
 
-    if( item == &(lifo->lifo_ghost) ) return NULL;
-#ifdef DAGUE_DEBUG
-    item->list_next = NULL;
-    item->refcount--;
-    item->belong_to_list = NULL;
-#endif  /* DAGUE_DEBUG */
+    item = DAGUE_LIFO_PTR(item);
+
+    if( item == lifo->lifo_ghost ) return NULL;
     return item;
 }
 
 static inline void dague_atomic_lifo_construct( dague_atomic_lifo_t* lifo )
 {
-    lifo->lifo_ghost.list_next = &(lifo->lifo_ghost);
-    lifo->lifo_head = &(lifo->lifo_ghost);
-    dague_mfence();
+    posix_memalign( (void**)&lifo->lifo_ghost, DAGUE_LIFO_ALIGNMENT, sizeof(dague_list_item_t));
+    lifo->lifo_ghost->list_next = lifo->lifo_ghost;
+    lifo->lifo_ghost->list_prev = lifo->lifo_ghost;
+    lifo->lifo_head = lifo->lifo_ghost;
 }
 
 #endif
--- tests/unit/atomics/lifo.c
+++ tests/unit/atomics/lifo.c
@@ -39,7 +39,7 @@ static elt_t *create_elem(int base)
     unsigned int j;
 
     r = rand() % 1024;
-    elt = (elt_t*)malloc( r * sizeof(unsigned int) + sizeof(elt_t) );
+    posix_memalign( (void**)&elt, DAGUE_LIFO_ALIGNMENT, r * sizeof(unsigned int) + sizeof(elt_t) );
     elt->base = base;
     elt->nbelt = r;
     for(j = 0; j < r; j++)
