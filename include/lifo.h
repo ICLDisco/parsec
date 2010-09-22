e8d215b2744b472d66f8fa12cd4c1baf395120da
thomas herault <herault@eecs.utk.edu>
2010-09-22 09:31:17 -0400

    spinlock-based LIFO implementation (activable using a configure switch)

--- include/lifo.h
+++ include/lifo.h
@@ -25,11 +25,92 @@ typedef struct dague_list_item_t {
 #define DAGUE_LIST_ITEM_SINGLETON(item) dague_list_item_singleton((dague_list_item_t*) item)
 static inline dague_list_item_t* dague_list_item_singleton(dague_list_item_t* item)
 {
-    dague_atomic_cas( &(item->list_next), (uintptr_t) item->list_next, (uintptr_t) item );
-    dague_atomic_cas( &(item->list_prev), (uintptr_t) item->list_prev, (uintptr_t) item );
-    dague_mfence();
+    item->list_next = item;
+    item->list_prev = item;
     return item;
 }
+
+
+#if defined(DAGUE_LIFO_USE_LOCKS)
+
+typedef struct dague_atomic_lifo_t {
+    volatile dague_list_item_t* lifo_head;
+    dague_list_item_t  lifo_ghost;
+    volatile uint32_t  lifo_lock;
+} dague_atomic_lifo_t;
+
+static inline int dague_atomic_lifo_is_empty( dague_atomic_lifo_t* lifo )
+{
+    int ret;
+    dague_atomic_lock( &lifo->lifo_lock );
+    ret = (lifo->lifo_head == &(lifo->lifo_ghost) ? 1 : 0);
+    dague_atomic_unlock( &lifo->lifo_lock );
+    return ret;
+}
+
+/* Add double-linked element to the LIFO. We will return the last head
+ * of the list to allow the upper level to detect if this element is
+ * the first one in the list (if the list was empty before this
+ * operation).
+ */
+static inline dague_list_item_t* dague_atomic_lifo_push( dague_atomic_lifo_t* lifo,
+                                                         dague_list_item_t* items )
+{
+    dague_list_item_t* tail = (dague_list_item_t*)items->list_prev;
+    dague_list_item_t* ret;
+
+#ifdef DAGUE_DEBUG
+    {
+        dague_list_item_t* item = items;
+        do {
+            item->refcount++;
+            item->belong_to_list = (struct dague_list_t*)lifo;
+            item = (dague_list_item_t*)item->list_next;
+        } while (item != tail);
+    }
+#endif  /* DAGUE_DEBUG */
+
+    dague_atomic_lock( &lifo->lifo_lock );
+    ret = (dague_list_item_t*)lifo->lifo_head;
+    tail->list_next = ret;
+    lifo->lifo_head = items;
+    dague_atomic_unlock( &lifo->lifo_lock );
+    return ret;
+}
+
+/* Retrieve one element from the LIFO. If we reach the ghost element then the LIFO
+ * is empty so we return NULL.
+ */
+static inline dague_list_item_t* dague_atomic_lifo_pop( dague_atomic_lifo_t* lifo )
+{
+    dague_list_item_t* item;
+
+    dague_atomic_lock( &lifo->lifo_lock );
+    item = (dague_list_item_t*)lifo->lifo_head;
+    lifo->lifo_head = item->list_next;
+    dague_atomic_unlock( &lifo->lifo_lock );
+
+    if( item == &(lifo->lifo_ghost) ) 
+        return NULL;
+
+#if defined(DAGUE_DEBUG)
+    item->refcount--;
+    item->belong_to_list = NULL;
+#endif /* DAGUE_DEBUG */
+
+    return item;
+}
+
+static inline void dague_atomic_lifo_construct( dague_atomic_lifo_t* lifo )
+{
+    lifo->lifo_ghost.list_next = &(lifo->lifo_ghost);
+    lifo->lifo_head = &(lifo->lifo_ghost);
+    lifo->lifo_lock = 0;
+}
+
+
+#else
+
 
 typedef struct dague_atomic_lifo_t {
     volatile dague_list_item_t* lifo_head;
@@ -107,5 +188,7 @@ static inline void dague_atomic_lifo_construct( dague_atomic_lifo_t* lifo )
     lifo->lifo_head = &(lifo->lifo_ghost);
     dague_mfence();
 }
+
+#endif
 
 #endif  /* LIFO_H_HAS_BEEN_INCLUDED */
