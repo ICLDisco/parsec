b5ed435f202c468ee91fe185df5e46db445ac31a
George Bosilca <bosilca@eecs.utk.edu>
2010-10-21 16:58:35 -0400

    Add the DAGUE matrix type.

--- dplasma/lib/dplasmatypes.h
+++ dplasma/lib/dplasmatypes.h
@@ -23,6 +23,7 @@
 #define dagueprefix(fn) dague_z##fn
 #define DAGUEprefix(fn) DAGUE_z##fn
 #define MPITYPE  MPI_DOUBLE_COMPLEX
+#define DAGUE_TYPE_ENUM matrix_ComplexDouble
 
 #elif defined(PRECISION_c)
 #define TYPENAME   PLASMA_Complex32_t
@@ -33,6 +34,7 @@
 #define dagueprefix(fn) dague_c##fn
 #define DAGUEprefix(fn) DAGUE_c##fn
 #define MPITYPE  MPI_COMPLEX
+#define DAGUE_TYPE_ENUM matrix_ComplexFloat
 
 #elif defined(PRECISION_d)
 #define TYPENAME   double
@@ -43,6 +45,7 @@
 #define dagueprefix(fn) dague_d##fn
 #define DAGUEprefix(fn) DAGUE_d##fn
 #define MPITYPE  MPI_DOUBLE
+#define DAGUE_TYPE_ENUM matrix_RealDouble
 
 #elif defined(PRECISION_s) 
 #define TYPENAME   float
@@ -53,8 +56,10 @@
 #define dagueprefix(fn) dague_s##fn
 #define DAGUEprefix(fn) DAGUE_s##fn
 #define MPITYPE  MPI_FLOAT
+#define DAGUE_TYPE_ENUM matrix_RealFloat
+
 #else
-#error "Precision is not selected. You have to set Precision_[zcdf]"
+#error "Precision is not selected. You have to define PRECISION_[zcdf]"
 #endif
 
 #ifdef DAGUE_DRY_RUN
