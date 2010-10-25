c4ee3887f354f9b155a73ba0cb24f63f202f7168
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2010-10-25 18:06:29 -0400

    depends on data_dist matrix

--- dplasma/lib/dplasmaaux.h
+++ dplasma/lib/dplasmaaux.h
@@ -1,6 +1,8 @@
 #ifndef _DPLASMAAUX_H_INCLUDED
 #define _DPLASMAAUX_H_INCLUDED
 
+#include "data_dist/matrix/matrix.h"
+
 int dplasma_aux_get_priority( char* function, const tiled_matrix_desc_t* ddesc );   
 
 #endif
