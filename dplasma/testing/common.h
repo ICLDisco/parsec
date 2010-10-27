fd329bdbb7082696b540032ccbe56519aabf6070
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2010-10-27 16:29:59 -0400

    geqrf + tentative things for the GPU code.

--- dplasma/testing/common.h
+++ dplasma/testing/common.h
@@ -80,10 +80,20 @@ void iparam_default_ibnbmb(int* iparam, int ib, int nb, int mb);
   int NB    = iparam[IPARAM_NB];\
   int SMB   = iparam[IPARAM_SMB];\
   int SNB   = iparam[IPARAM_SNB];\
+  int MT    = (M%MB==0) ? (M/MB) : (M/MB+1); \
+  int NT    = (N%NB==0) ? (N/NB) : (N/NB+1); \
   int check = iparam[IPARAM_CHECK];\
   int loud  = iparam[IPARAM_VERBOSE];\
   (void)rank;(void)nodes;(void)cores;(void)gpus;(void)prio;(void)P;(void)Q;(void)M;(void)N;(void)K;\
   (void)LDA;(void)LDB;(void)LDC;(void)MB;(void)NB;(void)SMB;(void)SNB;(void)check;(void)loud;
+
+/* Some code for disabling CUDA */
+#define PASTE_CODE_IPARAM_ENABLE_GPU(COND, iparam) \
+#if defined(COND) \
+    iparam[IPARAM_NGPUS] = 0; \
+#else \
+    iparam[IPARAM_NGPUS] = -1; \ 
+#endif
 
 /* Define a double type which not pass through the precision generation process */
 typedef double DagDouble_t;
