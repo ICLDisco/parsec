915fd1c84a7e8486756391f6061e1fa5f3f25d34
thomas herault <herault@eecs.utk.edu>
2011-05-23 22:35:44 -0400

    Use #defines to allocate memory pools of appropriate size. Does not resolve the problem, but should be a step forward.

--- dplasma/lib/zherbt_wrapper.c
+++ dplasma/lib/zherbt_wrapper.c
@@ -20,9 +20,9 @@
 dague_object_t *
 dplasma_zherbt_New( PLASMA_enum uplo,
                     int ib,
-                    PLASMA_desc descA,
+                    PLASMA_desc desc_A,
                     tiled_matrix_desc_t *A,
-                    PLASMA_desc descT,
+                    PLASMA_desc desc_T,
                     tiled_matrix_desc_t *T)
 {
     dague_object_t *dague_zherbt = NULL;
@@ -34,24 +34,24 @@ dplasma_zherbt_New( PLASMA_enum uplo,
     }
 
     pool[0] = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));  /* tau */
-    dague_private_memory_init( pool[0], descT.nb * sizeof(Dague_Complex64_t) );
+    dague_private_memory_init( pool[0], zherbt_L_pool_0_SIZE );
     pool[1] = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));  /* work */
-    dague_private_memory_init( pool[1], descT.mb * descT.nb * sizeof(Dague_Complex64_t) );
+    dague_private_memory_init( pool[1], zherbt_L_pool_1_SIZE );
     pool[2] = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));  /* work for HERFB1 */
-    dague_private_memory_init( pool[2], 2 * descT.nb * descT.nb * sizeof(Dague_Complex64_t) );
+    dague_private_memory_init( pool[2], zherbt_L_pool_2_SIZE );
     pool[3] = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));  /* work for the TSMQRLR */
-    dague_private_memory_init( pool[3], 4 * descT.nb * descT.nb * sizeof(Dague_Complex64_t) );
+    dague_private_memory_init( pool[3], zherbt_L_pool_3_SIZE );
 
     if( PlasmaLower == uplo ) {
         dague_zherbt = (dague_object_t *)dague_zherbt_L_new(uplo, 
-                                                            descA, &A->super, 
-                                                            descT, &T->super,
+                                                            desc_A, &A->super, 
+                                                            desc_T, &T->super,
                                                             ib,
                                                             pool[0], pool[1], pool[2], pool[3]);
         dplasma_add2arena_rectangle( ((dague_zherbt_L_object_t *)dague_zherbt)->arenas[DAGUE_zherbt_L_LITTLE_T_ARENA], 
-                                     descT.mb*descT.nb*sizeof(Dague_Complex64_t),
+                                     desc_T.mb*desc_T.nb*sizeof(Dague_Complex64_t),
                                      DAGUE_ARENA_ALIGNMENT_SSE,
-                                     MPI_DOUBLE_COMPLEX, descT.mb, descT.nb, -1);
+                                     MPI_DOUBLE_COMPLEX, desc_T.mb, desc_T.nb, -1);
     }
 
     return dague_zherbt;
