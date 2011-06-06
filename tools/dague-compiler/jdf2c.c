5b596c9d3003b118b5b4e325d20c2dcaf19d14dc
George Bosilca <bosilca@eecs.utk.edu>
2011-06-06 14:36:04 -0400

    Protect the conditions.

--- tools/dague-compiler/jdf2c.c
+++ tools/dague-compiler/jdf2c.c
@@ -1400,7 +1400,7 @@ static char* dump_direct_input_conditions(void **elt, void *arg)
                         string_arena_init(sa1);
                         info.sa = sa1;
                         dump_expr((void**)&dep->guard->guard, &info);
-                        string_arena_add_string( sa, "|| (%s) ", string_arena_get_string(sa1) );
+                        string_arena_add_string( sa, " || (%s) ", string_arena_get_string(sa1) );
                     }
                 }
             }
@@ -1417,7 +1417,7 @@ static char* dump_direct_input_conditions(void **elt, void *arg)
                             string_arena_init(sa1);
                             info.sa = sa1;
                             dump_expr((void**)&dep->guard->guard, &info);
-                            string_arena_add_string( sa, "|| (%s) ", string_arena_get_string(sa1) );
+                            string_arena_add_string( sa, " || (%s) ", string_arena_get_string(sa1) );
                         }
                     } else if( NULL == dep->guard->callfalse->var ) {
                         string_arena_init(sa1);
@@ -1427,7 +1427,7 @@ static char* dump_direct_input_conditions(void **elt, void *arg)
                             string_arena_add_string( sa, "(!(%s)) ", string_arena_get_string(sa1) );
                             already_added = 1;
                         } else {
-                            string_arena_add_string( sa, "|| (!(%s)) ", string_arena_get_string(sa1) );
+                            string_arena_add_string( sa, " || (!(%s)) ", string_arena_get_string(sa1) );
                         }
                     }
                 }
@@ -1507,7 +1507,7 @@ static void jdf_generate_startup_tasks(const jdf_t *jdf, const jdf_function_entr
         string_arena_init(sa2);
         
         condition = UTIL_DUMP_LIST(sa1, f->dataflow, next, dump_direct_input_conditions, sa2,
-                                   "", "", " && ", "");
+                                   "", "(", ") && ", ")");
         if( strlen(condition) > 1 )
             coutput("%s  if( !(%s) ) continue;\n", indent(nesting), condition );
     }
