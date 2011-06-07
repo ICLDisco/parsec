b3771eb22cbba9c5a510d1a2570ab6fdd0b764b1
George Bosilca <bosilca@eecs.utk.edu>
2011-06-07 18:49:16 -0400

    Correctly define the band matrix.

--- dplasma/testing/testing_zhbrdt.c
+++ dplasma/testing/testing_zhbrdt.c
@@ -46,8 +46,8 @@ int main(int argc, char *argv[])
 
     PASTE_CODE_ALLOCATE_MATRIX(ddescA, 1, 
                                two_dim_block_cyclic, (&ddescA, matrix_ComplexDouble,
-                                                      nodes, cores, rank, MB, NB, MB, NB*NT, 0, 0, 
-                                                      MB, NB*NT, 1, SNB, 1 /* 1D cyclic */ ));
+                                                      nodes, cores, rank, MB+1, NB+2, MB+1, (NB+2)*NT, 0, 0, 
+                                                      MB+1, (NB+2)*NT, 1, SNB, 1 /* 1D cyclic */ ));
 
     generate_tiled_random_mat((tiled_matrix_desc_t *) &ddescA, 100);
 
