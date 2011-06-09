5a113e62e647ad036591abcc26dfb456483efeb6
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2011-06-09 14:24:37 -0400

    Remove zheev parts from herbt, we have a separate heev testing,

--- dplasma/testing/testing_zherbt.c
+++ dplasma/testing/testing_zherbt.c
@@ -208,9 +208,6 @@ int main(int argc, char *argv[])
     PASTE_CODE_IPARAM_LOCALS(iparam)
     PASTE_CODE_FLOPS_COUNT(FADDS_ZHERBT, FMULS_ZHERBT, ((DagDouble_t)N))
 
-    LDA = max(M, LDA);
-    LDB = max( LDB, N );
-
     PLASMA_Init(1);
 
     /*
@@ -247,11 +244,6 @@ int main(int argc, char *argv[])
          ddescT.super.lm, ddescT.super.ln, ddescT.super.i, ddescT.super.j, 
          ddescT.super.m, ddescT.super.n);
 
-    PASTE_CODE_ALLOCATE_MATRIX(ddescBAND, 1, 
-        two_dim_block_cyclic, (&ddescBAND, matrix_ComplexDouble,
-        nodes, cores, rank, MB+1, NB+2, MB+1, (NB+2)*(NT+1), 0, 0, 
-        MB+1, (NB+2)*(NT+1), 1, SNB, 1 /* 1D cyclic */ ));
-
 
     PLASMA_enum uplo = PlasmaLower;
 
@@ -261,6 +253,7 @@ int main(int argc, char *argv[])
     double *W1             = (double *)malloc(N*sizeof(double));
     double *W2             = (double *)malloc(N*sizeof(double));
 
+
     if( check ) {
         /*int i, j;*/
         PLASMA_Tile_to_Lapack(plasmaDescA, (void*)A2, N);
@@ -268,84 +261,31 @@ int main(int argc, char *argv[])
         LAPACKE_zheev( LAPACK_COL_MAJOR,
                lapack_const(PlasmaNoVec), lapack_const(uplo), 
                N, A2, LDA, W1);
-
-	/*
-        printf("A2 avant\n");
-        for (i = 0; i < N; i++){
-            for (j = 0; j < N; j++) {
-                //printf("%f+%fi ", creal(A2[LDA*j+i]), cimag(A2[LDA*j+i]));
-                printf("%f ", A2[LDA*j+i]);
-            }
-            printf("\n");
-        }
-	printf("Eigenvalues original\n");
-        for (i = 0; i < N; i++){
-            printf("%f \n", W1[i]);
-        }
-        printf("\n");
-	*/
     }
     
+    /*
+    printf("A2 avant\n");
+    for (i = 0; i < N; i++){
+        for (j = 0; j < N; j++) {
+            //printf("%f+%fi ",
+                creal(A2[LDA*j+i]),
+                cimag(A2[LDA*j+i]));
+            printf("%f ", A2[LDA*j+i]);
+        }
+        printf("\n");
+    }
+    printf("Eigenvalues original\n");
+    for(i = 0; i < N; i++){
+        printf("%f\n", W1[i]);
+    }
+    printf("\n");
+    */
+
     PASTE_CODE_ENQUEUE_KERNEL(dague, zherbt, 
          (uplo, IB, *plasmaDescA, (tiled_matrix_desc_t*)&ddescA, *plasmaDescT, (tiled_matrix_desc_t*)&ddescT));
 
     PASTE_CODE_PROGRESS_KERNEL(dague, zherbt);
     
-#if 0
-/* Check for the conversion from 2D band diag to 1D band */
-    int i,j;
-    for(j = 0; j < NT; j++) for(i = 0; i < MT; i++)
-    {
-        if(rank == ddescA.super.super.rank_of(&ddescA.super.super, i, j))
-        {
-            A2 = ddescA.super.super.data_of(&ddescA.super.super, i, j);
-            int ii,jj;
-            for(jj = 0; jj < NB; jj++) 
-            {for(ii = 0; ii < MB; ii++)
-            {
-                A2[jj*MB+ii] = (Dague_Complex64_t)(ii+i*MB+(j*NB+jj)*100);
-                //printf(" %g", A2[jj*MB+ii]);
-            }
-            //printf("\n");
-            }
-            //printf("\n");
-        }
-    }
-#endif
-
-    SYNC_TIME_START();
-    dague_object_t* DAGUE_diag_band_to_rect = (dague_object_t*) dague_diag_band_to_rect_new((sym_two_dim_block_cyclic_t*)&ddescA, &ddescBAND, 
-            MT, NT, MB, NB, sizeof(matrix_ComplexDouble));
-    dague_arena_t* arena = ((dague_diag_band_to_rect_object_t*)DAGUE_diag_band_to_rect)->arenas[DAGUE_diag_band_to_rect_DEFAULT_ARENA]; 
-    dplasma_add2arena_tile(arena,
-        MB*NB*sizeof(Dague_Complex64_t),
-        DAGUE_ARENA_ALIGNMENT_SSE,
-        MPI_DOUBLE_COMPLEX, MB);
-    dague_enqueue(dague, DAGUE_diag_band_to_rect);
-    dague_progress(dague);
-    SYNC_TIME_PRINT(rank, ( "diag_band_to_rect N= %d NB = %d : %f s\n", N, NB, sync_time_elapsed));
-
-#if 0
-/* Check for the conversion from 2D band diag to 1D band */
-    for(j = 0; j < NT+1; j++)
-    {
-        if(rank == ddescBAND.super.super.rank_of(&ddescBAND.super.super, 0, j))
-        {
-            int ii,jj;
-            A2 = ddescBAND.super.super.data_of(&ddescBAND.super.super, 0, j);
-            for(jj = 0; jj < NB; jj++) { for(ii = 0; ii < MB+1; ii++)
-            {
-                printf(" %g", A2[jj*(MB+1)+ii]);
-                if(ii+jj+j*MB >= MT*MB) assert(A2[jj*(MB+1)+ii] == 0e0);
-                else assert(A2[jj*(MB+1)+ii] == (ii+jj+j*MB+(j*NB+jj)*100));
-            }
-            printf("\n");
-            }
-            printf("\n");
-        }
-    }
-#endif
-
     if( check ) {
         int i, j;
         PLASMA_Tile_to_Lapack(plasmaDescA, (void*)A2, N);
@@ -376,7 +316,7 @@ int main(int argc, char *argv[])
 
         double eps = BLAS_dfpinfo( blas_eps );
         printf("\n");
-        printf("------ TESTS FOR PLASMA ZHEEV ROUTINE -------  \n");
+        printf("------ TESTS FOR PLASMA ZHERBT ROUTINE -------  \n");
         printf("        Size of the Matrix %d by %d\n", N, N);
         printf("\n");
         printf(" The matrix A is randomly generated for each test.\n");
@@ -389,28 +329,26 @@ int main(int argc, char *argv[])
     
         if (info_solution == 0) {
             printf("***************************************************\n");
-            printf(" ---- TESTING ZHEEV ...................... PASSED !\n");
+            printf(" ---- TESTING ZHERBT ..................... PASSED !\n");
             printf("***************************************************\n");
         }
         else {
             printf("************************************************\n");
-            printf(" - TESTING ZHEEV ... FAILED !\n");
+            printf(" - TESTING ZHERBT ..................... FAILED !\n");
             printf("************************************************\n");
         }
     }
 
-    free(A2); free(W1); free(W2);
     dplasma_zherbt_Destruct( DAGUE_zherbt );
 
+    free(A2); free(W1); free(W2);
     dague_data_free(ddescA.mat);
     dague_data_free(ddescT.mat);
-    dague_data_free(ddescBAND.mat);
+    dague_ddesc_destroy((dague_ddesc_t*)&ddescA);
+    dague_ddesc_destroy((dague_ddesc_t*)&ddescT);
 
     cleanup_dague(dague, iparam);
    
-    dague_ddesc_destroy((dague_ddesc_t*)&ddescBAND);
-    dague_ddesc_destroy((dague_ddesc_t*)&ddescA);
-    dague_ddesc_destroy((dague_ddesc_t*)&ddescT);
         
     return EXIT_SUCCESS;
 }
