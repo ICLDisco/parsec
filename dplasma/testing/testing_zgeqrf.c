097f3ba7d35d28cbf018f3131a062696de4b4341
Mathieu Faverge <mfaverge@eecs.utk.edu>
2011-07-29 18:33:06 -0400

    Add semicolons
    --HG--
    branch : new_scheduling

--- dplasma/testing/testing_zgeqrf.c
+++ dplasma/testing/testing_zgeqrf.c
@@ -38,8 +38,8 @@ int main(int argc, char ** argv)
 
     /* Initialize DAGuE */
     dague = setup_dague(argc, argv, iparam);
-    PASTE_CODE_IPARAM_LOCALS(iparam)
-    PASTE_CODE_FLOPS_COUNT(FADDS_GEQRF, FMULS_GEQRF, ((DagDouble_t)M,(DagDouble_t)N))
+    PASTE_CODE_IPARAM_LOCALS(iparam);
+    PASTE_CODE_FLOPS_COUNT(FADDS_GEQRF, FMULS_GEQRF, ((DagDouble_t)M,(DagDouble_t)N));
       
     LDA = max(M, LDA);
     /* initializing matrix structure */
