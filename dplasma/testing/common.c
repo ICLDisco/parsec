d658c054a79fa584f0d1242c6421410ac2ae90b1
George Bosilca <bosilca@eecs.utk.edu>
2011-12-18 02:58:37 -0500

    A serialized friendly MPI should be enough.

--- dplasma/testing/common.c
+++ dplasma/testing/common.c
@@ -427,11 +427,9 @@ dague_context_t* setup_dague(int argc, char **argv, int *iparam)
 #endif
 #ifdef HAVE_MPI
     int provided;
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &provided);
     MPI_Comm_size(MPI_COMM_WORLD, &iparam[IPARAM_NNODES]);
     MPI_Comm_rank(MPI_COMM_WORLD, &iparam[IPARAM_RANK]); 
-    if(provided < MPI_THREAD_SERIALIZED)
-        fprintf(stderr, "!!! MPI serialized thread safety is not provided.\n");
 #else
     iparam[IPARAM_NNODES] = 1;
     iparam[IPARAM_RANK] = 0;
