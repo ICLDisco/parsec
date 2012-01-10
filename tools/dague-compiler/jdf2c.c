cf1f6a706e2e78e9733527547745bb93b3125977
George Bosilca <bosilca@eecs.utk.edu>
2012-01-10 08:33:00 +0300

    Include the datarepo header.

--- tools/dague-compiler/jdf2c.c
+++ tools/dague-compiler/jdf2c.c
@@ -958,6 +958,7 @@ static void jdf_generate_structure(const jdf_t *jdf)
     coutput("#include <dague.h>\n"
             "#include <scheduling.h>\n"
             "#include <remote_dep.h>\n"
+            "#include <datarepo.h>\n"
             "#if defined(HAVE_PAPI)\n"
             "#include <papime.h>\n"
             "#endif\n"
