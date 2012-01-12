1a7673a49753665816599bb4021d978bda2964fb
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2012-01-12 16:46:10 -0500

    fw masks are not in the eu_context anymore. i

--- include/execution_unit.h
+++ include/execution_unit.h
@@ -41,8 +41,6 @@ struct dague_execution_unit {
     dague_context_t*        master_context;
     dague_thread_mempool_t* context_mempool;
     dague_thread_mempool_t* datarepo_mempools[MAX_PARAM_COUNT+1];
-
-    uint32_t* remote_dep_fw_mask;
 };
 
 struct dague_context_t {
