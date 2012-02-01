461b822ffab5b62abce57ddd9fbacb20883aa1f6
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-02-01 17:22:10 -0500

    Add herk declaration

--- dplasma/include/dplasma_z.h
+++ dplasma/include/dplasma_z.h
@@ -18,6 +18,9 @@ void dplasma_zgemm( dague_context_t *dague, const int transA, const int transB,
 int  dplasma_zhemm( dague_context_t *dague, const PLASMA_enum side, const PLASMA_enum uplo,
                     const Dague_Complex64_t alpha, const tiled_matrix_desc_t* A, const tiled_matrix_desc_t* B,
                     const double beta,  tiled_matrix_desc_t* C);
+int  dplasma_zherk( dague_context_t *dague, const PLASMA_enum uplo, const PLASMA_enum trans,
+                    const double alpha, const tiled_matrix_desc_t* A,
+                    const double beta,  tiled_matrix_desc_t* C);
 void dplasma_ztrmm( dague_context_t *dague, const PLASMA_enum side, const PLASMA_enum uplo, const PLASMA_enum trans, const PLASMA_enum diag,
                     const PLASMA_Complex64_t alpha, const tiled_matrix_desc_t *A, tiled_matrix_desc_t *B);
 int dplasma_ztrsm( dague_context_t *dague, const PLASMA_enum side, const PLASMA_enum uplo, const PLASMA_enum trans, const PLASMA_enum diag,
@@ -68,6 +71,9 @@ dague_object_t* dplasma_zgemm_New( const int transa, const int transb,
 dague_object_t* dplasma_zhemm_New( const PLASMA_enum side, const PLASMA_enum uplo,
                                    const Dague_Complex64_t alpha, const tiled_matrix_desc_t* A, const tiled_matrix_desc_t* B,
                                    const double beta,  tiled_matrix_desc_t* C);
+dague_object_t* dplasma_zherk_New( const PLASMA_enum uplo, const PLASMA_enum trans,
+                                   const double alpha, const tiled_matrix_desc_t* A,
+                                   const double beta,  tiled_matrix_desc_t* C);
 dague_object_t* dplasma_ztrmm_New( const PLASMA_enum side, const PLASMA_enum uplo, const PLASMA_enum trans, const PLASMA_enum diag,
 				   const PLASMA_Complex64_t alpha, const tiled_matrix_desc_t *A, tiled_matrix_desc_t *B);
 dague_object_t* dplasma_ztrsm_New( const PLASMA_enum side, const PLASMA_enum uplo, const PLASMA_enum trans, const PLASMA_enum diag,
@@ -113,6 +119,7 @@ dague_object_t* dplasma_zgebut_New( tiled_matrix_desc_t *A, int it, int jt, int 
  */
 void dplasma_zgemm_Destruct( dague_object_t *o );
 void dplasma_zhemm_Destruct( dague_object_t *o );
+void dplasma_zherk_Destruct( dague_object_t *o );
 void dplasma_ztrmm_Destruct( dague_object_t *o );
 void dplasma_ztrsm_Destruct( dague_object_t *o );
 void dplasma_ztrsmpl_Destruct( dague_object_t *o );
