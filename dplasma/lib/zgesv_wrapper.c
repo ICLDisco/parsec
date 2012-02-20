7d0f9a81d4a4c9869beebfbaca724956809a020c
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-02-20 10:15:10 -0500

    Add getrs and gesv wrapper for partial pivoting interface

---
+++ dplasma/lib/zgesv_wrapper.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2010      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+#include "dague.h"
+#include <plasma.h>
+#include "dplasma.h"
+
+int
+dplasma_zgesv( dague_context_t *dague,
+               tiled_matrix_desc_t *A,
+               tiled_matrix_desc_t *IPIV,
+               tiled_matrix_desc_t *B)
+{
+    int info;
+
+#ifdef DAGUE_COMPOSITION
+#warning "Not implemented"
+
+    dague_object_t *dague_zgetrf  = dplasma_zgetrf_New(A, L, IPIV, &info);
+    dague_object_t *dague_ztrsm   = dplasma_ztrsmpl_New(A, L, IPIV, B);
+    dague_object_t *dague_ztrsm   = dplasma_ztrsm_New(PlasmaLeft, PlasmaUpper, PlasmaNoTrans, PlasmaNonUnit, 1.0, A, B);
+
+    dague_enqueue( dague, dague_zgetrf  );
+    dague_enqueue( dague, dague_ztrsmpl );
+    dague_enqueue( dague, dague_ztrsm   );
+
+    dplasma_progress( dague );
+
+    dplasma_zgetrf_Destruct( dague_zgetrf  );
+    dplasma_ztrsmpl_Destruct( dague_ztrsmpl );
+    dplasma_ztrsm_Destruct( dague_ztrsm   );
+#else
+    info = dplasma_zgetrf(dague, A, IPIV );
+    if( info == 0 ) {
+        dplasma_zgetrs( dague, PlasmaNoTrans, A, IPIV, B );
+    }
+#endif
+
+    return info;
+}
