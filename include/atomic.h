d10dcb8bc076e8c7ed06b71f0a48ff55c57dc67a
George Bosilca <bosilca@eecs.utk.edu>
2012-03-12 11:10:46 -0400

    Pedantic.

--- include/atomic.h
+++ include/atomic.h
@@ -34,29 +34,29 @@
 #include <assert.h>
 
 static inline int dague_atomic_cas_xxb( volatile void* location,
-                                          uint64_t old_value,
-                                          uint64_t new_value,
-                                          size_t type_size )
+                                        uint64_t old_value,
+                                        uint64_t new_value,
+                                        size_t type_size )
 {
     switch(type_size){
     case 4:
         return dague_atomic_cas_32b( (volatile uint32_t*)location,
-                                       (uint32_t)old_value, (uint32_t)new_value );
+                                     (uint32_t)old_value, (uint32_t)new_value );
     case 8:
         return dague_atomic_cas_64b( (volatile uint64_t*)location,
-                                       (uint64_t)old_value, (uint64_t)new_value );
+                                     (uint64_t)old_value, (uint64_t)new_value );
     }
     return 0;
 }
 
 static inline uint64_t dague_atomic_bor_xxb( volatile void* location,
-                                               uint64_t or_value,
-                                               size_t type_size )
+                                             uint64_t or_value,
+                                             size_t type_size )
 {
     assert( 4 == type_size );
-	(void)type_size;
+    (void)type_size;
     return (uint64_t)dague_atomic_bor_32b( (volatile uint32_t*)location,
-                                             (uint32_t)or_value);
+                                           (uint32_t)or_value);
 }
 
 #define dague_atomic_band(LOCATION, OR_VALUE)  \
@@ -65,10 +65,10 @@ static inline uint64_t dague_atomic_bor_xxb( volatile void* location,
 #define dague_atomic_bor(LOCATION, OR_VALUE)  \
     (__typeof__(*(LOCATION)))dague_atomic_bor_xxb(LOCATION, OR_VALUE, sizeof(*(LOCATION)) )
 
-#define dague_atomic_cas(LOCATION, OLD_VALUE, NEW_VALUE)              \
-    dague_atomic_cas_xxb((volatile void*)(LOCATION),              \
-                           (uint64_t)(OLD_VALUE), (uint64_t)(NEW_VALUE), \
-                           sizeof(*(LOCATION)))                         \
+#define dague_atomic_cas(LOCATION, OLD_VALUE, NEW_VALUE)               \
+    dague_atomic_cas_xxb((volatile void*)(LOCATION),                   \
+                         (uint64_t)(OLD_VALUE), (uint64_t)(NEW_VALUE), \
+                         sizeof(*(LOCATION)))
     
 #define dague_atomic_set_mask(LOCATION, MASK) dague_atomic_bor((LOCATION), (MASK))
 #define dague_atomic_clear_mask(LOCATION, MASK)  dague_atomic_band((LOCATION), ~(MASK))
