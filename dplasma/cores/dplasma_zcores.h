7813bbe340d9b8b1a0282ff749578ff6a14e16f7
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-03-30 00:38:00 -0400

    Add declaration of getrf_rectil
    --HG--
    branch : qr_lu

--- dplasma/cores/dplasma_zcores.h
+++ dplasma/cores/dplasma_zcores.h
@@ -7,10 +7,18 @@
  *
  */
 
+#include <math.h>
+#include <cblas.h>
+#include <lapacke.h>
+#include <plasma.h>
 #include "dague.h"
+#include "data_dist/matrix/precision.h"
+#include "dplasma_cores.h"
 
 int blgchase_ztrdv2(int NT, int N, int NB,
                    Dague_Complex64_t *A1, Dague_Complex64_t *A2,
                    Dague_Complex64_t *V1, Dague_Complex64_t *TAU1,
                    Dague_Complex64_t *V2, Dague_Complex64_t *TAU2,
                    int sweep, int id, int blktile);
+
+int CORE_zgetrf_rectil_1thrd(const PLASMA_desc A, int *IPIV);
