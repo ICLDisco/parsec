e8e544db4495a15a86bff2a8e4537920bcb0476d
ozenati@remus.eecs.utk.edu <ozenati@remus.eecs.utk.edu>
2012-04-03 21:23:05 -0400

    Modified the jdf of zlange inf. Added the wrapper and testing. Segfault for the moment.
    --HG--
    branch : zenati

---
+++ dplasma/lib/zlange_inf_wrapper.c
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2011      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+#include "dague.h"
+#include <plasma.h>
+#include <core_blas.h>
+#include "dplasma.h"
+#include "dplasma/lib/dplasmatypes.h"
+#include "dplasma/lib/dplasmaaux.h"
+#include "data_dist/matrix/two_dim_rectangle_cyclic.h"
+#include "map2.h"
+
+#include "zlange_inf_cyclic.h"
+
+dague_object_t* dplasma_zlange_inf_New( tiled_matrix_desc_t *A,
+                                        tiled_matrix_desc_t *W,
+                                        tiled_matrix_desc_t *S,
+                                        int P, int Q)
+{
+    dague_object_t *dague_zlange_inf = NULL;
+
+    dague_zlange_inf = (dague_object_t*)dague_zlange_inf_cyclic_new(*A, (dague_ddesc_t*)A,
+                                                                    *W, (dague_ddesc_t*)W,
+                                                                    *S, (dague_ddesc_t*)S,
+                                                                    P, Q);
+
+    dplasma_add2arena_tile(((dague_zlange_inf_cyclic_object_t*)dague_zlange_inf)->arenas[DAGUE_zlange_inf_cyclic_DEFAULT_ARENA],
+                           A->mb*A->nb*sizeof(Dague_Complex64_t),
+                           DAGUE_ARENA_ALIGNMENT_SSE,
+                           MPI_DOUBLE_COMPLEX, A->mb);
+
+    dplasma_add2arena_tile(((dague_zlange_inf_cyclic_object_t*)dague_zlange_inf)->arenas[DAGUE_zlange_inf_cyclic_DEFAULT_ARENA],
+                           A->mb*A->nb*sizeof(Dague_Complex64_t),
+                           DAGUE_ARENA_ALIGNMENT_SSE,
+                           MPI_DOUBLE_COMPLEX, A->mb);
+
+    return dague_zlange_inf;
+}
+
+void
+dplasma_zlange_inf_Destruct( dague_object_t *o )
+{
+    dague_zlange_inf_cyclic_object_t *dague_zlange = (dague_zlange_inf_cyclic_object_t *)o;
+    dague_zlange_inf_cyclic_destroy(dague_zlange);
+}
+
+double dplasma_zlange_inf( dague_context_t *dague,
+                           PLASMA_enum ntype,
+                           tiled_matrix_desc_t *A)
+{
+    two_dim_block_cyclic_t workW, workS;
+    double result = -1.0;
+    dague_object_t *dague_zlange_inf = NULL;
+
+    switch( ntype )
+    {
+    case PlasmaInfNorm:
+        PASTE_CODE_INIT_AND_ALLOCATE_MATRIX(
+            workW, two_dim_block_cyclic,
+            (&workW, matrix_RealDouble, matrix_Tile, 1, A->super.cores, A->super.myrank,
+             1, 1, A->mt, A->nt, 0, 0, A->mt, A->nt,
+             ((two_dim_block_cyclic_t*)A)->grid.strows, ((two_dim_block_cyclic_t*)A)->grid.stcols,
+             ((two_dim_block_cyclic_t*)A)->grid.rows));
+        PASTE_CODE_INIT_AND_ALLOCATE_MATRIX(
+            workS, two_dim_block_cyclic,
+            (&workS, matrix_RealDouble, matrix_Tile, 1, A->super.cores, A->super.myrank,
+             1, 1, A->mt, A->nt, 0, 0, A->mt, A->nt, 1, 1, 1));
+        break;
+
+    default:
+        return -1.0;
+    }
+
+    dague_zlange_inf = dplasma_zlange_inf_New(A, (tiled_matrix_desc_t *)&workW, (tiled_matrix_desc_t *)&workS, A->mt, A->nt);
+
+    if ( dague_zlange_inf != NULL )
+    {
+        dague_enqueue( dague, (dague_object_t*)dague_zlange_inf);
+        dplasma_progress(dague);
+        dplasma_zlange_inf_Destruct( dague_zlange_inf );
+    }
+
+
+    dague_data_free(workW.mat);
+    dague_ddesc_destroy((dague_ddesc_t*)&workW);
+    dague_data_free(workS.mat);
+    dague_ddesc_destroy((dague_ddesc_t*)&workS);
+
+
+#if defined(HAVE_MPI)
+    MPI_Bcast(&result, 1, MPI_DOUBLE, 0, dplasma_comm);
+#endif
+
+    return result;
+}
+
---
+++ dplasma/testing/testing_zlange.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2009-2011 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+
+#include "common.h"
+#include "data_dist/matrix/two_dim_rectangle_cyclic.h"
+
+
+int main(int argc, char ** argv)
+{
+    dague_context_t* dague;
+    int iparam[IPARAM_SIZEOF];
+
+    /* Set defaults for non argv iparams */
+    iparam_default_facto(iparam);
+    iparam_default_ibnbmb(iparam, 40, 200, 200);
+    iparam[IPARAM_LDA] = -'m';
+    iparam[IPARAM_LDB] = -'m';
+#if defined(HAVE_CUDA) && defined(PRECISION_s) && 0
+    iparam[IPARAM_NGPUS] = 0;
+#endif
+    /* Initialize DAGuE */
+    dague = setup_dague(argc, argv, iparam);
+    PASTE_CODE_IPARAM_LOCALS(iparam)
+    PASTE_CODE_FLOPS(FLOPS_ZGETRF, ((DagDouble_t)M, (DagDouble_t)N))
+
+    if ( M != N && check ) {
+        fprintf(stderr, "Check cannot be perfomed with M != N\n");
+        check = 0;
+    }
+
+    /* initializing matrix structure */
+    PASTE_CODE_ALLOCATE_MATRIX(ddescA, 1,
+        two_dim_block_cyclic, (&ddescA, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDA, N, 0, 0,
+                               M, N, SMB, SNB, P));
+    /* matrix generation */
+    if(loud > 2) printf("+++ Generate matrices ... ");
+    dplasma_zplrnt( dague, (tiled_matrix_desc_t *)&ddescA, 7657);
+
+    if(loud > 2) printf("Done\n");
+
+    if(loud > 2) printf("+++ Computing getrf_sp ... ");
+
+    /* Computing LU */
+
+    double ret = dplasma_zlange_inf(dague,PlasmaInfNorm, (tiled_matrix_desc_t *)&ddescA);
+
+    printf("The infini norm of A is %g",ret);
+
+    if(check)
+    {
+        Dague_Complex64_t *A;
+        A = (Dague_Complex64_t *)malloc((ddescA.super.lm)*(ddescA.super.n)*sizeof(Dague_Complex64_t));
+
+        twoDBC_ztolapack( &ddescA, A, LDA );
+
+        double ret = LAPACKE_zlange(LAPACK_COL_MAJOR, 'i', N, N, A, LDA);
+        printf("The infini Lapacke norm of A is %g",ret);
+
+        free(A);
+    }
+
+    if(loud > 2) printf("Done.\n");
+
+
+    dague_data_free(ddescA.mat);
+    dague_ddesc_destroy((dague_ddesc_t*)&ddescA);
+
+    cleanup_dague(dague, iparam);
+
+    return EXIT_SUCCESS;
+}
