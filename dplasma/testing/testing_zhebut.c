d1ddddc87301259f77cd2828cde67b27d7a073ad
Anthony Danalis <adanalis@eecs.utk.edu>
2012-04-10 12:13:13 -0400

    Code to check the solution.

--- dplasma/testing/testing_zhebut.c
+++ dplasma/testing/testing_zhebut.c
@@ -15,6 +15,11 @@
 #include "dplasma/cores/cuda_sgemm.h"
 #endif
 
+static int check_solution( dague_context_t *dague, int loud, PLASMA_enum uplo,
+                           tiled_matrix_desc_t *ddescA,
+                           tiled_matrix_desc_t *ddescB,
+                           tiled_matrix_desc_t *ddescX );
+
 int main(int argc, char ** argv)
 {
     dague_context_t* dague;
@@ -47,26 +52,43 @@ int main(int argc, char ** argv)
                                    nodes, cores, rank, MB, NB, LDA, N, 0, 0,
                                    N, N, P, uplo));
 
-    PASTE_CODE_ALLOCATE_MATRIX(ddescX, 1,
+    PASTE_CODE_ALLOCATE_MATRIX(ddescA0, check,
+        sym_two_dim_block_cyclic, (&ddescA0, matrix_ComplexDouble,
+                                   nodes, cores, rank, MB, NB, LDA, N, 0, 0,
+                                   N, N, P, uplo));
+
+    PASTE_CODE_ALLOCATE_MATRIX(ddescB, check,
+        two_dim_block_cyclic, (&ddescB, matrix_ComplexDouble, matrix_Tile,
+                                   nodes, cores, rank, MB, NB, LDB, NRHS, 0, 0,
+                                   N, NRHS, SMB, SNB, P));
+
+    PASTE_CODE_ALLOCATE_MATRIX(ddescX, check,
         two_dim_block_cyclic, (&ddescX, matrix_ComplexDouble, matrix_Tile,
-                                   nodes, cores, rank, MB, NB, LDA, NRHS, 0, 0,
+                                   nodes, cores, rank, MB, NB, LDB, NRHS, 0, 0,
                                    N, NRHS, SMB, SNB, P));
 
     /* matrix generation */
     if(loud > 2) printf("+++ Generate matrices ... ");
-    dplasma_zplghe( dague, (double)(N), uplo,
+    dplasma_zplghe( dague, (double)(0.), uplo,
                     (tiled_matrix_desc_t *)&ddescA, 1358);
-    dplasma_zplrnt( dague,
-                    (tiled_matrix_desc_t *)&ddescX, 3872);
+    if( check ){
+        dplasma_zplrnt( dague,
+                        (tiled_matrix_desc_t *)&ddescB, 3872);
+        dplasma_zlacpy( dague, uplo, 
+                        (tiled_matrix_desc_t *)&ddescA,
+                        (tiled_matrix_desc_t *)&ddescA0);
+        dplasma_zlacpy( dague, PlasmaUpperLower, 
+                        (tiled_matrix_desc_t *)&ddescB,
+                        (tiled_matrix_desc_t *)&ddescX);
+    }
     if(loud > 2) printf("Done\n");
 
-    if(loud > 2) printf("+++ Computing Butterfly ... ");
+    if(loud > 2) printf("+++ Computing Butterfly ... \n");
 
     SYNC_TIME_START();
     TIME_START();
     dplasma_zhebut(dague, (tiled_matrix_desc_t *)&ddescA, butterfly_level);
     dplasma_zhetrf(dague, (tiled_matrix_desc_t *)&ddescA);
-    dplasma_zhetrs(dague, (const tiled_matrix_desc_t *)&ddescA, (tiled_matrix_desc_t *)&ddescX);
 
     fprintf(stderr,"-- DONE\n");
 
@@ -83,6 +105,20 @@ int main(int argc, char ** argv)
     if ( info != 0 ) {
         if( rank == 0 && loud ) printf("-- The butterfly failed (info = %d) ! \n", info);
         ret |= 1;
+    }else if(check){
+        dplasma_zhetrs(dague, uplo, (const tiled_matrix_desc_t *)&ddescA, (tiled_matrix_desc_t *)&ddescX);
+
+        /* Check the solution */
+        /*
+        ret |= check_factorization( dague, (rank == 0) ? loud : 0, uplo,
+                                    (tiled_matrix_desc_t *)&ddescA,
+                                    (tiled_matrix_desc_t *)&ddescA0);
+        */
+
+        ret |= check_solution( dague, (rank == 0) ? loud : 0, uplo,
+                               (tiled_matrix_desc_t *)&ddescA0,
+                               (tiled_matrix_desc_t *)&ddescB,
+                               (tiled_matrix_desc_t *)&ddescX);
     }
 
     cleanup_dague(dague, iparam);
@@ -93,3 +129,52 @@ int main(int argc, char ** argv)
     return ret;
 }
 
+
+/*
+ * This function destroys B
+ */
+static int check_solution( dague_context_t *dague, int loud, PLASMA_enum uplo,
+                           tiled_matrix_desc_t *ddescA,
+                           tiled_matrix_desc_t *ddescB,
+                           tiled_matrix_desc_t *ddescX )
+{
+    int info_solution;
+    double Rnorm = 0.0;
+    double Anorm = 0.0;
+    double Bnorm = 0.0;
+    double Xnorm, result;
+    int N = ddescB->m;
+    double eps = LAPACKE_dlamch_work('e');
+
+    Anorm = dplasma_zlanhe(dague, PlasmaMaxNorm, uplo, ddescA);
+    Bnorm = dplasma_zlange(dague, PlasmaMaxNorm, ddescB);
+    Xnorm = dplasma_zlange(dague, PlasmaMaxNorm, ddescX);
+
+    /* Compute A*x */
+    dplasma_zhemm( dague, PlasmaLeft, uplo, -1.0, ddescA, ddescX, 1.0, ddescB);
+
+    Rnorm = dplasma_zlange(dague, PlasmaMaxNorm, ddescB);
+
+    result = Rnorm / ( ( Anorm * Xnorm + Bnorm ) * N * eps ) ;
+
+    if ( loud > 2 ) {
+        printf("============\n");
+        printf("Checking the Residual of the solution \n");
+        if ( loud > 3 )
+            printf( "-- ||A||_oo = %e, ||X||_oo = %e, ||B||_oo= %e, ||A X - B||_oo = %e\n",
+                    Anorm, Xnorm, Bnorm, Rnorm );
+
+        printf("-- ||Ax-B||_oo/((||A||_oo||x||_oo+||B||_oo).N.eps) = %e \n", result);
+    }
+
+    if (  isnan(Xnorm) || isinf(Xnorm) || isnan(result) || isinf(result) || (result > 60.0) ) {
+        if( loud ) printf("-- Solution is suspicious ! \n");
+        info_solution = 1;
+    }
+    else{
+        if( loud ) printf("-- Solution is CORRECT ! \n");
+        info_solution = 0;
+    }
+
+    return info_solution;
+}
