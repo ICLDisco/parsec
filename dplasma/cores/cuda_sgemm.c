684d7a3f427aa0ced454e448db0fa59c45e8bf79
George Bosilca <bosilca@eecs.utk.edu>
2012-04-13 20:34:07 -0400

    Cleanup the CUDA code. Don't forget to remove the CUDA context once we completed the finalization.

--- dplasma/cores/cuda_sgemm.c
+++ dplasma/cores/cuda_sgemm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010      The University of Tennessee and The University
+ * Copyright (c) 2010-2012 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  */
@@ -343,6 +343,10 @@ int sgemm_cuda_fini(dague_context_t* dague_context)
 #endif  /* !defined(DAGUE_GPU_STREAM_PER_TASK) */
         free(gpu_device->gpu_mem_lru); gpu_device->gpu_mem_lru = NULL;
         active_devices++;
+
+        status = (cudaError_t)cuCtxPopCurrent(NULL);
+        DAGUE_CUDA_CHECK_ERROR( "(INIT) cuCtxPopCurrent ", status,
+                                {continue;} );
     }
 
     gpu_data_map_fini(&gpu_data);
