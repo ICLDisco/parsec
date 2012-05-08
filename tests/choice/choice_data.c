049f7e6a7448ad9c48ee22362c84cf8db53004cb
Thomas Herault <herault@eecs.utk.edu>
2012-05-07 20:07:30 -0400

    Now that we use VPs, we need to define the VPID of the ad-hoc data distribution of examples
    --HG--
    branch : qr_lu

--- tests/choice/choice_data.c
+++ tests/choice/choice_data.c
@@ -36,6 +36,21 @@ static void *data_of(dague_ddesc_t *desc, ...)
     return (void*)(&dat->data[k]);
 } 
 
+static int vpid_of(dague_ddesc_t *desc, ...)
+{
+    int k;
+
+    va_list ap;
+
+    va_start(ap, desc);
+    k = va_arg(ap, int);
+    va_end(ap);
+
+    (void)k;
+
+    return 0;
+}
+
 dague_ddesc_t *create_and_distribute_data(int rank, int world, int cores, int size)
 {
     my_datatype_t *m = (my_datatype_t*)calloc(1, sizeof(my_datatype_t));
@@ -46,6 +61,7 @@ dague_ddesc_t *create_and_distribute_data(int rank, int world, int cores, int si
     d->nodes  = world;
     d->rank_of = rank_of;
     d->data_of = data_of;
+    d->vpid_of = vpid_of;
 
     m->data = (int32_t*)malloc(size * sizeof(int32_t));
 
