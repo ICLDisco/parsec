746b47ccf08897194ceebde4b07f17eafe4ff7cd
Julien Herrmann <julien.herrmann@ens-lyon.fr>
2012-05-14 15:35:55 -0400

    Add zgetrf_hpp2 : identical to zgetrf_hpp exept that the last panel is done with QR instead of LU with PP
    --HG--
    branch : qr_lu

---
+++ dplasma/lib/zgetrf_hpp2_wrapper.c
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2012      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+#include "dague.h"
+#include <plasma.h>
+#include "dplasma.h"
+#include "dplasma/lib/dplasmatypes.h"
+#include "dplasma/lib/dplasmaaux.h"
+#include "dplasma/lib/memory_pool.h"
+
+#include "zgetrf_hpp2.h"
+
+dague_object_t* dplasma_zgetrf_hpp2_New( qr_piv_t *qrpiv,
+                                            tiled_matrix_desc_t *A,
+                                            tiled_matrix_desc_t *IPIV,
+                                            tiled_matrix_desc_t *LT,
+                                            int* INFO )
+{
+    dague_zgetrf_hpp2_object_t* object;
+    int ib = LT->mb;
+
+    /*
+     * TODO: We consider ib is T->mb but can be incorrect for some tricks with GPU,
+     * it should be passed as a parameter as in getrf
+     */
+
+    object = dague_zgetrf_hpp2_new( *A,  (dague_ddesc_t*)A,
+                                   (dague_ddesc_t*)IPIV,
+                                   *LT, (dague_ddesc_t*)LT,
+                                   qrpiv, ib,
+                                   NULL, NULL,
+                                   INFO);
+
+    object->p_work = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
+    dague_private_memory_init( object->p_work, ib * LT->nb * sizeof(Dague_Complex64_t) );
+
+    object->p_tau = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
+    dague_private_memory_init( object->p_tau, LT->nb * sizeof(Dague_Complex64_t) );
+
+    /* Default type */
+    dplasma_add2arena_tile( object->arenas[DAGUE_zgetrf_hpp2_DEFAULT_ARENA],
+                            A->mb*A->nb*sizeof(Dague_Complex64_t),
+                            DAGUE_ARENA_ALIGNMENT_SSE,
+                            MPI_DOUBLE_COMPLEX, A->mb );
+
+    /* Upper triangular part of tile with diagonal */
+    dplasma_add2arena_upper( object->arenas[DAGUE_zgetrf_hpp2_UPPER_TILE_ARENA],
+                             A->mb*A->nb*sizeof(Dague_Complex64_t),
+                             DAGUE_ARENA_ALIGNMENT_SSE,
+                             MPI_DOUBLE_COMPLEX, A->mb, 1 );
+
+    /* IPIV */
+    dplasma_add2arena_rectangle( object->arenas[DAGUE_zgetrf_hpp2_PIVOT_ARENA],
+                                 A->mb*sizeof(int),
+                                 DAGUE_ARENA_ALIGNMENT_SSE,
+                                 MPI_INT, A->mb, 1, -1 );
+
+    /* Lower triangular part of tile without diagonal */
+    dplasma_add2arena_lower( object->arenas[DAGUE_zgetrf_hpp2_LOWER_TILE_ARENA],
+                             A->mb*A->nb*sizeof(Dague_Complex64_t),
+                             DAGUE_ARENA_ALIGNMENT_SSE,
+                             MPI_DOUBLE_COMPLEX, A->mb, 0 );
+
+    /* Little T */
+    dplasma_add2arena_rectangle( object->arenas[DAGUE_zgetrf_hpp2_LITTLE_T_ARENA],
+                                 LT->mb*LT->nb*sizeof(Dague_Complex64_t),
+                                 DAGUE_ARENA_ALIGNMENT_SSE,
+                                 MPI_DOUBLE_COMPLEX, LT->mb, LT->nb, -1);
+
+    return (dague_object_t*)object;
+}
+
+int dplasma_zgetrf_hpp2( dague_context_t *dague,
+                            qr_piv_t *qrpiv,
+                            tiled_matrix_desc_t *A,
+                            tiled_matrix_desc_t *IPIV,
+                            tiled_matrix_desc_t *LT,
+                            int* INFO )
+{
+    dague_object_t *dague_zgetrf_hpp2 = NULL;
+
+    dague_zgetrf_hpp2 = dplasma_zgetrf_hpp2_New(qrpiv, A, IPIV, LT, INFO);
+
+    dague_enqueue(dague, (dague_object_t*)dague_zgetrf_hpp2);
+    dplasma_progress(dague);
+
+    dplasma_zgetrf_hpp2_Destruct( dague_zgetrf_hpp2 );
+    return 0;
+}
+
+void
+dplasma_zgetrf_hpp2_Destruct( dague_object_t *o )
+{
+    dague_zgetrf_hpp2_object_t *dague_zgetrf_hpp2 = (dague_zgetrf_hpp2_object_t *)o;
+
+    dplasma_datatype_undefine_type( &(dague_zgetrf_hpp2->arenas[DAGUE_zgetrf_hpp2_DEFAULT_ARENA   ]->opaque_dtt) );
+    dplasma_datatype_undefine_type( &(dague_zgetrf_hpp2->arenas[DAGUE_zgetrf_hpp2_UPPER_TILE_ARENA]->opaque_dtt) );
+    dplasma_datatype_undefine_type( &(dague_zgetrf_hpp2->arenas[DAGUE_zgetrf_hpp2_PIVOT_ARENA     ]->opaque_dtt) );
+    dplasma_datatype_undefine_type( &(dague_zgetrf_hpp2->arenas[DAGUE_zgetrf_hpp2_LOWER_TILE_ARENA]->opaque_dtt) );
+    dplasma_datatype_undefine_type( &(dague_zgetrf_hpp2->arenas[DAGUE_zgetrf_hpp2_LITTLE_T_ARENA  ]->opaque_dtt) );
+
+    dague_private_memory_fini( dague_zgetrf_hpp2->p_work );
+    dague_private_memory_fini( dague_zgetrf_hpp2->p_tau  );
+
+    free( dague_zgetrf_hpp2->p_work );
+    free( dague_zgetrf_hpp2->p_tau  );
+
+    dague_zgetrf_hpp2_destroy(dague_zgetrf_hpp2);
+}
+
---
+++ dplasma/testing/testing_zgetrf_hpp2.c
@@ -0,0 +1,317 @@
+/*
+ * Copyright (c) 2009-2012 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+
+#include "common.h"
+#include "data_dist/matrix/two_dim_rectangle_cyclic.h"
+
+static int check_inverse( dague_context_t *dague, int loud,
+                          tiled_matrix_desc_t *ddescA,
+                          tiled_matrix_desc_t *ddescInvA,
+                          tiled_matrix_desc_t *ddescI );
+
+static int check_solution( dague_context_t *dague, int loud,
+                           tiled_matrix_desc_t *ddescA,
+                           tiled_matrix_desc_t *ddescB,
+                           tiled_matrix_desc_t *ddescX);
+
+
+int main(int argc, char ** argv)
+{
+    dague_context_t* dague;
+    int iparam[IPARAM_SIZEOF];
+    int info_sol = 0;
+    int info = 0, ret = 0;
+    qr_piv_t *qrpiv;
+
+    /* Set defaults for non argv iparams */
+    iparam_default_facto(iparam);
+    iparam_default_ibnbmb(iparam, 48, 144, 144);
+    iparam[IPARAM_LDA] = -'m';
+    iparam[IPARAM_LDB] = -'m';
+
+    /* Initialize DAGuE */
+    dague = setup_dague(argc, argv, iparam);
+    PASTE_CODE_IPARAM_LOCALS(iparam)
+    PASTE_CODE_FLOPS(FLOPS_ZGETRF, ((DagDouble_t)M,(DagDouble_t)N))
+
+    LDA = max(M, LDA);
+
+    int check_inv = check == 0 ?  0 : 0;
+
+
+	if ( M != N && check ) {
+        fprintf(stderr, "Check is impossible if M != N\n");
+        check = 0;
+	}
+
+    /* initializing matrix structure */
+    PASTE_CODE_ALLOCATE_MATRIX(ddescA, 1,
+        two_dim_block_cyclic, (&ddescA, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDA, N, 0, 0,
+                               M, N, SMB, SNB, P));
+    PASTE_CODE_ALLOCATE_MATRIX(ddescLT, 1,
+        two_dim_block_cyclic, (&ddescLT, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, IB, NB, MT*IB, N, 0, 0,
+                               MT*IB, N, SMB, SNB, P));
+    PASTE_CODE_ALLOCATE_MATRIX(ddescIPIV, 1,
+        two_dim_block_cyclic, (&ddescIPIV, matrix_Integer, matrix_Tile,
+                               nodes, cores, rank, MB, 1, M, NT, 0, 0,
+                               M, NT, SMB, SNB, P));
+    PASTE_CODE_ALLOCATE_MATRIX(ddescA0, check,
+        two_dim_block_cyclic, (&ddescA0, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDA, N, 0, 0,
+                               M, N, SMB, SNB, P));
+    PASTE_CODE_ALLOCATE_MATRIX(ddescB, check,
+        two_dim_block_cyclic, (&ddescB, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDB, NRHS, 0, 0,
+                               M, NRHS, SMB, SNB, P));
+    PASTE_CODE_ALLOCATE_MATRIX(ddescX, check,
+        two_dim_block_cyclic, (&ddescX, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDB, NRHS, 0, 0,
+                               M, NRHS, SMB, SNB, P));
+
+    PASTE_CODE_ALLOCATE_MATRIX(ddescInvA, check_inv,
+        two_dim_block_cyclic, (&ddescInvA, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDA, N, 0, 0,
+                               M, N, SMB, SNB, P));
+    PASTE_CODE_ALLOCATE_MATRIX(ddescI, check_inv,
+        two_dim_block_cyclic, (&ddescI, matrix_ComplexDouble, matrix_Tile,
+                               nodes, cores, rank, MB, NB, LDA, N, 0, 0,
+                               M, N, SMB, SNB, P));
+    
+
+    /* matrix generation */
+    if(loud > 2) printf("+++ Generate matrices ... ");
+
+//printf("\nGenerate matrices !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+
+    dplasma_zplrnt( dague, (tiled_matrix_desc_t *)&ddescA, 3872);
+    dplasma_zlaset( dague, PlasmaUpperLower, 0., 0., (tiled_matrix_desc_t *)&ddescLT);
+    qrpiv = dplasma_pivgen_init( (tiled_matrix_desc_t *)&ddescA,
+                                 iparam[IPARAM_LOWLVL_TREE], 
+                                 iparam[IPARAM_HIGHLVL_TREE],
+                                 iparam[IPARAM_QR_TS_SZE],
+                                 P,          /*iparam[IPARAM_QR_HLVL_SZE],*/
+                                 0,
+                                 0);
+	if ( check ) {
+        dplasma_zlacpy( dague, PlasmaUpperLower,
+                        (tiled_matrix_desc_t *)&ddescA,
+                        (tiled_matrix_desc_t *)&ddescA0 );
+		dplasma_zplrnt( dague, (tiled_matrix_desc_t *)&ddescB, 9867 );
+        dplasma_zlacpy( dague, PlasmaUpperLower,
+                        (tiled_matrix_desc_t *)&ddescB,
+                        (tiled_matrix_desc_t *)&ddescX );
+	}
+	if (check_inv != 0)
+	  {	
+                dplasma_zlaset( dague, PlasmaUpperLower, 0., 1., (tiled_matrix_desc_t *)&ddescI);
+		dplasma_zlaset( dague, PlasmaUpperLower, 0., 1., (tiled_matrix_desc_t *)&ddescInvA);
+	  }
+   
+    if(loud > 2) printf("Done\n");
+
+    /* Create DAGuE */
+    if(loud > 2) printf("+++ Computing getrf_hpp2 ... ");
+
+//printf("\n+++ Computing getrf_hpp2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+
+    PASTE_CODE_ENQUEUE_KERNEL(dague, zgetrf_hpp2,
+                              (qrpiv,
+                               (tiled_matrix_desc_t*)&ddescA,
+                               (tiled_matrix_desc_t*)&ddescIPIV,
+                               (tiled_matrix_desc_t*)&ddescLT,
+                               &info));
+
+    /* lets rock! */
+    PASTE_CODE_PROGRESS_KERNEL(dague, zgetrf_hpp2);
+    dplasma_zgetrf_hpp2_Destruct( DAGUE_zgetrf_hpp2 );
+
+//printf("\nDone!!!!!!!!!!!!!!!!!!!!!!! \n");
+
+    if ( info != 0 ) {
+        if( rank == 0 && loud ) printf("-- Factorization is suspicious (info = %d) ! \n", info );
+        ret |= 1;
+    }
+    else if( check ) {
+        /*
+         * First check with a right hand side
+         */
+        //printf("\nFirst check with a right hand side.\n");
+        dplasma_ztrsmpl_hpp2( dague, qrpiv,
+                              (tiled_matrix_desc_t *)&ddescA,
+                              (tiled_matrix_desc_t *)&ddescX,
+                              (tiled_matrix_desc_t *)&ddescIPIV,
+                              (tiled_matrix_desc_t *)&ddescLT,
+                              &info);
+        dplasma_ztrsm(dague, PlasmaLeft, PlasmaUpper, PlasmaNoTrans, PlasmaNonUnit, 1.0,
+                      (tiled_matrix_desc_t *)&ddescA,
+                      (tiled_matrix_desc_t *)&ddescX);
+
+        /* Check the solution */
+        ret |= check_solution(dague, (rank == 0) ? loud : 0,
+                              (tiled_matrix_desc_t *)&ddescA0,
+                              (tiled_matrix_desc_t *)&ddescB,
+                              (tiled_matrix_desc_t *)&ddescX);
+
+        /*
+         * Second check with inverse
+         */
+        //printf("\nSecond check with inverse (right hand side is Identity).\n");
+        if (check_inv != 0)
+	  {
+        dplasma_ztrsmpl_hpp2( dague, qrpiv,
+                              (tiled_matrix_desc_t *)&ddescA,
+                              (tiled_matrix_desc_t *)&ddescInvA,
+                              (tiled_matrix_desc_t *)&ddescIPIV,
+                              (tiled_matrix_desc_t *)&ddescLT,
+                              &info);
+        dplasma_ztrsm(dague, PlasmaLeft, PlasmaUpper, PlasmaNoTrans, PlasmaNonUnit, 1.0,
+                      (tiled_matrix_desc_t *)&ddescA,
+                      (tiled_matrix_desc_t *)&ddescInvA);
+
+        /* Check the solution */
+        ret |= check_inverse(dague, (rank == 0) ? loud : 0,
+                             (tiled_matrix_desc_t *)&ddescA0,
+                             (tiled_matrix_desc_t *)&ddescInvA,
+                             (tiled_matrix_desc_t *)&ddescI);
+	  }
+    }
+
+    dplasma_pivgen_finalize( qrpiv );
+
+    cleanup_dague(dague, iparam);
+
+//printf("\nMaintenant dlata_free A!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+    dague_data_free(ddescA.mat);
+//printf("\nMaintenant data_free LT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+    dague_data_free(ddescLT.mat);
+//printf("\nMaintenant data_free IPIV!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+    dague_data_free(ddescIPIV.mat);
+    
+
+//printf("\nMaintenant destroy!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
+    dague_ddesc_destroy((dague_ddesc_t*)&ddescA);
+    dague_ddesc_destroy((dague_ddesc_t*)&ddescLT);
+    dague_ddesc_destroy((dague_ddesc_t*)&ddescIPIV);
+    
+    if ( check ) {
+        dague_data_free(ddescA0.mat);
+        if (check_inv != 0)
+	  {
+        dague_data_free(ddescInvA.mat);
+        dague_data_free(ddescI.mat);
+	  }
+        dague_data_free(ddescB.mat);
+        dague_data_free(ddescX.mat);
+    
+        dague_ddesc_destroy((dague_ddesc_t*)&ddescA0);
+        dague_ddesc_destroy((dague_ddesc_t*)&ddescB);
+        dague_ddesc_destroy((dague_ddesc_t*)&ddescX);
+        if (check_inv != 0)
+	  {
+        dague_ddesc_destroy((dague_ddesc_t*)&ddescI);
+        dague_ddesc_destroy((dague_ddesc_t*)&ddescInvA);
+	  }
+
+    } 
+    return info_sol;
+}
+
+
+static int check_solution( dague_context_t *dague, int loud,
+                           tiled_matrix_desc_t *ddescA,
+                           tiled_matrix_desc_t *ddescB,
+                           tiled_matrix_desc_t *ddescX )
+{
+    int info_solution;
+    double Rnorm = 0.0;
+    double Anorm = 0.0;
+    double Bnorm = 0.0;
+    double Xnorm, result;
+    int m = ddescB->m;
+    double eps = LAPACKE_dlamch_work('e');
+
+    Anorm = dplasma_zlange(dague, PlasmaInfNorm, ddescA);
+    Bnorm = dplasma_zlange(dague, PlasmaInfNorm, ddescB);
+    Xnorm = dplasma_zlange(dague, PlasmaInfNorm, ddescX);
+
+    /* Compute b - A*x */
+    dplasma_zgemm( dague, PlasmaNoTrans, PlasmaNoTrans, -1.0, ddescA, ddescX, 1.0, ddescB);
+
+    Rnorm = dplasma_zlange(dague, PlasmaInfNorm, ddescB);
+
+    result = Rnorm / ( ( Anorm * Xnorm + Bnorm ) * m * eps ) ;
+
+    // if ( loud > 2 ) {
+        //printf("============\n");
+        //printf("Checking the Residual of the solution \n");
+        /*if ( loud > 3 )
+            printf( "-- ||A||_oo = %e, ||X||_oo = %e, ||B||_oo= %e, ||A X - B||_oo = %e\n",
+                    Anorm, Xnorm, Bnorm, Rnorm );*/
+
+    if( loud ) printf("-- ||Ax-B||_oo/((||A||_oo||x||_oo+||B||_oo).N.eps) = %e \n", result);
+    // }
+
+    if (  isnan(Xnorm) || isinf(Xnorm) || isnan(result) || isinf(result) || (result > 60.0) ) {
+        if( loud ) printf("-- Solution is suspicious ! \n");
+        info_solution = 1;
+    }
+    else{
+        if( loud ) printf("-- Solution is CORRECT ! \n");
+        info_solution = 0;
+    }
+
+    return info_solution;
+}
+
+static int check_inverse( dague_context_t *dague, int loud,
+                          tiled_matrix_desc_t *ddescA,
+                          tiled_matrix_desc_t *ddescInvA,
+                          tiled_matrix_desc_t *ddescI )
+{
+    int info_solution;
+    double Anorm    = 0.0;
+    double InvAnorm = 0.0;
+    double Rnorm, result;
+    int m = ddescA->m;
+    double eps = LAPACKE_dlamch_work('e');
+
+    Anorm    = dplasma_zlange(dague, PlasmaInfNorm, ddescA   );
+    InvAnorm = dplasma_zlange(dague, PlasmaInfNorm, ddescInvA);
+
+    /* Compute I - A*A^{-1} */
+    dplasma_zgemm( dague, PlasmaNoTrans, PlasmaNoTrans, -1.0, ddescA, ddescInvA, 1.0, ddescI);
+
+    Rnorm = dplasma_zlange(dague, PlasmaInfNorm, ddescI);
+
+    result = Rnorm / ( ( Anorm * InvAnorm ) * m * eps ) ;
+
+    // if ( loud > 2 ) {
+        //printf("============\n");
+       // printf("Checking the Residual of the solution \n");
+        /*if ( loud > 3 )
+            printf( "-- ||A||_oo = %e, ||A^{-1}||_oo = %e, ||A A^{-1} - I||_oo = %e\n",
+                    Anorm, InvAnorm, Rnorm );*/
+
+    if( loud) printf("-- ||AA^{-1}-I||_oo/((||A||_oo||A^{-1}||_oo).N.eps) = %e \n", result);
+    // }
+
+    if (  isnan(Rnorm) || isinf(Rnorm) || isnan(result) || isinf(result) || (result > 60.0) ) {
+        if( loud ) printf("-- Solution is suspicious ! \n");
+        info_solution = 1;
+    }
+    else{
+        if( loud ) printf("-- Solution is CORRECT ! \n");
+        info_solution = 0;
+    }
+
+    return info_solution;
+}
+	
