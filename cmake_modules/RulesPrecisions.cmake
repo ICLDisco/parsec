0e76f06a6689f4afb357af919120c4457ae3ba94
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-06-13 10:53:57 -0400

    Replace the MAGMA_COMPILE_INPLACE

--- cmake_modules/RulesPrecisions.cmake
+++ cmake_modules/RulesPrecisions.cmake
@@ -72,7 +72,7 @@ macro(precisions_rules_py)
 
       # Force the copy of the original files in the binary_dir
       # for VPATH compilation
-      if( NOT MAGMA_COMPILE_INPLACE )
+      if( NOT DAGUE_COMPILE_INPLACE )
         set(generate_out 1)
       else( NOT DAGUE_COMPILE_INPLACE )
         string(COMPARE NOTEQUAL "${prec_rules_OSRC}" "${prec_rules_SOURCE}" generate_out )
