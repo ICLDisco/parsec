05ba57a702c36fbafb1945a577f8169090090980
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-07-02 23:07:13 -0400

    Fix data distribution with super tile in norms (may cause pb in specific case. Will need to be checked correctly for ST

--- dplasma/lib/zlange_wrapper.c
+++ dplasma/lib/zlange_wrapper.c
@@ -26,13 +26,22 @@ dague_object_t* dplasma_zlange_New( PLASMA_enum ntype,
                                     tiled_matrix_desc_t *A,
                                     double *result )
 {
-    int m, n, mb, nb, elt;
+    int m, n, mb, nb, smb, snb, elt;
     two_dim_block_cyclic_t *W;
     dague_object_t *dague_zlange = NULL;
 
     /* Create the workspace */
     W = (two_dim_block_cyclic_t*)malloc(sizeof(two_dim_block_cyclic_t));
 
+    if ( A->dtype & two_dim_block_cyclic_type ) {
+        smb = ((two_dim_block_cyclic_t*)A)->grid.strows;
+        snb = ((two_dim_block_cyclic_t*)A)->grid.stcols;
+    } else {
+        smb = 1;
+        snb = 1;
+    }
+
+    /* Warning: Pb with smb/snb when mt/nt lower than P/Q */
     switch( ntype ) {
     case PlasmaFrobeniusNorm:
         mb = 2;
@@ -40,6 +49,7 @@ dague_object_t* dplasma_zlange_New( PLASMA_enum ntype,
         m  = dague_imax(A->mt, P) * mb;
         n  = Q;
         elt = 2;
+        snb = 1;
         break;
     case PlasmaInfNorm:
         mb = A->mb;
@@ -47,6 +57,7 @@ dague_object_t* dplasma_zlange_New( PLASMA_enum ntype,
         m  = dague_imax(A->mt, P) * mb;
         n  = Q;
         elt = 1;
+        snb = 1;
         break;
     case PlasmaOneNorm:
         mb = 1;
@@ -54,6 +65,7 @@ dague_object_t* dplasma_zlange_New( PLASMA_enum ntype,
         m  = P;
         n  = dague_imax(A->nt, Q) * nb;
         elt = 1;
+        smb = 1;
         break;
     case PlasmaMaxNorm:
     default:
@@ -62,17 +74,18 @@ dague_object_t* dplasma_zlange_New( PLASMA_enum ntype,
         m  = dague_imax(A->mt, P);
         n  = Q;
         elt = 1;
+        snb = 1;
     }
 
     PASTE_CODE_INIT_AND_ALLOCATE_MATRIX(
         (*W), two_dim_block_cyclic,
         (W, matrix_RealDouble, matrix_Tile,
          A->super.nodes, A->super.cores, A->super.myrank,
-         mb, nb,   /* Dimesions of the tile                */
-         m,  n,    /* Dimensions of the matrix             */
-         0,  0,    /* Starting points (not important here) */
-         m,  n,    /* Dimensions of the submatrix          */
-         1, 1, P));
+         mb,  nb,   /* Dimesions of the tile                */
+         m,   n,    /* Dimensions of the matrix             */
+         0,   0,    /* Starting points (not important here) */
+         m,   n,    /* Dimensions of the submatrix          */
+         smb, snb, P));
 
     /* Create the DAG */
     switch( ntype ) {
