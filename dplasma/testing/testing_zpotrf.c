d18d7c2fca8adfb324f78b732819dbc0d917d7df
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2012-08-01 18:42:28 -0400

    Georges'  fix works wonder, removing the now useless debug printfs

--- dplasma/testing/testing_zpotrf.c
+++ dplasma/testing/testing_zpotrf.c
@@ -62,19 +62,17 @@ int main(int argc, char ** argv)
 
     /* load the GPU kernel */
 #if defined(HAVE_CUDA)
-    if(iparam[IPARAM_NGPUS] > 0)
-        {
-            if(loud > 3) printf("+++ Load GPU kernel ... ");
-            if(0 != gpu_kernel_init_zgemm(dague))
-                {
-                    printf("XXX Unable to load GPU kernel.\n");
-                    exit(3);
-                }
-            dague_gpu_data_register(dague,
-                                    (dague_ddesc_t*)&ddescA,
-                                    MT*NT, MB*NB*sizeof(dague_complex64_t) );
-            if(loud > 3) printf("Done\n");
+    if(iparam[IPARAM_NGPUS] > 0) {
+        if(loud > 3) printf("+++ Load GPU kernel ... ");
+        if(0 != gpu_kernel_init_zgemm(dague)) {
+            printf("XXX Unable to load GPU kernel.\n");
+            exit(3);
         }
+        dague_gpu_data_register(dague,
+                                (dague_ddesc_t*)&ddescA,
+                                MT*NT, MB*NB*sizeof(dague_complex64_t) );
+        if(loud > 3) printf("Done\n");
+    }
 #endif
 
     PASTE_CODE_ENQUEUE_KERNEL(dague, zpotrf,
@@ -105,6 +103,7 @@ int main(int argc, char ** argv)
         ret |= check_factorization( dague, (rank == 0) ? loud : 0, uplo,
                                     (tiled_matrix_desc_t *)&ddescA,
                                     (tiled_matrix_desc_t *)&ddescA0);
+
         /* Check the solution */
         PASTE_CODE_ALLOCATE_MATRIX(ddescB, check,
             two_dim_block_cyclic, (&ddescB, matrix_ComplexDouble, matrix_Tile,
@@ -158,13 +157,11 @@ static int check_factorization( dague_context_t *dague, int loud, PLASMA_enum up
     double eps = LAPACKE_dlamch_work('e');
     PLASMA_enum side;
 
-fprintf(stderr, "D%d\n", twodA->grid.rank);
     PASTE_CODE_ALLOCATE_MATRIX(L1, 1,
         sym_two_dim_block_cyclic, (&L1, matrix_ComplexDouble,
                                    A->super.nodes, A->super.cores, twodA->grid.rank,
                                    A->mb, A->nb, M, N, 0, 0,
                                    M, N, twodA->grid.rows, uplo));
-fprintf(stderr, "E%d\n", twodA->grid.rank);
     PASTE_CODE_ALLOCATE_MATRIX(L2, 1,
         two_dim_block_cyclic, (&L2, matrix_ComplexDouble, matrix_Tile,
                                A->super.nodes, A->super.cores, twodA->grid.rank,
@@ -274,3 +271,4 @@ static int check_solution( dague_context_t *dague, int loud, PLASMA_enum uplo,
 
     return info_solution;
 }
+
