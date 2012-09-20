89be6f4403367c6b6e1acea18c67314e105fed8f
Julien Herrmann <julien.herrmann@ens-lyon.fr>
2012-09-20 11:58:12 +0200

    change the CMakeLists
    --HG--
    branch : qr_lu

--- dplasma/include/dplasma_z.h
+++ dplasma/include/dplasma_z.h
@@ -189,22 +189,22 @@ int dplasma_zgetrf_hincpiv( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matri
 int dplasma_zgetrf_hpp( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 int dplasma_zgetrf_hpp2( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, tiled_matrix_desc_t *LT2, int* INFO);
 int dplasma_zgetrf_hpp_multithrd( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
-int dplasma_zgetrf_qrf( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
+int dplasma_zgetrf_qrf( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *SAV, tiled_matrix_desc_t *LT, int* lu_tab, int* INFO);
 void dplasma_ztrsmpl_hincpiv( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 int dplasma_ztrsmpl_hpp( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 int dplasma_ztrsmpl_hpp2( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, tiled_matrix_desc_t *LT2, int* INFO);
-int dplasma_ztrsmpl_qrf( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
+int dplasma_ztrsmpl_qrf( dague_context_t *dague, qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *SAV, tiled_matrix_desc_t *LT, int* lu_tab, int* INFO);
 
 dague_object_t* dplasma_zgetrf_hincpiv_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 dague_object_t* dplasma_zgetrf_hpp_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 dague_object_t* dplasma_zgetrf_hpp2_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, tiled_matrix_desc_t *LT2, int* INFO);
 dague_object_t* dplasma_zgetrf_hpp_multithrd_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
-dague_object_t* dplasma_zgetrf_qrf_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
+dague_object_t* dplasma_zgetrf_qrf_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *SAV, tiled_matrix_desc_t *LT, int* lu_tab, int* INFO);
 
 dague_object_t* dplasma_ztrsmpl_hincpiv_New( qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 dague_object_t* dplasma_ztrsmpl_hpp_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
 dague_object_t* dplasma_ztrsmpl_hpp2_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, tiled_matrix_desc_t *LT2, int* INFO);
-dague_object_t* dplasma_ztrsmpl_qrf_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *LT, int* INFO);
+dague_object_t* dplasma_ztrsmpl_qrf_New(qr_piv_t *qrpiv, tiled_matrix_desc_t *A, tiled_matrix_desc_t *B, tiled_matrix_desc_t *IPIV, tiled_matrix_desc_t *SAV, tiled_matrix_desc_t *LT, int* lu_tab, int* INFO);
 
 void dplasma_zgetrf_hincpiv_Destruct( dague_object_t *o );
 void dplasma_zgetrf_hpp_Destruct( dague_object_t *o );
