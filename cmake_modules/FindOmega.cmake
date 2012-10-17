7c8e934dfe88d7dfe2233b1f5652085ab3137656
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2012-10-17 14:08:09 -0400

    Fix omega detection system to work in all cases, when omega is installed or from source code

--- cmake_modules/FindOmega.cmake
+++ cmake_modules/FindOmega.cmake
@@ -12,62 +12,54 @@
 # This module sets the following variables:
 #  OMEGA_FOUND - set to true if a library implementing the interface
 #    is found
-#  OMEGA_LINKER_FLAGS - uncached list of required linker flags (excluding -l
-#    and -L).
 #  OMEGA_LIBRARIES - uncached list of libraries (using full path name) to
 #    link against to use OMEGA
-#  OMEGA_INCLUDE_DIR - directory where the OMEGA header files are
+#  OMEGA_INCLUDE_DIRS - uncached directories where the OMEGA header files are
 #
 ##########
 
+mark_as_advanced(OMEGA_INCLUDE_DIR OMEGA_SRC_INCLUDE_DIR OMEGA_DIR OMEGA_LIBRARY)
 
-if( OMEGA_DIR )
-  if( NOT OMEGA_INCLUDE_DIR )
-    set(OMEGA_INCLUDE_DIR ${OMEGA_DIR}/omega_lib/include ${OMEGA_DIR}/basic/include)
-  endif( NOT OMEGA_INCLUDE_DIR )
-  if( NOT OMEGA_LIBRARIES )
-    set(OMEGA_LIBRARIES "${OMEGA_DIR}/omega_lib/obj")
-  endif( NOT OMEGA_LIBRARIES )
-endif( OMEGA_DIR )
-
-if( NOT OMEGA_INCLUDE_DIR )
-  set(OMEGA_INCLUDE_DIR)
-endif( NOT OMEGA_INCLUDE_DIR )
-if( NOT OMEGA_LIBRARIES )
-  set(OMEGA_LIBRARIES)
-endif( NOT OMEGA_LIBRARIES )
-if( NOT OMEGA_LINKER_FLAGS )
-  set(OMEGA_LINKER_FLAGS)
-endif( NOT OMEGA_LINKER_FLAGS )
+find_path(OMEGA_INCLUDE_DIR omega.h PATHS "${OMEGA_DIR}" PATH_SUFFIXES include/omega omega_lib/include) 
+find_path(OMEGA_SRC_INCLUDE_DIR basic/bool.h PATHS "${OMEGA_DIR}" PATH_SUFFIXES basic/include) 
+if(OMEGA_SRC_INCLUDE_DIR) 
+  set(OMEGA_INCLUDE_DIRS ${OMEGA_INCLUDE_DIR} ${OMEGA_SRC_INCLUDE_DIR})
+else()
+  set(OMEGA_INCLUDE_DIRS ${OMEGA_INCLUDE_DIR})
+endif()
 
 find_library(OMEGA_LIBRARY omega
-            PATHS "${OMEGA_LIBRARIES}"
-            DOC "Where the Omega  libraries are")
-set(OMEGA_LIBRARIES "-L${OMEGA_LIBRARIES} -lomega")
+             PATHS "${OMEGA_DIR}"
+             PATH_SUFFIXES lib omega_lib/obj
+             DOC "Where the Omega  libraries are")
+set(OMEGA_LIBRARIES ${OMEGA_LIBRARY})
 
 include(CheckCXXSourceCompiles)
-list(APPEND CMAKE_REQUIRED_INCLUDES ${OMEGA_INCLUDE_DIR})
+list(APPEND CMAKE_REQUIRED_INCLUDES ${OMEGA_INCLUDE_DIRS})
 list(APPEND CMAKE_REQUIRED_LIBRARIES ${OMEGA_LIBRARIES})
 check_cxx_source_compiles("#include <omega.h>
-int main(void) { Relation R; R.is_set(); return 0;}" OMEGA_FOUND)
-
+    int main(void) { Relation R; R.is_set(); return 0;}" OMEGA_FOUND)
 
 if(OMEGA_FOUND)
   if(NOT OMEGA_FIND_QUIETLY)
-    message(STATUS "A library with OMEGA API found: ${OMEGA_LIBRARIES}.")
+    message(STATUS "A library with OMEGA API found: [${OMEGA_LIBRARIES}], [${OMEGA_INCLUDE_DIRS}].")
   endif(NOT OMEGA_FIND_QUIETLY)
   set(HAVE_OMEGA 1)
 else(OMEGA_FOUND)
   if(OMEGA_FIND_REQUIRED)
     message(FATAL_ERROR
-        "An required library with OMEGA API not found.\n    Please specify library location "
-        "using OMEGA_DIR or a combination of OMEGA_INCLUDE_DIR and OMEGA_LIBRARIES.\n")
+        "An required library with OMEGA API not found.\n"
+        "    You need to specify the library location\n"
+        "    - using OMEGA_DIR [${OMEGA_DIR}]\n"
+        "    - or a combination of OMEGA_INCLUDE_DIR [${OMEGA_INCLUDE_DIR}] and OMEGA_LIBRARY [${OMEGA_LIBRARY}]\n")
   else(OMEGA_FIND_REQUIRED)
     if(NOT OMEGA_FIND_QUIETLY)
       message(STATUS
-        "An optional library with OMEGA API not found.\n    Please specify library location "
-        "using OMEGA_DIR or a combination of OMEGA_INCLUDE_DIR and OMEGA_LIBRARIES.\n"
-        "    Options depending on Omega will be disabled.")
+        "An optional library with OMEGA API not found.\n"
+        "    To prevent options depending on Omega from being disabled, please specify the library location\n"
+        "    - using OMEGA_DIR [${OMEGA_DIR}]\n"
+        "    - or a combination of OMEGA_INCLUDE_DIR [${OMEGA_INCLUDE_DIR}] and OMEGA_LIBRARY [${OMEGA_LIBRARY}]\n")
     endif(NOT OMEGA_FIND_QUIETLY)
   endif(OMEGA_FIND_REQUIRED)
 endif(OMEGA_FOUND)
+
