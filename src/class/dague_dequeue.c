5cb1e14cc9761e1c68e606abcde046534edcdda2
George Bosilca <bosilca@eecs.utk.edu>
2013-01-09 00:17:54 +0100

    Add the class initialized for some of the most basic objects.
    --HG--
    branch : all-about-data

---
+++ src/class/dague_dequeue.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2013      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+#include <dague_config.h>
+#include "dequeue.h"
+
+OBJ_CLASS_INSTANCE(dague_dequeue_t, dague_list_t, 
+                   NULL, NULL);
+
