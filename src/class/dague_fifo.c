273c56ac440789c7762e117311d60a01cbf19ee6
George Bosilca <bosilca@eecs.utk.edu>
2013-01-10 17:55:32 -0500

    Update the FIFO to become an object.
    --HG--
    branch : all-about-data

---
+++ src/class/dague_fifo.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2013      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+#include <dague_config.h>
+#include "fifo.h"
+
+OBJ_CLASS_INSTANCE(dague_fifo_t, dague_list_t,
+                   NULL, NULL);
