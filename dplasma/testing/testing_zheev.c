2aa2b5621423877e7946d6e74702f748bda6bdf7
Aurelien Bouteiller <bouteill@eecs.utk.edu>
2013-01-23 18:46:33 -0500

    Set some sensible defaults
    --HG--
    branch : bulgechasing

--- dplasma/testing/testing_zheev.c
+++ dplasma/testing/testing_zheev.c
@@ -32,7 +32,8 @@ int main(int argc, char *argv[])
      /* Set defaults for non argv iparams */
     iparam_default_facto(iparam);
     //iparam_default_ibnbmb(iparam, 48, 144, 144);
-    iparam_default_ibnbmb(iparam, 4, 4, 4);
+    iparam_default_ibnbmb(iparam, 40, 120, 120);
+    iparam[IPARAM_SNB] = 1;
 #if defined(HAVE_CUDA) && defined(PRECISION_s)
     iparam[IPARAM_NGPUS] = 0;
 #endif
