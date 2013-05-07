1d72ad44b36ee2964a9557f985335936e1d428d4
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-05-07 22:54:14 +0200

    Add zamax on descriptor
    --HG--
    branch : qr_lu

--- dplasma/cores/core_zamax.c
+++ dplasma/cores/core_zamax.c
@@ -23,6 +23,40 @@
 
 #define COMPLEX
 
+#ifdef BLKLDD
+#undef BLKLDD
+#define BLKLDD(A, k) ( ( (k) + (A).i/(A).mb) < (A).lm1 ? (A).mb : (A).lm%(A).mb )
+#endif
+
+/***************************************************************************//**
+ *  Internal function to return adress of block (m,n)
+ **/
+inline static void *plasma_getaddr(PLASMA_desc A, int m, int n)
+{
+    int mm = m+A.i/A.mb;
+    int nn = n+A.j/A.nb;
+    size_t eltsize = sizeof( PLASMA_Complex64_t );
+    size_t offset = 0;
+
+    if (mm < A.lm1) {
+        if (nn < A.ln1)
+            offset = A.bsiz*(mm+A.lm1*nn);
+        else
+            offset = A.A12 + (A.mb*(A.ln%A.nb)*mm);
+    }
+    else {
+        if (nn < A.ln1)
+            offset = A.A21 + ((A.lm%A.mb)*A.nb*nn);
+        else
+            offset = A.A22;
+    }
+
+    return (void*)((intptr_t)A.mat + (offset*eltsize) );
+}
+
+#define BLKADDR(A, type, m, n)  (type *)plasma_getaddr(A, m, n)
+#define A(m) BLKADDR(descA, PLASMA_Complex64_t, m, 0)
+
 /***************************************************************************//**
  *
  * @ingroup CORE_PLASMA_Complex64_t
@@ -33,7 +67,7 @@
 #define CORE_dzamax PCORE_dzamax
 #endif
 int CORE_zamax(PLASMA_enum storev, PLASMA_enum uplo, int M, int N,
-                 const PLASMA_Complex64_t *A, int lda, double *work)
+               const PLASMA_Complex64_t *A, int lda, double *work)
 {
     const PLASMA_Complex64_t *tmpA;
     double *tmpW, max, abs;
@@ -46,11 +80,12 @@ int CORE_zamax(PLASMA_enum storev, PLASMA_enum uplo, int M, int N,
             max = 0.0;
             for (i = 0; i < j; i++) {
                 abs      = cabs(*tmpA);
-                max      = abs < max ? max : abs;
-                work[i]  = abs < max ? max : abs;
+                max      = abs < max     ? max     : abs;
+                work[i]  = abs < work[i] ? work[i] : abs;
                 tmpA++;
             }
-            work[j] = max < cabs(*tmpA) ? cabs(*tmpA) : max;
+            work[j] = work[j] < max         ? max         : work[j];
+            work[j] = work[j] < cabs(*tmpA) ? cabs(*tmpA) : work[j];
         }
         break;
     case PlasmaLower:
@@ -63,11 +98,11 @@ int CORE_zamax(PLASMA_enum storev, PLASMA_enum uplo, int M, int N,
             tmpA++;
             for (i = j+1; i < M; i++) {
                 abs      = cabs(*tmpA);
-                max      = abs < max ? max : abs;
-                work[i]  = abs < max ? max : abs;
+                max      = abs < max     ? max     : abs;
+                work[i]  = abs < work[i] ? work[i] : abs;
                 tmpA++;
             }
-            work[j] = max < work[j] ? work[j] : max;
+            work[j] = work[j] < max ? max : work[j];
         }
         break;
     case PlasmaUpperLower:
@@ -76,7 +111,8 @@ int CORE_zamax(PLASMA_enum storev, PLASMA_enum uplo, int M, int N,
             for (j = 0; j < N; j++) {
                 tmpA = A+(j*lda);
                 for (i = 0; i < M; i++) {
-                    work[j] = work[j] < cabs(*tmpA) ? cabs(*tmpA) : work[j];
+                    abs = cabs(*tmpA);
+                    work[j] = work[j] < abs ? abs : work[j];
                     tmpA++;
                 }
             }
@@ -86,11 +122,61 @@ int CORE_zamax(PLASMA_enum storev, PLASMA_enum uplo, int M, int N,
                 tmpA = A+(j*lda);
                 tmpW = work;
                 for (i = 0; i < M; i++) {
-                    *tmpW = *tmpW < cabs(*tmpA) ? cabs(*tmpA) : *tmpW;
+                    abs = cabs(*tmpA);
+                    *tmpW = *tmpW < abs ? abs : *tmpW;
                     tmpA++; tmpW++;
                 }
             }
         }
     }
+    return PLASMA_SUCCESS;
+}
+
+
+#if defined(PLASMA_HAVE_WEAK)
+#pragma weak CORE_dzamax_tile = PCORE_dzamax_tile
+#define CORE_dzamax_tile PCORE_dzamax_tile
+#endif
+int CORE_zamax_tile( PLASMA_enum storev,
+                     PLASMA_enum uplo,
+                     const PLASMA_desc descA,
+                     double *work)
+{
+    PLASMA_Complex64_t *A;
+    int m, lda, tempmm;
+
+    /* Check input arguments */
+    if ((storev != PlasmaColumnwise) && (storev != PlasmaRowwise)) {
+        coreblas_error(1, "Illegal value of storev");
+        return -1;
+    }
+    if ((uplo != PlasmaLower) && (uplo != PlasmaUpper) &&
+        (uplo != PlasmaUpperLower))
+    {
+        coreblas_error(2, "Illegal value of uplo");
+        return -2;
+    }
+    if (storev != PlasmaColumnwise) {
+        coreblas_error(1, "Illegal value of storev (Not supported)");
+        return -1;
+    }
+    if (uplo != PlasmaUpperLower)
+    {
+        coreblas_error(2, "Illegal value of uplo (Not supported)");
+        return -2;
+    }
+
+    for( m = 0; m < descA.mt; m++)
+    {
+        lda    = BLKLDD( descA, m );
+        A      = A( m );
+        tempmm = (m == (descA.mt-1)) ? (descA.m - m * descA.mb) : descA.mb;
+
+        /* Apply the GEMM */
+        CORE_zamax( PlasmaColumnwise, PlasmaUpperLower,
+                    tempmm, descA.n,
+                    A, lda, work );
+    }
+
     return PLASMA_SUCCESS;
 }
