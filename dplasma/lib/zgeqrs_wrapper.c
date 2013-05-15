7d78436b2052f96a37d4bb2d20bbe4a50f0bf7fc
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-05-15 22:36:12 +0200

    Add wrappers for QR solve

---
+++ dplasma/lib/zgeqrs_wrapper.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2010-2012 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+#include "dague_internal.h"
+#include <plasma.h>
+#include "dplasma.h"
+
+int
+dplasma_zgeqrs(dague_context_t *dague,
+               tiled_matrix_desc_t* A,
+               tiled_matrix_desc_t* T,
+               tiled_matrix_desc_t* B)
+{
+
+    dplasma_zunmqr( dague, PlasmaLeft, PlasmaConjTrans, A, T, B );
+    dplasma_ztrsm( dague, PlasmaLeft, PlasmaUpper, PlasmaNoTrans, PlasmaNonUnit, 1.0, A, B );
+
+    return 0;
+}
+
+
