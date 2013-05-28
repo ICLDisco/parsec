e3bfe26ca6b39f2e23537d8b763cb8dfb5270d86
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-05-28 15:19:39 +0200

    Add House matrix generator
    --HG--
    branch : qr_lu

--- dplasma/lib/zplrnt_perso_wrapper.c
+++ dplasma/lib/zplrnt_perso_wrapper.c
@@ -8,6 +8,7 @@
  */
 #include "dague_internal.h"
 #include <plasma.h>
+#include "data_dist/matrix/two_dim_rectangle_cyclic.h"
 #include "dplasma.h"
 #include "dplasma/lib/dplasmatypes.h"
 #include "dplasma/lib/dplasmaaux.h"
@@ -53,12 +54,55 @@ int dplasma_zplrnt_perso( dague_context_t *dague,
 {
     dague_object_t *dague_zplrnt_perso = NULL;
 
-    dague_zplrnt_perso = dplasma_zplrnt_perso_New( A, type, seed );
+    switch( type ) {
+    case MATRIX_HOUSE:
+    {
+        two_dim_block_cyclic_t *twodA = (two_dim_block_cyclic_t *)A;
+        two_dim_block_cyclic_t V, T;
+        tiled_matrix_desc_t *subA;
+        two_dim_block_cyclic_init( &V, matrix_ComplexDouble, matrix_Tile,
+                                   A->super.nodes, A->super.cores, A->super.myrank,
+                                   A->mb, A->nb, A->m, 1, 0, 0, A->m, 1,
+                                   twodA->grid.strows, twodA->grid.stcols, twodA->grid.rows );
+        V.mat = dague_data_allocate((size_t)V.super.nb_local_tiles *
+                                    (size_t)V.super.bsiz *
+                                    (size_t)dague_datadist_getsizeoftype(V.super.mtype));
+        dague_ddesc_set_key((dague_ddesc_t*)&V, "V");
 
-    dague_enqueue(dague, (dague_object_t*)dague_zplrnt_perso);
-    dplasma_progress(dague);
+        two_dim_block_cyclic_init( &T, matrix_ComplexDouble, matrix_Tile,
+                                   A->super.nodes, A->super.cores, A->super.myrank,
+                                   32, A->nb, ( A->m + 31 ) / 32, 1, 0, 0, ( A->m + 31 ) / 32, 1,
+                                   twodA->grid.strows, twodA->grid.stcols, twodA->grid.rows );
+        T.mat = dague_data_allocate((size_t)T.super.nb_local_tiles *
+                                    (size_t)T.super.bsiz *
+                                    (size_t)dague_datadist_getsizeoftype(T.super.mtype));
+        dague_ddesc_set_key((dague_ddesc_t*)&T, "T");
 
-    dplasma_zplrnt_perso_Destruct( dague_zplrnt_perso );
+        dplasma_zplrnt( dague, (tiled_matrix_desc_t *)&V, 3456 );
+        dplasma_zlaset( dague, PlasmaUpperLower, 0., 0., (tiled_matrix_desc_t *)&T);
+
+        subA = tiled_matrix_submatrix( A, 0, 0, A->m, 1 );
+
+        dplasma_zlacpy( dague, PlasmaUpperLower,
+                        (tiled_matrix_desc_t *)&V, subA );
+        free( subA );
+        dplasma_zgeqrf( dague, (tiled_matrix_desc_t *)&V, (tiled_matrix_desc_t *)&T );
+        dplasma_zungqr( dague,
+                        (tiled_matrix_desc_t *)&V,
+                        (tiled_matrix_desc_t *)&T,
+                        (tiled_matrix_desc_t *)&A );
+
+    }
+    break;
+
+    default:
+        dague_zplrnt_perso = dplasma_zplrnt_perso_New( A, type, seed );
+
+        dague_enqueue(dague, (dague_object_t*)dague_zplrnt_perso);
+        dplasma_progress(dague);
+
+        dplasma_zplrnt_perso_Destruct( dague_zplrnt_perso );
+    }
     return 0;
 }
 
