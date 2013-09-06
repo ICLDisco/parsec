be0a430e5a447d8eca9254fd1bfc168cdaa6fdd7
George Bosilca <bosilca@eecs.utk.edu>
2013-09-06 12:39:10 +0200

    Naming is diffenret between the trunk and the all-about-data branch.

--- dplasma/lib/zlaset_wrapper.c
+++ dplasma/lib/zlaset_wrapper.c
@@ -41,10 +41,10 @@
  *         On exit, A has been set accordingly.
  *
  **/
-dague_handle_t* dplasma_zlaset_New( PLASMA_enum uplo, dague_complex64_t alpha, dague_complex64_t beta,
+dague_object_t* dplasma_zlaset_New( PLASMA_enum uplo, dague_complex64_t alpha, dague_complex64_t beta,
                                     tiled_matrix_desc_t *A )
 {
-    dague_zlaset_handle_t* object;
+    dague_zlaset_object_t* object;
 
     object = dague_zlaset_new( uplo, alpha, beta, *A, (dague_ddesc_t*)A);
 
@@ -54,18 +54,18 @@ dague_handle_t* dplasma_zlaset_New( PLASMA_enum uplo, dague_complex64_t alpha, d
                                  DAGUE_ARENA_ALIGNMENT_SSE,
                                  MPI_DOUBLE_COMPLEX, A->mb, A->nb, -1);
 
-    return (dague_handle_t*)object;
+    return (dague_object_t*)object;
 }
 
 int dplasma_zlaset( dague_context_t *dague,
                     PLASMA_enum uplo, dague_complex64_t alpha, dague_complex64_t beta,
                     tiled_matrix_desc_t *A)
 {
-    dague_handle_t *dague_zlaset = NULL;
+    dague_object_t *dague_zlaset = NULL;
 
     dague_zlaset = dplasma_zlaset_New(uplo, alpha, beta, A);
 
-    dague_enqueue(dague, (dague_handle_t*)dague_zlaset);
+    dague_enqueue(dague, (dague_object_t*)dague_zlaset);
     dplasma_progress(dague);
 
     dplasma_zlaset_Destruct( dague_zlaset );
@@ -73,9 +73,9 @@ int dplasma_zlaset( dague_context_t *dague,
 }
 
 void
-dplasma_zlaset_Destruct( dague_handle_t *o )
+dplasma_zlaset_Destruct( dague_object_t *o )
 {
-    dague_zlaset_handle_t *dague_zlaset = (dague_zlaset_handle_t *)o;
+    dague_zlaset_object_t *dague_zlaset = (dague_zlaset_object_t *)o;
     dplasma_datatype_undefine_type( &(dague_zlaset->arenas[DAGUE_zlaset_DEFAULT_ARENA   ]->opaque_dtt) );
-    DAGUE_INTERNAL_HANDLE_DESTRUCT(dague_zlaset);
+    DAGUE_INTERNAL_OBJECT_DESTRUCT(dague_zlaset);
 }
