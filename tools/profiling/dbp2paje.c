b7e9ba98dc27d6d08cf4e73c5871b1433d9effa5
George Bosilca <bosilca@eecs.utk.edu>
2013-10-13 18:59:10 +0200

    Replace the existing "hash" function with a stronger version to avoid as much as possible the collisions. The current version of our conversion tool is extremely prone to collisions, dumping useless profiles if they happen. Thus a strong hash function is a requirement.

--- tools/profiling/dbp2paje.c
+++ tools/profiling/dbp2paje.c
@@ -303,21 +303,65 @@ typedef struct uidentry {
 #define UID_HASH_LEN 256
 static uidentry_t *UIDs[UID_HASH_LEN] = { NULL, };
 
+/**
+ * Improve the hash function to avoid as much as possible the collisions. Use a
+ * murmur3 with a 32 bits length derived function.
+ */
 static hash_key_t uid_hash_va(const char *first, va_list va)
 {
-    hash_key_t r;
-    unsigned char c;
-    int i=0;
+    const uint32_t c1 = 0xcc9e2d51;
+    const uint32_t c2 = 0x1b873593;
+    hash_key_t h = 0, k;
+    uint32_t nbytes, tbytes = 0;
     const char* arg = first;
-    r = *(unsigned char *)arg++;
+
     do {
-        while( *arg != '\0' ) {
-            c = *(unsigned char*)arg++;
-            r = r ^ (0xff & (c << (++i%8) ));
+        nbytes = strlen(arg);
+        int nblocks = nbytes / 4;
+        const uint32_t *blocks = (const uint32_t *)(arg);
+        const uint8_t *tail = (const uint8_t *)(arg + (nblocks * 4));
+
+        for(; nblocks > 0; nblocks--, blocks++) {
+            k = *blocks;
+
+            k *= c1;
+            k = (k << 15) | (k >> (32 - 15));
+            k *= c2;
+
+            h ^= k;
+            h = (h << 13) | (h >> (32 - 13));
+            h = (h * 5) + 0xe6546b64;
         }
+
+        /* epilogue */
+        k = 0;
+        switch (nbytes & 3) {
+        case 3:
+            k ^= tail[2] << 16;
+        case 2:
+            k ^= tail[1] << 8;
+        case 1:
+            k ^= tail[0];
+            k *= c1;
+            k = (k << 13) | (k >> (32 - 15));
+            k *= c2;
+            h ^= k;
+        }
+        tbytes += nbytes;  /* update the total number of bytes */
+
         arg = va_arg(va, const char*);
     } while( NULL != arg );
-    return r;
+
+    /* The final epilogue */
+    h ^= tbytes;
+
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+
+    return h;
 }
 
 static uidentry_t *uidhash_lookup_create_entry(const char *first, ...)
