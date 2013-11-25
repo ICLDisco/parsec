0574baeb142fe340c8612866711a1c815515c401
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-11-26 00:27:53 +0100

    A few modification on Vector to make it distributed by row, column or diagonal
    --HG--
    branch : qr_lu

--- data_dist/matrix/vector_two_dim_cyclic.h
+++ data_dist/matrix/vector_two_dim_cyclic.h
@@ -14,48 +14,48 @@
 #include "data_dist/matrix/matrix.h"
 #include "data_dist/matrix/grid_2Dcyclic.h"
 
-/*
- * General distribution of data. Suppose exists a matrix in process of mpi rank 0
- */
-
 /*******************************************************************
- * distributed data structure and basic functionalities
+ * distributed data vector and basic functionalities
  *******************************************************************/
+typedef enum vector_distrib {
+    PlasmaVectorRow,
+    PlasmaVectorCol,
+    PlasmaVectorDiag
+} vector_distrib_t;
 
-/* structure equivalent to PLASMA_desc, but for distributed matrix data
+/*
+ * Vector structure inheriting from tiled_matrix_desc_t
+ * Follows the same distribution than the diagonal tiles of the
+ * two_dim_block_cyclic_t structure.
  */
-typedef struct vector_two_dim_cyclic {
+typedef struct vector_two_dim_cyclic_s {
     tiled_matrix_desc_t super;
     grid_2Dcyclic_t     grid;
-    void *mat;      /**< pointer to the beginning of the matrix */
-    int nb_elem_r;  /**< number of row of tiles  handled by this process - derived parameter */
+    vector_distrib_t    distrib; /**< Distribution used for the vector: Row, Column or diagonal */
+    int   lcm;                   /**< number of processors present on diagonal */
+    void *mat;                   /**< pointer to the beginning of the matrix   */
 } vector_two_dim_cyclic_t;
 
 /**
- * Initialize the description of a  2-D block cyclic distributed matrix.
+ * Initialize the description of a 2-D block cyclic distributed vector.
+ *
  * @param Ddesc matrix description structure, already allocated, that will be initialize
  * @param mtype type of data used for this matrix
- * @param storage type of storage of data
  * @param nodes number of nodes
  * @param cores number of cores per node
  * @param myrank rank of the local node (as of mpi rank)
- * @param mb number of row in a tile
- * @param lm number of rows of the entire matrix
- * @param i starting row index for the computation on a submatrix
- * @param m number of rows of the entire submatrix
- * @param nrst number of rows of tiles for block distribution
- * @param process_GridRows number of row of processes of the process grid (has to divide nodes)
+ * @param mb number of elements in a segment
+ * @param lm number of elements in the full vector
+ * @param i starting element index for the computation on a subvector
+ * @param m number of elements of the entire subvector
+ * @param process_GridRows number of row of processes of in the process grid (has to divide nodes)
  */
-void vector_two_dim_cyclic_init(vector_two_dim_cyclic_t * twoDBCdesc,
-                               enum matrix_type mtype,
-                               enum matrix_storage storage,
-                               int nodes, int cores, int myrank,
-                               int mb,   /* Tile size */
-                               int lm,   /* Global matrix size (what is stored)*/
-                               int i,    /* Staring point in the global matrix */
-                               int m,    /* Submatrix size (the one concerned by the computation */
-                               int nrst, /* Super-tiling size */
-                               int process_GridRows );
+void vector_two_dim_cyclic_init(vector_two_dim_cyclic_t * vdesc,
+                                enum matrix_type    mtype,
+                                enum vector_distrib distrib,
+                                int nodes, int cores, int myrank,
+                                int mb, int lm, int i, int m,
+                                int process_GridRows );
 
 void vector_two_dim_cyclic_supertiled_view( vector_two_dim_cyclic_t* target,
                                             vector_two_dim_cyclic_t* origin,
