ed6cc409f5adc2ed703340df356a0db41105210b
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-11-26 11:34:44 +0100

    Protect call to dague_rpof_grapher in map_operator.c
    --HG--
    branch : qr_lu

--- data_dist/matrix/map_operator.c
+++ data_dist/matrix/map_operator.c
@@ -337,7 +337,9 @@ static int complete_hook(dague_execution_unit_t *context,
 
     TAKE_TIME(context, 2*this_task->function->function_id+1, map_operator_op_hash( __dague_object, k, n ), NULL, 0);
 
+#if defined(DAGUE_PROF_GRAPHER)
     dague_prof_grapher_task(this_task, context->th_id, context->virtual_process->vp_id, k+n);
+#endif  /* defined(DAGUE_PROF_GRAPHER) */
 
     release_deps(context, this_task,
                  (DAGUE_ACTION_RELEASE_REMOTE_DEPS |
