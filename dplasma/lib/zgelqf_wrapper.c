04f4fa1a1d2e1068c9b83ebaf20a4965e7d26b2c
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-12-02 22:41:43 +0100

    Add comments and parameter checks in QR/LQ wrappers
    --HG--
    branch : qr_lu

--- dplasma/lib/zgelqf_wrapper.c
+++ dplasma/lib/zgelqf_wrapper.c
@@ -1,13 +1,14 @@
 /*
- * Copyright (c) 2011-2012 The University of Tennessee and The University
+ * Copyright (c) 2011-2013 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
+ * Copyright (c) 2013      Inria. All rights reserved.
+ * $COPYRIGHT
  *
  * @precisions normal z -> s d c
  *
  */
 #include "dague_internal.h"
-#include <core_blas.h>
 #include "dplasma.h"
 #include "dplasma/lib/dplasmatypes.h"
 #include "dplasma/lib/dplasmaaux.h"
@@ -15,24 +16,88 @@
 
 #include "zgelqf.h"
 
-dague_object_t* dplasma_zgelqf_New( tiled_matrix_desc_t *A,
-                                    tiled_matrix_desc_t *T )
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64_t
+ *
+ * dplasma_zgelqf_New - Generates the object that computes the LQ factorization
+ * a complex M-by-N matrix A: A = L * Q.
+ *
+ * The method used in this algorithm is a tile LQ algorithm with a flat
+ * reduction tree. It is recommended to use the super tiling parameter (SNB) to
+ * improve the performance of the factorization.
+ * A high SNB parameter reduces the communication volume, but also deteriorates
+ * the load balancing if too important. A small one increases the communication
+ * volume, but improves load balancing.
+ * A good SNB value should provide enough work to all available cores on one
+ * node. It is then recommended to set it to 4 when creating the matrix
+ * descriptor.
+ *
+ * This variant is good for square large problems.
+ * For other problems, see:
+ *   - dplasma_zgelqf_param_New() parameterized with trees for tall and skinny
+ *     matrices
+ *   - dplasma_zgelqf_param_New() parameterized with systolic tree if
+ *     computation load per node is very low.
+ *
+ * WARNING: The computations are not done by this call.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] A
+ *          Descriptor of the distributed matrix A to be factorized.
+ *          On entry, describes the M-by-N matrix A.
+ *          On exit, the elements on and below the diagonal of the array contain
+ *          the M-by-min(M,N) lower trapezoidal matrix L (L is lower triangular
+ *          if (N >= M); the elements above the diagonal represent the unitary
+ *          matrix Q as a product of elementary reflectors stored by tiles.
+ *          It cannot be used directly as in Lapack.
+ *
+ * @param[out] T
+ *          Descriptor of the matrix T distributed exactly as the A matrix. T.mb
+ *          defines the IB parameter of tile LQ algorithm. This matrix must be
+ *          of size A.mt * T.mb - by - A.nt * T.nb, with T.nb == A.nb.
+ *          On exit, contains auxiliary information required to compute the Q
+ *          matrix, and/or solve the problem.
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval NULL if incorrect parameters are given.
+ *          \retval The dague object describing the operation that can be
+ *          enqueued in the runtime with dague_enqueue(). It, then, needs to be
+ *          destroy with dplasma_zgelqf_Destruct();
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgelqf
+ * @sa dplasma_zgelqf_Destruct
+ * @sa dplasma_cgelqf_New
+ * @sa dplasma_dgelqf_New
+ * @sa dplasma_sgelqf_New
+ *
+ ******************************************************************************/
+dague_object_t*
+dplasma_zgelqf_New( tiled_matrix_desc_t *A,
+                    tiled_matrix_desc_t *T )
 {
     dague_zgelqf_object_t* object;
     int ib = T->mb;
+
     /*
      * TODO: We consider ib is T->mb but can be incorrect for some tricks with GPU,
      * it should be passed as a parameter as in getrf
      */
-
-    object = dague_zgelqf_new( (dague_ddesc_t*)A, (dague_ddesc_t*)T,
+    object = dague_zgelqf_new( (dague_ddesc_t*)A,
+                               (dague_ddesc_t*)T,
                                ib, NULL, NULL);
 
     object->p_tau = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
-    dague_private_memory_init( object->p_tau, T->nb * sizeof(dague_complex64_t) );
+    dague_private_memory_init( object->p_tau, zgelqf_p_tau_SIZE );
 
     object->p_work = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
-    dague_private_memory_init( object->p_work, ib * T->nb * sizeof(dague_complex64_t) );
+    dague_private_memory_init( object->p_work, zgelqf_p_work_SIZE );
 
     /* Default type */
     dplasma_add2arena_tile( object->arenas[DAGUE_zgelqf_DEFAULT_ARENA],
@@ -61,19 +126,26 @@ dague_object_t* dplasma_zgelqf_New( tiled_matrix_desc_t *A,
     return (dague_object_t*)object;
 }
 
-int dplasma_zgelqf( dague_context_t *dague, tiled_matrix_desc_t *A, tiled_matrix_desc_t *T)
-{
-    dague_object_t *dague_zgelqf = NULL;
-
-    dague_zgelqf = dplasma_zgelqf_New(A, T);
-
-    dague_enqueue(dague, (dague_object_t*)dague_zgelqf);
-    dplasma_progress(dague);
-
-    dplasma_zgelqf_Destruct( dague_zgelqf );
-    return 0;
-}
-
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64_t
+ *
+ *  dplasma_zgelqf_Destruct - Free the data structure associated to an object
+ *  created with dplasma_zgelqf_New().
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] o
+ *          On entry, the object to destroy.
+ *          On exit, the object cannot be used anymore.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgelqf_New
+ * @sa dplasma_zgelqf
+ *
+ ******************************************************************************/
 void
 dplasma_zgelqf_Destruct( dague_object_t *o )
 {
@@ -90,4 +162,87 @@ dplasma_zgelqf_Destruct( dague_object_t *o )
     free( dague_zgelqf->p_tau  );
 
     DAGUE_INTERNAL_OBJECT_DESTRUCT(dague_zgelqf);
+}
+
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64_t
+ *
+ * dplasma_zgelqf - Computes the LQ factorization a M-by-N matrix A:
+ * A = L * Q.
+ *
+ * The method used in this algorithm is a tile LQ algorithm with a flat
+ * reduction tree. It is recommended to use the super tiling parameter (SNB) to
+ * improve the performance of the factorization.
+ * A high SNB parameter reduces the communication volume, but also deteriorates
+ * the load balancing if too important. A small one increases the communication
+ * volume, but improves load balancing.
+ * A good SNB value should provide enough work to all available cores on one
+ * node. It is then recommended to set it to 4 when creating the matrix
+ * descriptor.
+ *
+ * This variant is good for square large problems.
+ * For other problems, see:
+ *   - dplasma_zgelqf_param() parameterized with trees for tall and skinny
+ *     matrices
+ *   - dplasma_zgelqf_param() parameterized with systolic tree if computation
+ *     load per node is very low.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] dague
+ *          The dague context of the application that will run the operation.
+ *
+ * @param[in,out] A
+ *          Descriptor of the distributed matrix A to be factorized.
+ *          On entry, describes the M-by-N matrix A.
+ *          On exit, the elements on and below the diagonal of the array contain
+ *          the M-by-min(M,N) lower trapezoidal matrix L (L is lower triangular
+ *          if (N >= M); the elements above the diagonal represent the unitary
+ *          matrix Q as a product of elementary reflectors stored by tiles.
+ *          It cannot be used directly as in Lapack.
+ *
+ * @param[out] T
+ *          Descriptor of the matrix T distributed exactly as the A matrix. T.mb
+ *          defines the IB parameter of tile LQ algorithm. This matrix must be
+ *          of size A.mt * T.mb - by - A.nt * T.nb, with T.nb == A.nb.
+ *          On exit, contains auxiliary information required to compute the Q
+ *          matrix, and/or solve the problem.
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval -i if the ith parameters is incorrect.
+ *          \retval 0 on success.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgelqf_New
+ * @sa dplasma_zgelqf_Destruct
+ * @sa dplasma_cgelqf
+ * @sa dplasma_dgelqf
+ * @sa dplasma_sgelqf
+ *
+ ******************************************************************************/
+int
+dplasma_zgelqf( dague_context_t *dague,
+                tiled_matrix_desc_t *A,
+                tiled_matrix_desc_t *T )
+{
+    dague_object_t *dague_zgelqf = NULL;
+
+    if ( (A->mt != T->mb) || (A->nt != T->mt) ) {
+        dplasma_error("dplasma_zgelqf", "T doesn't have the same number of tiles as A");
+        return -101;
+    }
+
+    dague_zgelqf = dplasma_zgelqf_New(A, T);
+
+    if ( dague_zgelqf != NULL ) {
+        dague_enqueue(dague, (dague_object_t*)dague_zgelqf);
+        dplasma_progress(dague);
+        dplasma_zgelqf_Destruct( dague_zgelqf );
+    }
 }
--- dplasma/lib/zgeqrf_wrapper.c
+++ dplasma/lib/zgeqrf_wrapper.c
@@ -1,13 +1,14 @@
 /*
- * Copyright (c) 2011-2012 The University of Tennessee and The University
+ * Copyright (c) 2011-2013 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
+ * Copyright (c) 2013      Inria. All rights reserved.
+ * $COPYRIGHT
  *
  * @precisions normal z -> s d c
  *
  */
 #include "dague_internal.h"
-#include <core_blas.h>
 #include "dplasma.h"
 #include "dplasma/lib/dplasmatypes.h"
 #include "dplasma/lib/dplasmaaux.h"
@@ -15,24 +16,88 @@
 
 #include "zgeqrf.h"
 
-dague_object_t* dplasma_zgeqrf_New( tiled_matrix_desc_t *A,
-                                    tiled_matrix_desc_t *T )
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64_t
+ *
+ * dplasma_zgeqrf_New - Generates the object that computes the QR factorization
+ * a complex M-by-N matrix A: A = Q * R.
+ *
+ * The method used in this algorithm is a tile QR algorithm with a flat
+ * reduction tree.  It is recommended to use the super tiling parameter (SMB) to
+ * improve the performance of the factorization.
+ * A high SMB parameter reduces the communication volume, but also deteriorates
+ * the load balancing if too important. A small one increases the communication
+ * volume, but improves load balancing.
+ * A good SMB value should provide enough work to all available cores on one
+ * node. It is then recommended to set it to 4 when creating the matrix
+ * descriptor.
+ *
+ * This variant is good for square large problems.
+ * For other problems, see:
+ *   - dplasma_zgeqrf_param_New() parameterized with trees for tall and skinny
+ *     matrices
+ *   - dplasma_zgeqrf_param_New() parameterized with systolic tree if
+ *     computation load per node is very low.
+ *
+ * WARNING: The computations are not done by this call.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] A
+ *          Descriptor of the distributed matrix A to be factorized.
+ *          On entry, describes the M-by-N matrix A.
+ *          On exit, the elements on and above the diagonal of the array contain
+ *          the min(M,N)-by-N upper trapezoidal matrix R (R is upper triangular
+ *          if (M >= N); the elements below the diagonal represent the unitary
+ *          matrix Q as a product of elementary reflectors stored by tiles.
+ *          It cannot be used directly as in Lapack.
+ *
+ * @param[out] T
+ *          Descriptor of the matrix T distributed exactly as the A matrix. T.mb
+ *          defines the IB parameter of tile QR algorithm. This matrix must be
+ *          of size A.mt * T.mb - by - A.nt * T.nb, with T.nb == A.nb.
+ *          On exit, contains auxiliary information required to compute the Q
+ *          matrix, and/or solve the problem.
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval NULL if incorrect parameters are given.
+ *          \retval The dague object describing the operation that can be
+ *          enqueued in the runtime with dague_enqueue(). It, then, needs to be
+ *          destroy with dplasma_zgeqrf_Destruct();
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgeqrf
+ * @sa dplasma_zgeqrf_Destruct
+ * @sa dplasma_cgeqrf_New
+ * @sa dplasma_dgeqrf_New
+ * @sa dplasma_sgeqrf_New
+ *
+ ******************************************************************************/
+dague_object_t*
+dplasma_zgeqrf_New( tiled_matrix_desc_t *A,
+                    tiled_matrix_desc_t *T )
 {
     dague_zgeqrf_object_t* object;
     int ib = T->mb;
+
     /*
      * TODO: We consider ib is T->mb but can be incorrect for some tricks with GPU,
      * it should be passed as a parameter as in getrf
      */
-
-    object = dague_zgeqrf_new( (dague_ddesc_t*)A, (dague_ddesc_t*)T,
+    object = dague_zgeqrf_new( (dague_ddesc_t*)A,
+                               (dague_ddesc_t*)T,
                                ib, NULL, NULL);
 
     object->p_tau = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
-    dague_private_memory_init( object->p_tau, T->nb * sizeof(dague_complex64_t) );
+    dague_private_memory_init( object->p_tau, zgeqrf_p_tau_SIZE );
 
     object->p_work = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
-    dague_private_memory_init( object->p_work, ib * T->nb * sizeof(dague_complex64_t) );
+    dague_private_memory_init( object->p_work, zgeqrf_p_work_SIZE );
 
     /* Default type */
     dplasma_add2arena_tile( object->arenas[DAGUE_zgeqrf_DEFAULT_ARENA],
@@ -61,19 +126,26 @@ dague_object_t* dplasma_zgeqrf_New( tiled_matrix_desc_t *A,
     return (dague_object_t*)object;
 }
 
-int dplasma_zgeqrf( dague_context_t *dague, tiled_matrix_desc_t *A, tiled_matrix_desc_t *T)
-{
-    dague_object_t *dague_zgeqrf = NULL;
-
-    dague_zgeqrf = dplasma_zgeqrf_New(A, T);
-
-    dague_enqueue(dague, (dague_object_t*)dague_zgeqrf);
-    dplasma_progress(dague);
-
-    dplasma_zgeqrf_Destruct( dague_zgeqrf );
-    return 0;
-}
-
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64_t
+ *
+ *  dplasma_zgeqrf_Destruct - Free the data structure associated to an object
+ *  created with dplasma_zgeqrf_New().
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] o
+ *          On entry, the object to destroy.
+ *          On exit, the object cannot be used anymore.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgeqrf_New
+ * @sa dplasma_zgeqrf
+ *
+ ******************************************************************************/
 void
 dplasma_zgeqrf_Destruct( dague_object_t *o )
 {
@@ -92,3 +164,85 @@ dplasma_zgeqrf_Destruct( dague_object_t *o )
     DAGUE_INTERNAL_OBJECT_DESTRUCT(dague_zgeqrf);
 }
 
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64_t
+ *
+ * dplasma_zgeqrf - Computes the QR factorization a M-by-N matrix A:
+ * A = Q * R.
+ *
+ * The method used in this algorithm is a tile QR algorithm with a flat
+ * reduction tree. It is recommended to use the super tiling parameter (SMB) to
+ * improve the performance of the factorization.
+ * A high SMB parameter reduces the communication volume, but also deteriorates
+ * the load balancing if too important. A small one increases the communication
+ * volume, but improves load balancing.
+ * A good SMB value should provide enough work to all available cores on one
+ * node. It is then recommended to set it to 4 when creating the matrix
+ * descriptor.
+ *
+ * This variant is good for square large problems.
+ * For other problems, see:
+ *   - dplasma_zgeqrf_param() parameterized with trees for tall and skinny
+ *     matrices
+ *   - dplasma_zgeqrf_param() parameterized with systolic tree if computation
+ *     load per node is very low.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] dague
+ *          The dague context of the application that will run the operation.
+ *
+ * @param[in,out] A
+ *          Descriptor of the distributed matrix A to be factorized.
+ *          On entry, describes the M-by-N matrix A.
+ *          On exit, the elements on and above the diagonal of the array contain
+ *          the min(M,N)-by-N upper trapezoidal matrix R (R is upper triangular
+ *          if (M >= N); the elements below the diagonal represent the unitary
+ *          matrix Q as a product of elementary reflectors stored by tiles.
+ *          It cannot be used directly as in Lapack.
+ *
+ * @param[out] T
+ *          Descriptor of the matrix T distributed exactly as the A matrix. T.mb
+ *          defines the IB parameter of tile QR algorithm. This matrix must be
+ *          of size A.mt * T.mb - by - A.nt * T.nb, with T.nb == A.nb.
+ *          On exit, contains auxiliary information required to compute the Q
+ *          matrix, and/or solve the problem.
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval -i if the ith parameters is incorrect.
+ *          \retval 0 on success.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgeqrf_New
+ * @sa dplasma_zgeqrf_Destruct
+ * @sa dplasma_cgeqrf
+ * @sa dplasma_dgeqrf
+ * @sa dplasma_sgeqrf
+ *
+ ******************************************************************************/
+int
+dplasma_zgeqrf( dague_context_t *dague,
+                tiled_matrix_desc_t *A,
+                tiled_matrix_desc_t *T )
+{
+    dague_object_t *dague_zgeqrf = NULL;
+
+    if ( (A->mt != T->mb) || (A->nt != T->mt) ) {
+        dplasma_error("dplasma_zgeqrf", "T doesn't have the same number of tiles as A");
+        return -101;
+    }
+
+    dague_zgeqrf = dplasma_zgeqrf_New(A, T);
+
+    if ( dague_zgeqrf != NULL ) {
+        dague_enqueue(dague, (dague_object_t*)dague_zgeqrf);
+        dplasma_progress(dague);
+        dplasma_zgeqrf_Destruct( dague_zgeqrf );
+    }
+}
