74b3af8bc6ac611a80763db497084a44ae8519d7
George Bosilca <bosilca@eecs.utk.edu>
2013-12-03 14:38:35 +0100

    Fix the test.

--- tools/dague-compiler/jdf2c.c
+++ tools/dague-compiler/jdf2c.c
@@ -3864,19 +3864,16 @@ static void jdf_generate_code_release_deps(const jdf_t *jdf, const jdf_function_
     else
         coutput("  arg.output_entry = NULL;\n");
 
-    if( f->flags & JDF_FUNCTION_FLAG_NO_SUCCESSORS )
+    if( !(f->flags & JDF_FUNCTION_FLAG_NO_SUCCESSORS) ) {
         coutput("#if defined(DISTRIBUTED)\n"
                 "  arg.remote_deps_count = 0;\n"
                 "  arg.remote_deps = NULL;\n"
                 "#endif\n");
 
-    if( !(f->flags & JDF_FUNCTION_FLAG_NO_SUCCESSORS) ) {
         coutput("  iterate_successors_of_%s_%s(eu, context, action_mask, dague_release_dep_fct, &arg);\n"
                 "\n",
                 jdf_basename, f->fname);
-    }
 
-    if( f->flags & JDF_FUNCTION_FLAG_NO_SUCCESSORS )
         coutput("#if defined(DISTRIBUTED)\n"
                 "  if( 0 == arg.remote_deps_count ) {\n"
                 "    if( NULL != arg.remote_deps ) {\n"
@@ -3889,7 +3886,7 @@ static void jdf_generate_code_release_deps(const jdf_t *jdf, const jdf_function_
                 "  }\n"
                 "#endif\n"
                 "\n");
-
+    }
     coutput("  if(action_mask & DAGUE_ACTION_RELEASE_LOCAL_DEPS) {\n"
             "    struct dague_vp** vps = eu->virtual_process->dague_context->virtual_processes;\n");
     if( 0 != has_output_data ) {
