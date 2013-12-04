15184056d2da5e20c3d4d113f92d1d7d4801b1ec
Mathieu Faverge <mfaverge@eecs.utk.edu>
2013-12-04 17:40:56 +0100

    Extract plasma_const macro from the ifdef
    --HG--
    branch : qr_lu

--- dplasma/lib/dplasmajdf.h
+++ dplasma/lib/dplasmajdf.h
@@ -9,10 +9,11 @@
 #define QUOTEME_(x) #x
 #define QUOTEME(x) QUOTEME_(x)
 
+#define plasma_const( x )  plasma_lapack_constants[x]
+
 #ifdef DAGUE_CALL_TRACE
 #   include <stdlib.h>
 #   include <stdio.h>
-#   define plasma_const( x )  plasma_lapack_constants[x]
 #   define printlog(str, ...) fprintf(stderr, "thread %d VP %d " str "\n", \
                                       context->th_id, context->virtual_process->vp_id, __VA_ARGS__)
 #   define OUTPUT(ARG)  printf ARG
