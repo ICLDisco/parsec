1e72f7d8db6d3dbff7e7a12c4ccf1a87e2a0b384
Aurelien Bouteiller <bouteill@icl.utk.edu>
2014-01-01 16:14:31 -0500

    Adding the possibility to change the name of the dynamically loaded Cuda functions
    --HG--
    branch : all-about-data

--- src/devices/cuda/dev_cuda.c
+++ src/devices/cuda/dev_cuda.c
@@ -241,7 +241,7 @@ void* cuda_solve_handle_dependencies(gpu_device_t* gpu_device,
 
   retry_lesser_sm_version:
     capability = cuda_legal_compute_capabilitites[index];
-    snprintf(function_name, FILENAME_MAX, "magmablas_S%s_SM%2d", fname, capability);
+    snprintf(function_name, FILENAME_MAX, "%s_SM%2d", fname, capability);
 
     for( target = argv; (NULL != target) && (NULL != *target); target++ ) {
         struct stat status;
@@ -327,21 +327,17 @@ dague_cuda_handle_register(dague_device_t* device, dague_handle_t* handle)
         const dague_function_t* function = handle->functions_array[i];
         __dague_chore_t* chores = (__dague_chore_t*)function->incarnations;
         for( dev_mask = j = 0; NULL != chores[j].hook; j++ ) {
-            dev_mask |= (1 << chores[j].type);
-        }
-        if(dev_mask & (1 << device->type)) {  /* find the function */
-            void* devf = cuda_solve_handle_dependencies(gpu_device, function->name);
-            if( NULL != devf ) {
-                /* TODO: Ugly code to be removed ASAP */
-                if( NULL == cuda_gemm_functions ) {
-                    cuda_gemm_functions = (void**)calloc(100, sizeof(void*));
+            if( chores[j].type == device->type ) {
+                void* devf = cuda_solve_handle_dependencies(gpu_device, NULL==chores[j].dyld?function->name:chores[j].dyld);
+                if( NULL != devf ) {
+                    /* TODO: Ugly code to be removed ASAP */
+                    if( NULL == cuda_gemm_functions ) {
+                        cuda_gemm_functions = (void**)calloc(100, sizeof(void*));
+                    }
+                    cuda_gemm_functions[gpu_device->cuda_index] = devf;
+                    rc = DAGUE_SUCCESS;
+                    dev_mask |= (1 << chores[j].type);
                 }
-                cuda_gemm_functions[gpu_device->cuda_index] = devf;
-                rc = DAGUE_SUCCESS;
-                /* TODO: Ugly code to be removed ASAP */
-            } else {
-                /* We cannot support this device, remove the chore from the list */
-                dev_mask ^= (1 << device->type);
             }
         }
     }
@@ -382,6 +378,7 @@ int dague_gpu_init(dague_context_t *dague_context)
     (void)dague_mca_param_reg_string_name("device_cuda", "path",
                                           "Path to the shared library files containing the CUDA version of the hooks. It is a ;-separated list of either directories or .so files.\n",
                                           false, false, DAGUE_LIB_CUDA_PREFIX, &cuda_lib_path);
+
     if( 0 == use_cuda ) {
         return -1;  /* Nothing to do around here */
     }
@@ -472,7 +469,6 @@ int dague_gpu_init(dague_context_t *dague_context)
         status = cuDeviceGetAttribute( &computemode, CU_DEVICE_ATTRIBUTE_COMPUTE_MODE, hcuDevice );
         DAGUE_CUDA_CHECK_ERROR( "cuDeviceGetAttribute ", status, {continue;} );
 
-        show_caps = 1;
         if( show_caps ) {
             STATUS(("GPU Device %d (capability %d.%d): %s\n", i, major, minor, szName ));
             STATUS(("\tSM                 : %d\n", streaming_multiprocessor ));
