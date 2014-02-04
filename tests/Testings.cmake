5202df7a10cd665e54cbc2b8f4a37bb0234f76ea
George Bosilca <bosilca@eecs.utk.edu>
2014-02-04 01:15:50 +0100

    Add the Fortran test to the testings.

--- tests/Testings.cmake
+++ tests/Testings.cmake
@@ -4,9 +4,15 @@
 
 add_test(unit_startup1 ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=10 -k=10 -v=5)
 add_test(unit_startup2 ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=20 -k=30 -v=5)
-add_test(unit_startup3 ${SHM_TEST_CMD_LIST} ./startup -i=50 -j=50 -k=50 -v=5)
+add_test(unit_startup3 ${SHM_TEST_CMD_LIST} ./startup -i=30 -j=30 -k=30 -v=5)
 
 add_test(unit_touch    ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
+if(CMAKE_Fortran_COMPILER_WORKS)
+  if(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
+    add_test(unit_touchf  ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
+  endif(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
+endif(CMAKE_Fortran_COMPILER_WORKS)
+
 
 # The headnode lack GPUs so we need MPI in order to get the test to run on
 # one of the nodes.
