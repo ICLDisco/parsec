a00bbc38be4c2b09c5afbed517b2542a5606d0a9
Mathieu Faverge <mfaverge@eecs.utk.edu>
2014-04-03 11:30:49 +0200

    Add check function declaration
    --HG--
    branch : all-about-data

--- dplasma/include/dplasma_z.h
+++ dplasma/include/dplasma_z.h
@@ -240,6 +240,12 @@ void dplasma_zplgsy_Destruct( dague_handle_t *o );
 void dplasma_zplrnt_Destruct( dague_handle_t *o );
 
 /**********************************************************
+ * Check routines
+ */
+int check_zaxmb(  dague_context_t *dague, int loud, PLASMA_enum uplo, tiled_matrix_desc_t *A, tiled_matrix_desc_t *b, tiled_matrix_desc_t *x );
+int check_zpotrf( dague_context_t *dague, int loud, PLASMA_enum uplo, tiled_matrix_desc_t *A, tiled_matrix_desc_t *A0 );
+
+/**********************************************************
  * Work in progress
  */
 
