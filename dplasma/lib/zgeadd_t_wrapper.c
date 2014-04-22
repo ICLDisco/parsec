dde17de6662a45c8907bfaf0e8133fdce74bfc5c
shawnccx@gmail.com <shawnccx@gmail.com>
2014-04-22 17:55:24 -0400

    Add zgeadd_t, this function does B += alpha * A' Two steps of doing this: (1) Transpose tile index Add a map2t function, which maps transposed tile index of A and B, like A(j,i) and B(i,j) (2) tile_B += alpha*tile_A' Implemented in zgeadd_t_wrapper
    --HG--
    branch : devel

---
+++ dplasma/lib/zgeadd_t_wrapper.c
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2011-2013 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2013      Inria. All rights reserved.
+ *
+ * @precisions normal z -> c d s
+ *
+ */
+
+#include "dplasma.h"
+#include "dplasma/lib/dplasmatypes.h"
+
+#include "map2t.h"
+
+static int
+dplasma_zgeadd_t_operator( dague_execution_unit_t *eu,
+                         const tiled_matrix_desc_t *descA,
+                         const tiled_matrix_desc_t *descB,
+                         const void *_A, void *_B,
+                         PLASMA_enum uplo, int m, int n,
+                         void *args )
+{
+    const dague_complex64_t *A     = (dague_complex64_t*)_A;
+    dague_complex64_t       *B     = (dague_complex64_t*)_B;
+    dague_complex64_t        alpha = *((dague_complex64_t*)args);
+    int j;
+    int tempmm, tempnn, ldam, ldbm;
+    (void)eu;
+
+    tempmm = ((m)==((descB->mt)-1)) ? ((descB->m)-(m*(descB->mb))) : (descB->mb);
+    tempnn = ((n)==((descB->nt)-1)) ? ((descB->n)-(n*(descB->nb))) : (descB->nb);
+    ldam = BLKLDD( *descA, n );
+    ldbm = BLKLDD( *descB, m );
+
+    switch ( uplo ) {
+    case PlasmaLower:
+        for (j = 0; j < tempnn; j++, tempmm--, A+=ldam+1, B+=ldbm+1) {
+            cblas_zaxpy(tempmm, CBLAS_SADDR(alpha), A, ldam, B, 1);
+        }
+        break;
+    case PlasmaUpper:
+        for (j = 0; j < tempnn; j++, A+=1, B+=ldbm) {
+            cblas_zaxpy(j+1, CBLAS_SADDR(alpha), A, ldam, B, 1);
+        }
+        break;
+    case PlasmaUpperLower:
+    default:
+        for (j = 0; j < tempnn; j++, A+=1, B+=ldbm) {
+            cblas_zaxpy(tempmm, CBLAS_SADDR(alpha), A, ldam, B, 1);
+        }
+    }
+
+    return 0;
+}
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64
+ *
+ * dplasma_zgeadd_t_New - Generates an object that computes the operation B =
+ * alpha * A' + B
+ *
+ * See dplasma_map2t_New() for further information.
+ *
+ * WARNING: The computations are not done by this call.
+ *
+ *******************************************************************************
+ *
+ * @param[in] uplo
+ *          Specifies whether the matrix A is upper triangular or lower
+ *          triangular:
+ *          = PlasmaUpperLower: All matrix A is referenced;
+ *          = PlasmaUpper: Upper triangle of A is referenced;
+ *          = PlasmaLower: Lower triangle of A is referenced.
+ *
+ * @param[in] alpha
+ *          The scalar alpha
+ *
+ * @param[in] A
+ *          The descriptor of the distributed matrix A of size M-by-N.
+ *
+ * @param[in,out] B
+ *          The descriptor of the distributed matrix B of size M-by-N.
+ *          On exit, the matrix B data are overwritten by the result of alpha*A+B
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval NULL if incorrect parameters are given.
+ *          \retval The dague object describing the operation that can be
+ *          enqueued in the runtime with dague_enqueue(). It, then, needs to be
+ *          destroy with dplasma_zgeadd_Destruct();
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgeadd
+ * @sa dplasma_zgeadd_Destruct
+ * @sa dplasma_cgeadd_New
+ * @sa dplasma_dgeadd_New
+ * @sa dplasma_sgeadd_New
+ *
+ ******************************************************************************/
+dague_handle_t*
+dplasma_zgeadd_t_New( PLASMA_enum uplo,
+                    dague_complex64_t alpha,
+                    const tiled_matrix_desc_t *A,
+                    tiled_matrix_desc_t *B)
+{
+    dague_complex64_t *a = (dague_complex64_t*)malloc(sizeof(dague_complex64_t));
+    *a = alpha;
+
+    return dplasma_map2t_New(uplo, A, B,
+                            dplasma_zgeadd_t_operator, (void *)a);
+}
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64
+ *
+ *  dplasma_zlacpy_Destruct - Free the data structure associated to an object
+ *  created with dplasma_zlacpy_New().
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] o
+ *          On entry, the object to destroy.
+ *          On exit, the object cannot be used anymore.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zlacpy_New
+ * @sa dplasma_zlacpy
+ *
+ ******************************************************************************/
+void
+dplasma_zgeadd_t_Destruct( dague_handle_t *o )
+{
+    dplasma_map2t_Destruct( o );
+}
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup dplasma_complex64
+ *
+ * dplasma_zgeadd - Generates an object that computes the operation B =
+ * alpha * A' + B
+ *
+ * See dplasma_map2t() for further information.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] dague
+ *          The dague context of the application that will run the operation.
+ *
+ * @param[in] uplo
+ *          Specifies whether the matrix A is upper triangular or lower
+ *          triangular:
+ *          = PlasmaUpperLower: All matrix A is referenced;
+ *          = PlasmaUpper: Upper triangle of A is referenced;
+ *          = PlasmaLower: Lower triangle of A is referenced.
+ *
+ * @param[in] alpha
+ *          The scalar alpha
+ *
+ * @param[in] A
+ *          The descriptor of the distributed matrix A of size M-by-N.
+ *
+ * @param[in,out] B
+ *          The descriptor of the distributed matrix B of size M-by-N.
+ *          On exit, the matrix B data are overwritten by the result of alpha*A+B
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval -i if the ith parameters is incorrect.
+ *          \retval 0 on success.
+ *
+ *******************************************************************************
+ *
+ * @sa dplasma_zgeadd_New
+ * @sa dplasma_zgeadd_Destruct
+ * @sa dplasma_cgeadd
+ * @sa dplasma_dgeadd
+ * @sa dplasma_sgeadd
+ *
+ ******************************************************************************/
+int
+dplasma_zgeadd_t( dague_context_t *dague,
+                PLASMA_enum uplo,
+                dague_complex64_t alpha,
+                const tiled_matrix_desc_t *A,
+                tiled_matrix_desc_t *B)
+{
+    dague_handle_t *dague_zgeadd_t = NULL;
+
+    if ((uplo != PlasmaUpperLower) &&
+        (uplo != PlasmaUpper)      &&
+        (uplo != PlasmaLower))
+    {
+        dplasma_error("dplasma_zgeadd", "illegal value of uplo");
+        return -2;
+    }
+
+    dague_zgeadd_t = dplasma_zgeadd_t_New(uplo, alpha, A, B);
+
+    if ( dague_zgeadd_t != NULL )
+    {
+        dague_enqueue(dague, (dague_handle_t*)dague_zgeadd_t);
+        dplasma_progress(dague);
+        dplasma_zgeadd_t_Destruct( dague_zgeadd_t );
+    }
+    return 0;
+}
