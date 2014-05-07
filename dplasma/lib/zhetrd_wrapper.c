505e1f8aca81e3e7c2fcda92c2f43930114efb0a
Aurelien Bouteiller <bouteill@icl.utk.edu>
2014-05-07 16:31:47 -0400

    Make it compile etc
    --HG--
    branch : devel

--- dplasma/lib/zhetrd_wrapper.c
+++ dplasma/lib/zhetrd_wrapper.c
@@ -13,36 +13,41 @@
 
 #include "data_dist/matrix/sym_two_dim_rectangle_cyclic.h"
 #include "data_dist/matrix/two_dim_rectangle_cyclic.h"
+#include "dplasma/lib/memory_pool.h"
 #include "data_dist/matrix/diag_band_to_rect.h"
-#include "dplasma/lib/zhetrd_he2hb.h"
-#include "dplasma/lib/zhetrd_hb2st.h"
+#include "dplasma/lib/zhetrd_h2b_L.h"
+#include "dplasma/lib/zhetrd_b2s.h"
 
 
 /* SUBROUTINE ZHETRD( UPLO, N, A, LDA, D, E, TAU, WORK, LWORK, INFO )
  */
 int
-dplasma_zhetrd( PLASMA_enum uplo,
+dplasma_zhetrd( dague_context_t* dague,
+                PLASMA_enum uplo,
+                int ib,
                 tiled_matrix_desc_t* A,
                 tiled_matrix_desc_t* DE,
                 tiled_matrix_desc_t* T,
                 int* info )
 {
-    dague_object_t* h2b_obj=NULL, * band2rect_obj=NULL, * b2s_obj=NULL;
+    dague_zhetrd_h2b_L_object_t * h2b_obj=NULL;
+    dague_diag_band_to_rect_object_t* band2rect_obj=NULL;
+    dague_zhetrd_b2s_object_t * b2s_obj=NULL;
     dague_memory_pool_t pool[4];
 
     if( uplo != PlasmaLower && uplo != PlasmaUpper ) {
         dplasma_error("DPLASMA_zhetrd", "illegal value of uplo");
         *info = -1;
-        return NULL;
+        return *info;
     }
     
-    dague_private_memory_init( pool[0], (sizeof(dague_complex64_t)*T->nb) ); /* tau */
-    dague_private_memory_init( pool[1], (sizeof(dague_complex64_t)*T->nb*IB) ); /* work */
-    dague_private_memory_init( pool[2], (sizeof(dague_complex64_t)*T->nb*2 *T->nb) ); /* work for HERFB1 */
-    dague_private_memory_init( pool[3], (sizeof(dague_complex64_t)*T->nb*4 *T->nb) ); /* work for the TSMQRLR */
+    dague_private_memory_init( &pool[0], (sizeof(dague_complex64_t)*T->nb) ); /* tau */
+    dague_private_memory_init( &pool[1], (sizeof(dague_complex64_t)*T->nb*ib) ); /* work */
+    dague_private_memory_init( &pool[2], (sizeof(dague_complex64_t)*T->nb*2 *T->nb) ); /* work for HERFB1 */
+    dague_private_memory_init( &pool[3], (sizeof(dague_complex64_t)*T->nb*4 *T->nb) ); /* work for the TSMQRLR */
     
-    if( Plasma_Lower == uplo ) {
-        h2b_obj = dague_zhetrd_h2b_L_new( uplo, ib, (sym_two_dim_block_cyclic_t*)A, T, pool[3], pool[2], pool[1], pool[0] );
+    if( PlasmaLower == uplo ) {
+        h2b_obj = dague_zhetrd_h2b_L_new( ib, A, *A, T, *T, &pool[3], &pool[2], &pool[1], &pool[0] );
         dplasma_add2arena_rectangle( h2b_obj->arenas[DAGUE_zhetrd_h2b_L_DEFAULT_ARENA],
                                  A->mb*A->nb*sizeof(dague_complex64_t),
                                  DAGUE_ARENA_ALIGNMENT_SSE,
@@ -51,8 +56,9 @@ dplasma_zhetrd( PLASMA_enum uplo,
                                  T->mb*T->nb*sizeof(dague_complex64_t),
                                  DAGUE_ARENA_ALIGNMENT_SSE,
                                  MPI_DOUBLE_COMPLEX, T->mb, T->nb, -1);
+#if 0
     } else {
-        h2b_obj = dague_zhetrd_h2b_U_new( uplo, ib, (sym_two_dim_block_cyclic_t*)A, T, pool[3], pool[2], pool[1], pool[0] );
+        h2b_obj = dague_zhetrd_h2b_U_new( ib, A, *A, T, *T, pool[3], pool[2], pool[1], pool[0] );
         dplasma_add2arena_rectangle( h2b_obj->arenas[DAGUE_zhetrd_h2b_U_DEFAULT_ARENA],
                                  A->mb*A->nb*sizeof(dague_complex64_t),
                                  DAGUE_ARENA_ALIGNMENT_SSE,
@@ -61,6 +67,7 @@ dplasma_zhetrd( PLASMA_enum uplo,
                                  T->mb*T->nb*sizeof(dague_complex64_t),
                                  DAGUE_ARENA_ALIGNMENT_SSE,
                                  MPI_DOUBLE_COMPLEX, T->mb, T->nb, -1);
+#endif
     }
     if( NULL == h2b_obj ) { 
         *info=-101; goto cleanup;
@@ -74,7 +81,7 @@ dplasma_zhetrd( PLASMA_enum uplo,
                            DAGUE_ARENA_ALIGNMENT_SSE,
                            MPI_DOUBLE_COMPLEX, A->mb);
 
-    b2s_obj = dague_zhetrd_b2s_new( (two_dim_block_cyclic_t*)DE, DE->mb-1 );
+    b2s_obj = dague_zhetrd_b2s_new( DE, DE->mb-1 );
     if( NULL == b2s_obj ) goto cleanup;
     dplasma_add2arena_rectangle(b2s_obj->arenas[DAGUE_zhetrd_b2s_DEFAULT_ARENA], 
                                 DE->mb*DE->nb*sizeof(dague_complex64_t),
@@ -82,7 +89,7 @@ dplasma_zhetrd( PLASMA_enum uplo,
                                 MPI_DOUBLE_COMPLEX, DE->mb, DE->nb, -1);
         
     dague_enqueue( dague, (dague_object_t*)h2b_obj );
-    dague_enqueue( dague, (dague_object_t*)band2rec );
+    dague_enqueue( dague, (dague_object_t*)band2rect_obj );
     dague_enqueue( dague, (dague_object_t*)b2s_obj );
     dplasma_progress(dague);
 
@@ -90,10 +97,10 @@ cleanup:
     if( h2b_obj ) DAGUE_INTERNAL_OBJECT_DESTRUCT( h2b_obj );
     if( band2rect_obj ) DAGUE_INTERNAL_OBJECT_DESTRUCT( band2rect_obj );
     if( b2s_obj ) DAGUE_INTERNAL_OBJECT_DESTRUCT( b2s_obj );
-    dague_private_memory_fini( pool[0] );
-    dague_private_memory_fini( pool[1] );
-    dague_private_memory_fini( pool[2] );
-    dague_private_memory_fini( pool[3] );
+    dague_private_memory_fini( &pool[0] );
+    dague_private_memory_fini( &pool[1] );
+    dague_private_memory_fini( &pool[2] );
+    dague_private_memory_fini( &pool[3] );
     return *info; 
 }
 
