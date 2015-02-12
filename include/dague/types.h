39a11f860abe256a8c284559100f4d50d485c5e1
George Bosilca <bosilca@icl.utk.edu>
2015-02-12 17:48:51 -0500

    Add a backpointer from the data_t to the original ddest_t. We can now track where each data_t belongs to.
    --HG--
    branch : devel

--- include/dague/types.h
+++ include/dague/types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012      The University of Tennessee and The University
+ * Copyright (c) 2012-2015 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  */
@@ -11,6 +11,7 @@
 typedef struct dague_data_s dague_data_t;
 typedef struct dague_data_copy_s dague_data_copy_t;
 typedef uint32_t dague_data_key_t;
+typedef struct dague_ddesc_s dague_ddesc_t;
 
 #endif  /* DAGUE_TYPES_H_HAS_BEEN_INCLUDED */
 
