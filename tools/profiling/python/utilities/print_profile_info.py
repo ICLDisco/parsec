42b6af7d02c4045a6d1212d225ff90c3344c0cbb
George Bosilca <bosilca@icl.utk.edu>
2015-03-13 00:41:04 -0400

    Fix the Python script to allow dumping the information about a profiling file.
    --HG--
    branch : devel

--- tools/profiling/python/utilities/print_profile_info.py
+++ tools/profiling/python/utilities/print_profile_info.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 from parsec_trace_tables import *
-import pbt2ptt as ptt_bin
+import pbt2ptt as ptt
 import os, sys
 
 if __name__ == '__main__':
@@ -11,6 +11,6 @@ if __name__ == '__main__':
         if os.path.exists(arg):
             filenames.append(arg)
     for filename in filenames:
-        trace = ptt_bin.get_info(filename)
+        trace = ptt.read(filename)
         print(trace.information)
         print(trace.event_types)
