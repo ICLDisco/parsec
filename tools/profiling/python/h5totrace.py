c15986730101202648e2cc67dbffe1f06180683d
thomas herault <herault@eecs.utk.edu>
2015-03-27 15:52:46 -0400

    Functional h5totrace python script, with capability to ignore selected events or threads, merge a dot file with a h5 file, and select types to display as counters instead of states.
    --HG--
    branch : devel

--- tools/profiling/python/h5totrace.py
+++ tools/profiling/python/h5totrace.py
@@ -8,20 +8,57 @@ import paje
 import argparse
 import sys
 import os
+import re
 
 def warning(*objs):
     print("WARNING: ", *objs, file=sys.stderr)
+
+def print_event_names(store):
+    maxlen = len("#rank")
+    for en in store.event_names:
+        if( maxlen < len(en) ):
+            maxlen = len(en)
+
+    line = ("{:<%s} "%(maxlen)).format("#rank")
+    for n in range(0, store.information.nb_nodes):
+        line += "%5d "%(n)
+    print(line)
+
+    for i in range(0, len(store.event_names)-1):
+        en = store.event_names[i]
+        line = ("{:>%d} "%(maxlen)).format(en)
+
+        df = store.events[:][store.events.type==i].groupby('node_id').count()['begin']
+        for n in range(0, len(df)):
+            line += "%5d "%(df[n])
+        print(line)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='convert an HDF5 profile file (and optionnaly a DOT file) into a PAJE trace')
     parser.add_argument('--h5', help='HDF5 input file', required=1, dest='input')
     parser.add_argument('--dot', help='DOT input file (should match the run of the HDF5 input file)', dest='dot')
-    parser.add_argument('--out', help='Output file name', required=1, dest='output')
+    parser.add_argument('--out', help='Output file name', dest='output')
+    parser.add_argument('--counter', help='Add a counter line per rank to represent the event names given as parameter (multiple allowed)',
+                        action='append', dest='counters', nargs='+')
+    parser.add_argument('--ignore-type', help='Set an event name to ignore (multiple allowed, can be a pattern with /pattern/)',
+                        action='append', dest='type_ignore', nargs='+')
+    parser.add_argument('--ignore-thread', help='Set a thread name to ignore (multiple allowed, can be a pattern with /pattern/)',
+                        action='append', dest='thread_ignore', nargs='+')
+    parser.add_argument('--list', help='List the events names in the hdf5 file', action='store_true', dest='list')
     args = parser.parse_args()
 
     store = pd.HDFStore(args.input)
     if( not 'events' in store or not 'threads' in store or not 'event_names' in store ):
         warning('This HDF5 store does not hold a PaRSEC profile')
+        sys.exit(1)
+
+    if( args.list ):
+        print_event_names(store)
+        sys.exit(0)
+
+    if( None == args.output ):
+        warning("Output file not specified, and --list not specified")
+        args.print_help()
         sys.exit(1)
 
     try:
@@ -69,11 +106,11 @@ if __name__ == '__main__':
     paje_vt = PajeContainerType.PajeEvent(Name="VP", Type=paje_pt)
     paje_tt = PajeContainerType.PajeEvent(Name="Thread", Type=paje_vt)
     paje_st = PajeStateType.PajeEvent(Name="CT_ST", Type=paje_tt)
-    paje_vt1 = PajeDefineVariableType.PajeEvent(Name="CT_VT1", Type=paje_tt, Color="1.0,1.0,1.0")
-    paje_vt2 = PajeDefineVariableType.PajeEvent(Name="CT_VT2", Type=paje_tt, Color="1.0,1.0,0.0")
+    paje_vt = PajeDefineVariableType.PajeEvent(Name="CT_VT", Type=paje_tt, Color="1.0,1.0,1.0")
 
     paje_entity_waiting = PajeEntityValue.PajeEvent(Name="Waiting", Type=paje_st, Color="0.2,0.2,0.2")
 
+    paje_container_aliases = dict()
     state_aliases = dict()
     for x in range(len(store.event_names)-1):
         color_code = int(store.event_attributes[x], 16)
@@ -83,10 +120,58 @@ if __name__ == '__main__':
         state_aliases[x] = PajeEntityValue.PajeEvent(Name=store.event_names[x], Type=paje_st, Color="%g,%g,%g" % (r/255.0, g/255.0, b/255.0))
 
     paje_c_appli = PajeContainerCreate.PajeEvent(Name=store.information.exe, Time=0.000, Type=paje_ct, Container="0")
-    paje_container_aliases = dict()
 
-    for tr in store.threads.iterrows():
-        t = tr[1]
+    types_to_count = list()
+    counter_events = dict()
+    if args.counters:
+        args.counters = [item for sublist in args.counters for item in sublist]
+        for c in args.counters:
+            index=store.event_names[store.event_names.str.contains('^%s$'%(c))].index.tolist()
+            if len(index)==1:
+                i = store.event_names[index[0]]
+                types_to_count.append(index[0])
+                counter_events[i] = dict()
+                counter_events[i]['events'] = list()
+                counter_events[i]['error'] = False
+                counter_events[i]['type_name'] = c
+                for n in range(0, store.information.nb_nodes):
+                    name = "M%d-%s"%(n,c)
+                    if "M%d"%(n) not in paje_container_aliases:
+                        paje_container_aliases["M%d"%(n)] = PajeContainerCreate.PajeEvent(Time = 0.0000, Name = "M%d" % (n),
+                                                                                          Type=paje_ct, Container=paje_c_appli)
+                    paje_container_aliases[name] = PajeContainerCreate.PajeEvent(Time = 0.0000, Name = name, Type=paje_ct,
+                                                                                Container=paje_container_aliases["M%d"%(n)])
+            else:
+                warning('%s is not a valid event name (run with --list to get the valid event names).'%(c))
+
+    types_to_ignore = list()
+    if args.type_ignore:
+        args.type_ignore = [item for sublist in args.type_ignore for item in sublist]
+        for c in args.type_ignore:
+            p = re.search('^\/(.*)\/$', c)
+            if p:
+                pattern = p.group(1)
+            else:
+                pattern = "^%s$"%(c)
+            index=store.event_names[store.event_names.str.contains(pattern)].index.tolist()
+            types_to_ignore += index
+
+    threads_to_ignore = list()
+    if args.thread_ignore:
+        args.thread_ignore = [item for sublist in args.thread_ignore for item in sublist]
+        for c in args.thread_ignore:
+            p = re.search('^\/(.*)\/$', c)
+            if p:
+                pattern = p.group(1)
+            else:
+                pattern = "^%s$"%(c)
+            index=store.threads[store.threads.description.str.contains(pattern)].index.tolist()
+            threads_to_ignore += index
+            print(threads_to_ignore)
+
+    for trindex, t in store.threads.iterrows():
+        if trindex in threads_to_ignore:
+            continue
         if "M%d"%(t.node_id) not in paje_container_aliases:
             paje_container_aliases["M%d"%(t.node_id)] = PajeContainerCreate.PajeEvent(Time = 0.0000, Name = "M%d" % (t.node_id), Type=paje_ct, Container=paje_c_appli)
         if isinstance(t.vp_id, int):
@@ -102,27 +187,43 @@ if __name__ == '__main__':
                                                                                                           Type=paje_ct, Container=paje_container_aliases["M%d"%(t.node_id)])
         PajeSetState.PajeEvent(Time=0.000, Type=paje_st, Container=paje_container_aliases["M%dT%d"%(t.node_id,t.thread_id)], Value="Waiting", task_name="")
 
-    counter_events = list()
     sev = store.events.sort(['node_id', 'thread_id', 'begin'])
     for evr in store.events.iterrows():
         ev = evr[1]
-        if ev['flags'] & (1<<2) != 0:
-            counter_events.append( {'time': float(ev.begin), 'size':float(ev['info_start']['size']), 'node_id': ev.node_id } )
-            counter_events.append( {'time': float(ev.end), 'size': -1.0 * float(ev['info_start']['size']), 'node_id': ev.node_id } )
+        if (ev['type'] in types_to_ignore):
+            continue
+        if (ev['type'] in types_to_count):
+            type_name = store.event_names[ ev['type'] ]
+            info_name = '%s_start'%(type_name)
+            if(ev['flags'] & (1<<2) != 0) and ('size' in ev[info_name]):
+                counter_events[type_name]['events'].append( {'time': float(ev.begin), 'size':float(ev[info_name ]['size']), 'node_id': ev.node_id } )
+                counter_events[type_name]['events'].append( {'time': float(ev.end), 'size': -1.0 * float(ev[info_name ]['size']), 'node_id': ev.node_id } )
+            elif not counter_events[ev['type']]['error']:
+                counter_events[type_name]['error'] = True
+                warning('You requested to use counters for events of type %s, but such events are not marked as counter-types'%(type_name))
         else:
-            key = "hid=%d:did=%d:tid=%d"%(ev.handle_id,ev.type,ev.id)
-            if key in task_names.keys():
-                PajeSetState.PajeEvent(Time=float(ev.begin), Type=paje_st, Container=paje_container_aliases["M%dT%d"%(ev.node_id,ev.thread_id)],
-                                        Value=state_aliases[ev.type], task_name=task_names[key])
-            else:
-                PajeSetState.PajeEvent(Time=float(ev.begin), Type=paje_st, Container=paje_container_aliases["M%dT%d"%(ev.node_id,ev.thread_id)],
-                                        Value=state_aliases[ev.type], task_name=store.event_names[ev.type])
-            PajeSetState.PajeEvent(Time=float(ev.end), Type=paje_st, Container=paje_container_aliases["M%dT%d"%(ev.node_id,ev.thread_id)],
-                                   Value=paje_entity_waiting, task_name="Waiting")
+            #Don't forget to check if that container was ignored by the user
+            if (ev['flags'] & (1<<2) == 0) and ("M%dT%d"%(ev.node_id,ev.thread_id) in paje_container_aliases):
+                key = "hid=%d:did=%d:tid=%d"%(ev.handle_id,ev.type,ev.id)
+                if key in task_names.keys():
+                    PajeSetState.PajeEvent(Time=float(ev.begin), Type=paje_st, Container=paje_container_aliases["M%dT%d"%(ev.node_id,ev.thread_id)],
+                                           Value=state_aliases[ev.type], task_name=task_names[key])
+                else:
+                    PajeSetState.PajeEvent(Time=float(ev.begin), Type=paje_st, Container=paje_container_aliases["M%dT%d"%(ev.node_id,ev.thread_id)],
+                                           Value=state_aliases[ev.type], task_name=store.event_names[ev.type])
+                PajeSetState.PajeEvent(Time=float(ev.end), Type=paje_st, Container=paje_container_aliases["M%dT%d"%(ev.node_id,ev.thread_id)],
+                                       Value=paje_entity_waiting, task_name="Waiting")
 
-    counter_events.sort(cmp = lambda x, y: cmp(x['time'], y['time']) )
-    for ce in counter_events:
-        PajeAddVariable.PajeEvent(Time=float(ce['time']), Type=paje_vt1, Container=paje_container_aliases["M%d"%(ce['node_id'])], Value=ce['size'])
+    for name,cer in counter_events.iteritems():
+        if not cer['error']:
+            ce = cer['events']
+            ce.sort(cmp = lambda x, y: cmp(x['time'], y['time']) )
+            for e in ce:
+                #Check if that container was ignored by the user
+                if "M%d-%s"%(e['node_id'],cer['type_name']) in paje_container_aliases:
+                    PajeAddVariable.PajeEvent(Time=float(e['time']), Type=paje_vt,
+                                              Container=paje_container_aliases["M%d-%s"%(e['node_id'],cer['type_name'])],
+                                              Value=e['size'])
 
     paje.endTrace();
 
