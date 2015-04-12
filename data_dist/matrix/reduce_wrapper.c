3728ee1b7379c3214d4b08f9cd28df0ae6f37f75
George Bosilca <bosilca@icl.utk.edu>
2015-04-12 14:48:38 -0400

    Create arenas for the reductions.
    --HG--
    branch : devel

--- data_dist/matrix/reduce_wrapper.c
+++ data_dist/matrix/reduce_wrapper.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014 The University of Tennessee and The University
+ * Copyright (c) 2011-2015 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  */
@@ -7,6 +7,8 @@
 #include "dague_config.h"
 #include "dague_internal.h"
 #include "data_dist/matrix/matrix.h"
+#include "dague/utils/output.h"
+#include "arena.h"
 #include "reduce_col.h"
 #include "reduce_row.h"
 
@@ -16,10 +18,26 @@ dague_reduce_col_New( const tiled_matrix_desc_t* src,
                       dague_operator_t operator,
                       void* op_data )
 {
-    dague_handle_t* dague;
+    dague_reduce_col_handle_t* handle;
+    dague_datatype_t oldtype, newtype;
+    int newsize;
 
-    dague = (dague_handle_t*)dague_reduce_col_new( src, dest, operator, op_data, 0, 0, src->lnt, src->lmt );
-    return dague;
+    assert(src->mtype == dest->mtype);
+    if( -1 == dague_traslate_matrix_type(src->mtype, &oldtype) ) {
+        dague_output(0, "Unknown matrix type in %s.\n", __func__);
+        return NULL;
+    }
+    dague_type_create_contiguous(src->mb*src->nb, oldtype, &newtype);
+    MPI_Type_size(newtype, &newsize);
+
+    handle = dague_reduce_col_new( src, dest, operator, op_data, 0, 0, src->lnt, src->lmt );
+
+    dague_arena_construct(handle->arenas[DAGUE_reduce_col_DEFAULT_ARENA],
+                          newsize,
+                          DAGUE_ARENA_ALIGNMENT_SSE,
+                          newtype);
+
+    return (dague_handle_t*)handle;
 }
 
 void dague_reduce_col_Destruct( dague_handle_t *o )
@@ -33,10 +51,26 @@ dague_reduce_row_New( const tiled_matrix_desc_t* src,
                       dague_operator_t operator,
                       void* op_data )
 {
-    dague_handle_t* dague;
+    dague_reduce_row_handle_t* handle;
+    dague_datatype_t oldtype, newtype;
+    int newsize;
 
-    dague = (dague_handle_t*)dague_reduce_row_new( src, dest, operator, op_data );
-    return dague;
+    assert(src->mtype == dest->mtype);
+    if( -1 == dague_traslate_matrix_type(src->mtype, &oldtype) ) {
+        dague_output(0, "Unknown matrix type in %s.\n", __func__);
+        return NULL;
+    }
+    dague_type_create_contiguous(src->mb*src->nb, oldtype, &newtype);
+    MPI_Type_size(newtype, &newsize);
+
+    handle = dague_reduce_row_new( src, dest, operator, op_data, 0, 0, src->lnt, src->lmt );
+
+    dague_arena_construct(handle->arenas[DAGUE_reduce_row_DEFAULT_ARENA],
+                          newsize,
+                          DAGUE_ARENA_ALIGNMENT_SSE,
+                          newtype);
+
+    return (dague_handle_t*)handle;
 }
 
 void dague_reduce_row_Destruct( dague_handle_t *o )
