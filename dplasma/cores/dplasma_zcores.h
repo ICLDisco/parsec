2e10a396d73a9ff9b2cacbd35be5baff80da9e3f
Mathieu Faverge <mfaverge@eecs.utk.edu>
2015-09-01 13:12:06 -0400

    Cleanup dplasma_zcores.h
    --HG--
    branch : recursive

--- dplasma/cores/dplasma_zcores.h
+++ dplasma/cores/dplasma_zcores.h
@@ -13,22 +13,12 @@
 #include "data_dist/matrix/precision.h"
 #include "data_dist/matrix/matrix.h"
 #include <core_blas.h>
-#include <plasma.h>
 
 int blgchase_ztrdv2(int NT, int N, int NB,
                    dague_complex64_t *A1, dague_complex64_t *A2,
                    dague_complex64_t *V1, dague_complex64_t *TAU1,
                    dague_complex64_t *V2, dague_complex64_t *TAU2,
                    int sweep, int id, int blktile);
-
-int CORE_zgetrf_rectil_1thrd(const PLASMA_desc A, int *IPIV);
-void dplasmacore_zgetrf_rectil_init(void);
-int dplasmacore_zgetrf_rectil(volatile dague_complex64_t *amax1buf,
-                              const tiled_matrix_desc_t *A, int *IPIV, int *info);
-
-int CORE_zplssq(int M, int N,
-                dague_complex64_t *A, int LDA,
-                double *scale, double *sumsq);
 
 int CORE_zamax(PLASMA_enum storev, PLASMA_enum uplo, int M, int N,
                const PLASMA_Complex64_t *A, int lda, double *work);
