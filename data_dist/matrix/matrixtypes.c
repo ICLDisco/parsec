225688466c4d2353b9278dc1b68cbe05df826977
Mathieu Faverge <mfaverge@eecs.utk.edu>
2015-11-15 15:33:59 +0300

    Silent warning
    --HG--
    branch : devel

--- data_dist/matrix/matrixtypes.c
+++ data_dist/matrix/matrixtypes.c
@@ -181,7 +181,7 @@ int dague_matrix_add2arena( dague_arena_t *arena, dague_datatype_t oldtype,
                             unsigned int m, unsigned int n, unsigned int ld,
                             size_t alignment, int resized )
 {
-    dague_datatype_t newtype = NULL;
+    dague_datatype_t newtype = DAGUE_DATATYPE_NULL;
     MPI_Aint extent = 0;
     int rc;
 
