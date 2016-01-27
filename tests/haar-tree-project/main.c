0580b7ef8b060f1c94b1f7f3311590eed3b98f8a
Thomas Herault <herault@icl.utk.edu>
2016-01-27 17:22:45 -0500

    Project / Walk work in distributed, except for issue #86 related problems.

--- tests/haar-tree-project/main.c
+++ tests/haar-tree-project/main.c
@@ -15,57 +15,113 @@ extern int optopt;
 extern int opterr;
 extern int optreset;
 
+#define SUM_VALUE 0xbdae8ab0a45fc32e
+
 typedef struct {
     int n;
     int l;
     double s;
 } leaf_t;
 
-static void walker_checksum_node(tree_dist_t *tree, int nid, int n, int l, double s, double d, void *param)
-{
-    double *sum = (double*)param;
-    *sum += s + d;
-    *sum += l ^ n;
-    (void)tree;
-    (void)nid;
-}
-
-static void walker_checksum_child(tree_dist_t *tree, int nid, int pn, int pl, int cn, int cl, void *param)
-{
-    double *sum = (double*)param;
-    *sum += pl ^ cl;
-    *sum += pn ^ cn;
-    (void)tree;
-    (void)nid;
-}
-
 static void cksum_node_fn(tree_dist_t *tree, node_t *node, int n, int l, void *param)
 {
-    double *cksum = (double*)param;
-    *cksum += node->s + node->d;
-    *cksum += l ^ n;
+    uint64_t *cksum = (uint64_t*)param;
+    uint64_t up = 0;
+    uint64_t nv, ov;
+    up ^= *(uint64_t*)&node->s;
+    up ^= *(uint64_t*)&node->d;
+    up ^= (((uint64_t)l)<<32) | (uint64_t)n;
+    do {
+        ov = *cksum;
+        nv = ov ^ up;
+    } while(!dague_atomic_cas(cksum, ov, nv));
     (void)tree;
+}
+
+typedef struct {
+    size_t size;
+    char *value;
+    int32_t cur_pos;
+    pthread_rwlock_t resize_lock;
+} redim_string_t;
+
+static void rs_add(redim_string_t *rs, const char *format, ...)
+{
+    int sz, ns;
+    int32_t my_pos;
+    va_list ap;
+
+    /** Compute size needed */
+    va_start(ap, format);
+    sz = vsnprintf(NULL, 0, format, ap) + 1;
+    va_end(ap);
+
+    my_pos = dague_atomic_add_32b(&rs->cur_pos, sz-1) - sz + 1;
+    assert(my_pos>=0);
+    for(;;) {
+        if( my_pos + sz < rs->size ) {
+            pthread_rwlock_rdlock(&rs->resize_lock);
+            va_start(ap, format);
+            vsnprintf(rs->value + my_pos, sz, format, ap);
+            va_end(ap);
+            pthread_rwlock_unlock(&rs->resize_lock);
+            return;
+        }
+
+        pthread_rwlock_wrlock(&rs->resize_lock);
+        if( my_pos + sz > rs->size ) {
+            for(rs->size = 2*rs->size;
+                rs->size < my_pos + sz;
+                rs->size *= 2) /* nothing */;
+            rs->value = (char*)realloc(rs->value, rs->size);
+        }
+        pthread_rwlock_unlock(&rs->resize_lock);
+    }
+}
+
+redim_string_t *rs_new(void)
+{
+    redim_string_t *rs = (redim_string_t*)malloc(sizeof(redim_string_t));
+    rs->size = 1;
+    rs->value = malloc(1);
+    rs->value[0] = '\0';
+    rs->cur_pos = 0;
+    pthread_rwlock_init(&rs->resize_lock, NULL);
+    return rs;
+}
+
+void rs_free(redim_string_t *rs)
+{
+    pthread_rwlock_destroy(&rs->resize_lock);
+    free(rs->value);
+    free(rs);
+}
+
+char *rs_string(redim_string_t *rs)
+{
+    return rs->value;
 }
 
 static void print_node_fn(tree_dist_t *tree, node_t *node, int n, int l, void *param)
 {
+    redim_string_t *rs = (redim_string_t*)param;
     if( NULL != node )
-        printf("N_%d_%d [label=\"[%d,%d](%g, %g)\"];\n", n, l, n, l, node->s, node->d);
+        rs_add(rs, "N_%d_%d [label=\"[%d,%d](%g, %g)\"];\n", n, l, n, l, node->s, node->d);
     else
-        printf("N_%d_%d [label=\"[%d,%d](-, -)\"];\n", n, l, n, l);
+        rs_add(rs, "N_%d_%d [label=\"[%d,%d](-, -)\"];\n", n, l, n, l);
     (void)param;
     (void)tree;
 }
 
 static void print_link_fn(tree_dist_t *tree, node_t *node, int n, int l, void *param)
 {
+    redim_string_t *rs = (redim_string_t*)param;
     if(n > 0)
-        printf("N_%d_%d -> N_%d_%d;\n", n, l, n-1, l/2);
+        rs_add(rs, "N_%d_%d -> N_%d_%d;\n", n, l, n-1, l/2);
     (void)param;
     (void)tree;
     (void)node;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -77,11 +133,12 @@ int main(int argc, char *argv[])
     dague_walk_handle_t *walker;
     dague_arena_t arena;
     node_t node;
-    int do_checks = 0;
+    int do_checks = 0, be_verbose = 0;
     int pargc = 0, i, dashdash = -1;
     char **pargv;
     int ret, ch;
-    double cksum = 0.0;
+    uint64_t cksum = 0;
+    redim_string_t *rs;
 
 #if defined(DAGUE_HAVE_MPI)
     {
@@ -115,10 +172,13 @@ int main(int argc, char *argv[])
     dague = dague_init(-1, &pargc, &pargv);
 
 
-    while ((ch = getopt(argc, argv, "xd:")) != -1) {
+    while ((ch = getopt(argc, argv, "xvd:")) != -1) {
         switch (ch) {
         case 'x':
             do_checks = 1;
+            break;
+        case 'v':
+            be_verbose = 1;
             break;
         case 'd':
             if( rank == atoi(optarg) ) {
@@ -135,10 +195,11 @@ int main(int argc, char *argv[])
         case '?':
         default:
             fprintf(stderr,
-                    "Usage: %s [-x] [-d rank -d rank -d rank] -- <parsec arguments>\n"
+                    "Usage: %s [-x] [-v] [-d rank -d rank -d rank] -- <parsec arguments>\n"
                     "   Implement the Project operation to build a Haar tree using PaRSEC JDFs\n"
                     "   if -x, create a haar-tree, and check that the tree correspond to a pre-computed checksum\n"
                     "   otherwise, output A.dot, a DOT file of the created tree\n"
+                    "   if -v, print some information on what task is executed by what rank\n"
                     "   -d rank will make rank d wait for a debugger to attach\n",
                     argv[0]);
             exit(1);
@@ -146,11 +207,6 @@ int main(int argc, char *argv[])
     }
 
     treeA = tree_dist_create_empty(rank, world);
-    if( 0 == rank ) {
-        node.s = 0.0;
-        node.d = 0.0;
-        tree_dist_insert_node(treeA, &node, 0, 0);
-    }
 
     two_dim_block_cyclic_init(&fakeDesc, matrix_RealFloat, matrix_Tile,
                               world, rank, 1, 1, world, world, 0, 0, world, world, 1, 1, 1);
@@ -160,36 +216,57 @@ int main(int argc, char *argv[])
                            DAGUE_ARENA_ALIGNMENT_SSE,
                            dague_datatype_float_t
                          );
-    project = dague_project_new(treeA, &treeA->super, world, (dague_ddesc_t*)&fakeDesc, 1e-3, 0);
+    project = dague_project_new(treeA, &treeA->super, world, (dague_ddesc_t*)&fakeDesc, 1e-3, 0, be_verbose);
     project->arenas[DAGUE_project_DEFAULT_ARENA] = &arena;
     dague_enqueue(dague, &project->super);
     dague_context_wait(dague);
 
-    printf("\n\n\n\n");
-
     if( do_checks ) {
         walker = dague_walk_new(treeA, &treeA->super, world, (dague_ddesc_t*)&fakeDesc,
                                 &cksum, cksum_node_fn, NULL,
-                                0);
+                                0, be_verbose);
     } else {
+        rs = rs_new();
         walker = dague_walk_new(treeA, &treeA->super, world, (dague_ddesc_t*)&fakeDesc,
-                                NULL, print_node_fn, print_link_fn,
-                                0);
+                                rs, print_node_fn, print_link_fn,
+                                0, be_verbose);
     }
     walker->arenas[DAGUE_walk_DEFAULT_ARENA] = &arena;
     dague_enqueue(dague, &walker->super);
     dague_context_wait(dague);
 
-    if( world > 1 ) {
-        printf("Distributed walking on tree not enabled yet.\n");
+    ret = 0;
+    if( do_checks ) {
+        uint64_t sum = 0;
+        MPI_Reduce(&cksum, &sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+        if( rank == 0 ) {
+            if( sum != SUM_VALUE ) {
+                printf("****  Sum = %llx instead of %llx\n", sum, SUM_VALUE);
+                ret = 1;
+            }
+        }
     } else {
-        if( do_checks ) {
-            double sum = 0.0;
-            walk_tree(&walker_checksum_node, &walker_checksum_child, &sum, treeA);
-            ret = (23589.2 == sum);
+        if( rank == 0 ) {
+            int r;
+            printf("digraph G {\n");
+            printf("%s", rs_string(rs));
+            rs_free(rs);
+            for(r = 1; r < world; r++) {
+                char *buff;
+                int size;
+                MPI_Recv(&size, 1, MPI_INT, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                buff = (char *)malloc(size);
+                MPI_Recv(buff, size, MPI_CHAR, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                printf("%s", buff);
+                free(buff);
+            }
+            printf("}\n");
         } else {
-            tree_dist_to_dotfile(treeA, "A.dot");
-            ret = 0;
+            char *buff = rs_string(rs);
+            int size = strlen(buff)+1;
+            MPI_Send(&size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(buff, size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+            rs_free(rs);
         }
     }
 
