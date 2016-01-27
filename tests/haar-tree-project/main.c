458d22a6d810f416a14e470e20fcf21f358183d5
Thomas Herault <herault@icl.utk.edu>
2016-01-26 22:58:22 -0500

    Allow distributed runs over haar tree projects. Rewrite tree_dist.c accordingly.

--- tests/haar-tree-project/main.c
+++ tests/haar-tree-project/main.c
@@ -1,9 +1,16 @@
-#include "dague.h"
-#include "data_dist/matrix/matrix.h"
-#include "dague/arena.h"
+#include <dague.h>
+#include <data_dist/matrix/matrix.h>
+#include <data_dist/matrix/two_dim_rectangle_cyclic.h>
+#include <dague/arena.h>
 
 #include "tree_dist.h"
 #include "project.h"
+#include <unistd.h>
+extern char *optarg;
+extern int optind;
+extern int optopt;
+extern int opterr;
+extern int optreset;
 
 typedef struct {
     int n;
@@ -11,7 +18,7 @@ typedef struct {
     double s;
 } leaf_t;
 
-static void walker_checksum_node(tree_dist_t *tree, int nid, int l, int n, double s, double d, void *param)
+static void walker_checksum_node(tree_dist_t *tree, int nid, int n, int l, double s, double d, void *param)
 {
     double *sum = (double*)param;
     *sum += s + d;
@@ -20,7 +27,7 @@ static void walker_checksum_node(tree_dist_t *tree, int nid, int l, int n, doubl
     (void)nid;
 }
 
-static void walker_checksum_child(tree_dist_t *tree, int nid, int pl, int pn, int cl, int cn, void *param)
+static void walker_checksum_child(tree_dist_t *tree, int nid, int pn, int pl, int cn, int cl, void *param)
 {
     double *sum = (double*)param;
     *sum += pl ^ cl;
@@ -34,13 +41,14 @@ int main(int argc, char *argv[])
     dague_context_t* dague;
     int rank, world;
     tree_dist_t *treeA;
+    two_dim_block_cyclic_t fakeDesc;
     dague_project_handle_t *project;
     dague_arena_t arena;
     node_t node;
     int do_checks = 0;
     int pargc = 0, i, dashdash = -1;
     char **pargv;
-    int ret;
+    int ret, ch;
 
 #if defined(DAGUE_HAVE_MPI)
     {
@@ -73,22 +81,53 @@ int main(int argc, char *argv[])
     }
     dague = dague_init(-1, &pargc, &pargv);
 
-    if( argc > 1 && !strcmp(argv[1], "-x" )) {
-        do_checks = 1;
+
+    while ((ch = getopt(argc, argv, "xd:")) != -1) {
+        switch (ch) {
+        case 'x':
+            do_checks = 1;
+            break;
+        case 'd':
+            if( rank == atoi(optarg) ) {
+                char hostname[256];
+                gethostname(hostname, 256);
+                fprintf(stderr, "Rank %d is pid %d on host %s -- Waiting for debugger to attach\n",
+                        rank, getpid(), hostname);
+                ret = 1;
+                while(ret != 0) {
+                    sleep(1);
+                }
+            }
+            break;
+        case '?':
+        default:
+            fprintf(stderr,
+                    "Usage: %s [-x] [-d rank -d rank -d rank] -- <parsec arguments>\n"
+                    "   Implement the Project operation to build a Haar tree using PaRSEC JDFs\n"
+                    "   if -x, create a haar-tree, and check that the tree correspond to a pre-computed checksum\n"
+                    "   otherwise, output A.dot, a DOT file of the created tree\n"
+                    "   -d rank will make rank d wait for a debugger to attach\n",
+                    argv[0]);
+            exit(1);
+        }
     }
 
     treeA = tree_dist_create_empty(rank, world);
-    node.s = 0.0;
-    node.d = 0.0;
-    tree_dist_insert_node(treeA, &node, 0, 0);
+    if( 0 == rank ) {
+        node.s = 0.0;
+        node.d = 0.0;
+        tree_dist_insert_node(treeA, &node, 0, 0);
+    }
+
+    two_dim_block_cyclic_init(&fakeDesc, matrix_RealFloat, matrix_Tile,
+                              world, rank, 1, 1, world, world, 0, 0, world, world, 1, 1, 1);
 
     dague_arena_construct( &arena,
                            2 * dague_datadist_getsizeoftype(matrix_RealFloat),
                            DAGUE_ARENA_ALIGNMENT_SSE,
                            dague_datatype_float_t
                          );
-
-    project = dague_project_new(treeA, &treeA->super, 1e-3);
+    project = dague_project_new(treeA, &treeA->super, world, (dague_ddesc_t*)&fakeDesc, 1e-3);
     project->arenas[DAGUE_project_DEFAULT_ARENA] = &arena;
     dague_enqueue(dague, &project->super);
     dague_context_wait(dague);
--- tests/haar-tree-project/tree_dist.h
+++ tests/haar-tree-project/tree_dist.h
@@ -20,28 +20,10 @@ struct node_s {
     double s;
 };
 
-int tree_dist_number_of_potential_nodes(tree_dist_t *treeA);
-int tree_dist_number_of_nodes(tree_dist_t *treeA);
-int tree_dist_instanciate_node(tree_dist_t *tree, int pnid);
-int tree_dist_lookup_node(tree_dist_t *tree, int l, int n);
-int tree_dist_level_of_node(tree_dist_t *tree, int nid);
-int tree_dist_position_of_node(tree_dist_t *tree, int nid);
-int tree_dist_parent_of_node(tree_dist_t *tree, int nid);
-int tree_dist_has_left_child(tree_dist_t *tree, int nid);
-int tree_dist_has_right_child(tree_dist_t *tree, int nid);
-int tree_dist_left_child_of_node(tree_dist_t *tree, int nid);
-int tree_dist_right_child_of_node(tree_dist_t *tree, int nid);
-static inline int tree_dist_is_leaf(tree_dist_t *tree, int nid) {
-    return tree_dist_left_child_of_node(tree, nid) == -1 &&
-        tree_dist_right_child_of_node(tree, nid) == -1;
-}
 void tree_copy_node(tree_dist_t *dst_tree, int dst_nid, node_t *src);
-int tree_dist_depth(tree_dist_t *tree);
-
 void tree_dist_insert_node(tree_dist_t *tree, node_t *node, int l, int n);
 void tree_dist_insert_data(tree_dist_t *tree, dague_data_t *data, int l, int n);
 tree_dist_t *tree_dist_create_empty(int myrank, int nodes);
-int tree_dist_depth(tree_dist_t *tree);
 
 int tree_dist_to_dotfile(tree_dist_t *tree, char *filename);
 
@@ -56,18 +38,10 @@ struct tree_dist_node_s {
     /** nodes are linked by nid in the nodes array
      *  and by (l, n) in the hash table. next_in_hash is used to
      *  handle collisions on the hash buckets. */
-    tree_dist_node_t *next_in_hash;
-    int nid;
-    int l, n;
+    int n, l;
 
     dague_data_t *data;
     int rank, vpid;
-    int nid_parent, nid_left, nid_right;
-};
-
-struct tree_hash_bucket_entry_s {
-    tree_dist_node_t *first;
-    pthread_rwlock_t rw_lock;
 };
 
 /** This structure will hold the actual data */
@@ -84,21 +58,11 @@ struct tree_dist_s {
     /** Actual memory in which the node_t elements reside */
     pthread_mutex_t     buffer_lock;
     tree_buffer_t      *buffers;
-    
-    /** Array structure: holds nodes one after the other, nids going from 0 to nb_nodes-1 */
-    pthread_mutex_t     resize_lock;
-    size_t              allocated_nodes;        /**< Size of nodes */
-    size_t              nb_nodes;               /**< Number of non-NULL elementsi n nodes */
+
+    /** Hash structure: holds nodes one after the other */
+    pthread_rwlock_t    resize_lock;
+    size_t              allocated_nodes;        /**< Size of of nodes */
     tree_dist_node_t  **nodes;
-
-    /** Hash table structure. (l, n) is used as key. */
-    size_t                     hash_size;
-    tree_hash_bucket_entry_t  *hash_buckets;
-
-    /** High-level information about the tree, updated by a call to tree_refinalize() */
-    int dirty;                                  /**< true iff the information below is not up to date */
-    int depth;                                  /**< max depth of tree */
-    size_t            nb_potential_nodes;
 };
 
 #endif
