6004190fcb3de35f5293e9728b42a5e7f1e2793b
Thomas Herault <herault@icl.utk.edu>
2016-09-28 13:46:24 -0400

    A better fix?

--- dague/dague.c
+++ dague/dague.c
@@ -166,14 +166,13 @@ static void* __dague_thread_init( __dague_temporary_thread_initialization_t* sta
     int pi;
 
     /* don't use DAGUE_THREAD_IS_MASTER, it is too early and we cannot yet allocate the eu struct */
-    if( (0 == startup->virtual_process) && (0 == startup->th_id) && dague_runtime_bind_main_thread ) {
+    if( (0 != startup->virtual_process) || (0 != startup->th_id) || dague_runtime_bind_main_thread ) {
         /* Bind to the specified CORE */
         dague_bindthread(startup->bindto, startup->bindto_ht);
         DAGUE_DEBUG_VERBOSE(10, dague_debug_output, "VP %i : bind thread %i.%i on core %i [HT %i]",
                             startup->virtual_process->vp_id, startup->virtual_process->vp_id,
                             startup->th_id, startup->bindto, startup->bindto_ht);
     } else {
-        dague_bindthread(startup->bindto, startup->bindto_ht);
         DAGUE_DEBUG_VERBOSE(10, dague_debug_output, "VP %i : Don't bind the main thread %i.%i",
                             startup->virtual_process->vp_id, startup->virtual_process->vp_id,
                             startup->th_id);
