152e0875bed03d03dec6641b737b86740f285699
Aurelien Bouteiller <bouteill@icl.utk.edu>
2017-07-11 19:15:10 +0000

    Merged in abouteiller/parsec-aurelien/ci-ctest (pull request #139)
    Ci ctest
    
    * Rationalize test names and labels to ease selecting them from scripts
    
    * touch_exf.F90 edited online with Bitbucket
    
    Approved-by: Aurelien Bouteiller <bouteill@icl.utk.edu>
    Approved-by: George Bosilca <bosilca@icl.utk.edu>

--- tests/Testings.cmake
+++ tests/Testings.cmake
@@ -6,28 +6,30 @@ add_test(unit_startup1 ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=10 -k=10 -v=5)
 add_test(unit_startup2 ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=20 -k=30 -v=5)
 add_test(unit_startup3 ${SHM_TEST_CMD_LIST} ./startup -i=30 -j=30 -k=30 -v=5)
 add_test(unit_reduce ${SHM_TEST_CMD_LIST} ./reduce)
-add_test(strange ${SHM_TEST_CMD_LIST} ./strange)
+add_test(unit_strange ${SHM_TEST_CMD_LIST} ./strange)
+add_test(unit_touchf  ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
 
-add_test(unit_touch    ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
+#
+# Fortran Testings
+#
 if(CMAKE_Fortran_COMPILER_WORKS)
   if(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
-    add_test(unit_touchf  ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
+    add_test(unit_touchf  ${SHM_TEST_CMD_LIST} ./touch_exf -v=5)
   endif(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
 endif(CMAKE_Fortran_COMPILER_WORKS)
-
-
-# The headnode lack GPUs so we need MPI in order to get the test to run on
-# one of the nodes.
-if (CUDA_FOUND AND MPI_C_FOUND)
-endif (CUDA_FOUND AND MPI_C_FOUND)
 
 #
 # Distributed Memory Testings
 #
 if( MPI_C_FOUND )
-  find_program(BINTRUE true)
-  # Check MPI
-  add_test(unit_mpi_test ${MPI_TEST_CMD_LIST} -np 8 ${BINTRUE})
-
 endif( MPI_C_FOUND )
+
+#
+# CUDA Testings
+#
+if (CUDA_FOUND AND MPI_C_FOUND)
+# The headnode lack GPUs so we need MPI in order to get the test to run on
+# one of the nodes.
+endif (CUDA_FOUND AND MPI_C_FOUND)
+
 
--- tests/merge_sort/Testings.cmake
+++ tests/merge_sort/Testings.cmake
@@ -1,5 +1,5 @@
-add_test(shm_merge_sort ${SHM_TEST_CMD_LIST} ./merge_sort)
+add_test(unit_merge_sort_shm ${SHM_TEST_CMD_LIST} ./merge_sort)
 if( MPI_C_FOUND )
-  add_test(mpi_merge_sort ${MPI_TEST_CMD_LIST} -np 4 ./merge_sort)
+  add_test(unit_merge_sort_mpi ${MPI_TEST_CMD_LIST} 4 ./merge_sort)
 endif( MPI_C_FOUND)
 
