e44cbed6c378da30666412c2efc6e6ac58fd1d94
Aurelien Bouteiller <bouteill@icl.utk.edu>
2017-07-12 14:53:29 -0400

    Add missing _shm tags to ctests

--- tests/Testings.cmake
+++ tests/Testings.cmake
@@ -2,19 +2,19 @@
 # Shared Memory Testings
 #
 
-add_test(unit_startup1 ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=10 -k=10 -v=5)
-add_test(unit_startup2 ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=20 -k=30 -v=5)
-add_test(unit_startup3 ${SHM_TEST_CMD_LIST} ./startup -i=30 -j=30 -k=30 -v=5)
-add_test(unit_reduce ${SHM_TEST_CMD_LIST} ./reduce)
-add_test(unit_strange ${SHM_TEST_CMD_LIST} ./strange)
-add_test(unit_touchf  ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
+add_test(unit_startup1_shm ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=10 -k=10 -v=5)
+add_test(unit_startup2_shm ${SHM_TEST_CMD_LIST} ./startup -i=10 -j=20 -k=30 -v=5)
+add_test(unit_startup3_shm ${SHM_TEST_CMD_LIST} ./startup -i=30 -j=30 -k=30 -v=5)
+add_test(unit_reduce_shm ${SHM_TEST_CMD_LIST} ./reduce)
+add_test(unit_strange_shm ${SHM_TEST_CMD_LIST} ./strange)
+add_test(unit_touch_shm  ${SHM_TEST_CMD_LIST} ./touch_ex -v=5)
 
 #
 # Fortran Testings
 #
 if(CMAKE_Fortran_COMPILER_WORKS)
   if(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
-    add_test(unit_touchf  ${SHM_TEST_CMD_LIST} ./touch_exf -v=5)
+    add_test(unit_touch_fortran_shm  ${SHM_TEST_CMD_LIST} ./touch_exf -v=5)
   endif(CMAKE_Fortran_COMPILER_SUPPORTS_F90)
 endif(CMAKE_Fortran_COMPILER_WORKS)
 
