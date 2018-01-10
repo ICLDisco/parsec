ICLDisco/parsec

commit d699b3818049c3b8bc16e0559245622edf776ff2

Clear definition of the supported taskpool types. Cleanup the uses of the taskpool->taskpool_type by defining the different possibilities and making sure they are consistently used. Ensure the DTD taskclass provide the task_key member.
Signed-off-by: George Bosilca <bosilca@icl.utk.edu>

parsec/mca/pins/task_profiler/pins_task_profiler_module.c
@@ -144,24 +144,18 @@ static void pins_thread_fini_task_profiler(struct parsec_execution_stream_s * es
  PINS CALLBACKS
  */
 
-static void task_profiler_release_deps_begin(struct parsec_execution_stream_s*     es,
-                                             struct parsec_task_s*               task,
-                                             struct parsec_pins_next_callback_s* cb_data)
+static void
+task_profiler_release_deps_begin(struct parsec_execution_stream_s*   es,
+                                 struct parsec_task_s*               task,
+                                 struct parsec_pins_next_callback_s* cb_data)
 {
-    uint64_t key;
     int32_t rd_fid = task->task_class->task_class_id;
 
-    if( 0 == task->taskpool->taskpool_type ) { /* PTG */
-        key = (*task->task_class->key)(task->taskpool, task->locals);
-    } else { /* DTD */
-        key = task->locals[0].value;
-    }
-
     PARSEC_PROFILING_TRACE(es->es_profile,
-                          release_deps_trace_keyin,
-                          key,
-                          task->taskpool->taskpool_id,
-                          (void *)&rd_fid);
+                           release_deps_trace_keyin,
+                           (*task->task_class->key)(task->taskpool, task->locals)
+                           task->taskpool->taskpool_id,
+                           (void *)&rd_fid);
 
     (void)cb_data;
 }
@@ -171,18 +165,11 @@ task_profiler_release_deps_end(struct parsec_execution_stream_s*   es,
                                struct parsec_task_s*               task,
                                struct parsec_pins_next_callback_s* cb_data)
 {
-    uint64_t key;
     int32_t rd_fid = task->task_class->task_class_id;
-
-    if( 0 == task->taskpool->taskpool_type ) { /* PTG */
-        key = (*task->task_class->key)(task->taskpool, task->locals);
-    } else { /* DTD */
-        key = task->locals[0].value;
-    }
 
     PARSEC_PROFILING_TRACE(es->es_profile,
                            release_deps_trace_keyout,
-                           key,
+                           (*task->task_class->key)(task->taskpool, task->locals),
                            task->taskpool->taskpool_id,
                            (void*)&rd_fid);
     (void)cb_data;

parsec/devices/cuda/dev_cuda.c
@@ -324,7 +324,7 @@ parsec_cuda_taskpool_register(parsec_device_t* device, parsec_taskpool_t* tp)
      * user to write the code to assess this.
      */
     assert(PARSEC_DEV_CUDA == device->type);
-    for( i = 0; i < tp->nb_task_classes; i++ ) {
+    for( i = 0; NULL != tp->task_classes_array[i]; i++ ) {
         const parsec_task_class_t* tc = tp->task_classes_array[i];
         __parsec_chore_t* chores = (__parsec_chore_t*)tc->incarnations;
         for( dev_mask = j = 0; NULL != chores[j].hook; j++ ) {

parsec/interfaces/ptg/ptg-compiler/jdf2c.c
@@ -3468,7 +3468,7 @@ static void jdf_generate_constructor( const jdf_t* jdf )
             "  __parsec_tp->super.super.task_classes_array = (const parsec_task_class_t**)\n"
             "              malloc(2 * __parsec_tp->super.super.nb_task_classes * sizeof(parsec_task_class_t*));\n"
             "  __parsec_tp->super.super.nb_tasks = 1;\n"
-            "  __parsec_tp->super.super.taskpool_type = 0;\n"
+            "  __parsec_tp->super.super.taskpool_type = PARSEC_TASKPOOL_TYPE_PTG;\n"
             "  __parsec_tp->super.super.nb_pending_actions = 1 + __parsec_tp->super.super.nb_task_classes;  /* for the startup tasks */\n"
             "  __parsec_tp->sync_point = __parsec_tp->super.super.nb_task_classes;\n"
             "  __parsec_tp->startup_queue = NULL;\n"

parsec/interfaces/superscalar/insert_function.c
@@ -95,7 +95,7 @@ static parsec_hook_return_t
 complete_hook_of_dtd(parsec_execution_stream_t *,
                      parsec_task_t *);
 
-static uint32_t hash_key (uintptr_t key, uint32_t hash_size, void *data);
+static uint32_t hash_key(uintptr_t key, uint32_t hash_size, void *data);
 
 inline int parsec_dtd_task_is_local(parsec_dtd_task_t *task) { return task->rank == task->super.taskpool->context->my_rank;}
 inline int parsec_dtd_task_is_remote(parsec_dtd_task_t *task) { return !parsec_dtd_task_is_local(task);}
@@ -447,8 +447,8 @@ parsec_dtd_data_flush(parsec_taskpool_t *tp, parsec_dtd_tile_t *tile)
     assert(tile->flushed == NOT_FLUSHED);
     parsec_dtd_tile_retain(tile);
     parsec_dtd_taskpool_insert_task( tp, parsec_dtd_copy_data_to_matrix, 0, "Copy_data_in_dist",
-                        PASSED_BY_REF,    tile, INOUT | AFFINITY,
-                        0 );
+                                     PASSED_BY_REF, tile, INOUT | AFFINITY,
+                                     0 );
     tile->flushed = FLUSHED;
     parsec_dtd_tile_remove( tile->dc, tile->key );
     parsec_dtd_tile_release( tile );
@@ -729,37 +729,31 @@ fill_color(int index, int colorspace)
  */
 void
 parsec_dtd_add_profiling_info( parsec_taskpool_t *tp,
-                    int task_class_id, char* name )
+                               int task_class_id, char* name )
 {
     char *str = fill_color(task_class_id, PARSEC_DTD_NB_TASK_CLASSES);
     parsec_profiling_add_dictionary_keyword(name, str,
-                                           sizeof(parsec_profile_data_collection_info_t),
-                                           PARSEC_PROFILE_DATA_COLLECTION_INFO_CONVERTOR,
-                                           (int *) &tp->profiling_array[0 +
-                                                                                  2 *
-                                                                                  task_class_id
-                                                                                  /* start key */
-                                                                                  ],
-                                           (int *) &tp->profiling_array[1 +
-                                                                                  2 *
-                                                                                  task_class_id
-                                                                                  /*  end key */
-                                                                                  ]);
+                                            sizeof(parsec_profile_data_collection_info_t),
+                                            PARSEC_PROFILE_DATA_COLLECTION_INFO_CONVERTOR,
+                                            (int *) &tp->profiling_array[0 +
+                                                                         2 * task_class_id]  /* start key */,
+                                            (int *) &tp->profiling_array[1 +
+                                                                         2 * task_class_id]  /*  end key */ );
     free(str);
 }
 
 void
 parsec_dtd_add_profiling_info_generic( parsec_taskpool_t *tp,
-                            char* name,
-                            int *keyin, int *keyout)
+                                       char* name,
+                                       int *keyin, int *keyout)
 {
     (void)tp;
     char *str = fill_color(*keyin, PARSEC_DTD_NB_TASK_CLASSES);
     parsec_profiling_add_dictionary_keyword(name, str,
-                                           sizeof(parsec_profile_data_collection_info_t),
-                                           PARSEC_PROFILE_DATA_COLLECTION_INFO_CONVERTOR,
-                                           keyin,
-                                           keyout);
+                                            sizeof(parsec_profile_data_collection_info_t),
+                                            PARSEC_PROFILE_DATA_COLLECTION_INFO_CONVERTOR,
+                                            keyin,
+                                            keyout);
     free(str);
 }
 
@@ -1231,11 +1225,12 @@ static symbol_t symb_dtd_taskid = {
 };
 
 /**
- * To make it consistent with PaRSEC we need to intialize and have this function
- * we do not use this at this point
+ * The task class key generator function. For DTD the key is stored in the
+ * first assignment value of the task.
  */
-static inline uint64_t DTD_identity_hash(const parsec_dtd_taskpool_t * __tp,
-                                         const assignment_t * assignments)
+static inline uint64_t
+DTD_taskclass_task_key(const parsec_dtd_taskpool_t * __tp,
+                       const assignment_t * assignments)
 {
     (void)__tp;
     return (uint64_t)assignments[0].value;
@@ -1305,16 +1300,16 @@ parsec_dtd_taskpool_new(void)
 
     parsec_dtd_taskpool_retain((parsec_taskpool_t *)__tp);
 
-    __tp->super.context          = NULL;
-    __tp->super.on_enqueue       = parsec_dtd_enqueue_taskpool;
-    __tp->super.on_enqueue_data  = NULL;
-    __tp->super.on_complete      = NULL;
-    __tp->super.on_complete_data = NULL;
-    __tp->super.devices_mask     = PARSEC_DEVICES_ALL;
-    __tp->super.nb_tasks         = PARSEC_RUNTIME_RESERVED_NB_TASKS;
-    __tp->super.taskpool_type       = 1;  /* Indicating this is a taskpool for dtd tasks */
-    __tp->super.nb_pending_actions  = 0;  /* For the future tasks that will be inserted */
-    __tp->super.nb_task_classes     = 0;
+    __tp->super.context            = NULL;
+    __tp->super.on_enqueue         = parsec_dtd_enqueue_taskpool;
+    __tp->super.on_enqueue_data    = NULL;
+    __tp->super.on_complete        = NULL;
+    __tp->super.on_complete_data   = NULL;
+    __tp->super.devices_mask       = PARSEC_DEVICES_ALL;
+    __tp->super.nb_tasks           = PARSEC_RUNTIME_RESERVED_NB_TASKS;
+    __tp->super.taskpool_type      = PARSEC_TASKPOOL_TYPE_DTD;  /* Indicating this is a taskpool for dtd tasks */
+    __tp->super.nb_pending_actions = 0;  /* For the future tasks that will be inserted */
+    __tp->super.nb_task_classes    = 0;
     __tp->super.update_nb_runtime_task = parsec_dtd_update_runtime_task;
 
     for(i = 0; i < vpmap_get_nb_vp(); i++) {
@@ -2240,7 +2235,7 @@ set_dependencies_for_function(parsec_taskpool_t* tp,
  */
 parsec_task_class_t*
 parsec_dtd_create_task_class( parsec_dtd_taskpool_t *__tp, parsec_dtd_funcptr_t* fpointer,
-                              char* name, int count_of_params, long unsigned int size_of_param,
+                              const char* name, int count_of_params, long unsigned int size_of_param,
                               int flow_count )
 {
     parsec_dtd_task_class_t *dtd_tc = (parsec_dtd_task_class_t *) calloc(1, sizeof(parsec_dtd_task_class_t));
@@ -2281,7 +2276,7 @@ parsec_dtd_create_task_class( parsec_dtd_taskpool_t *__tp, parsec_dtd_funcptr_t*
      To bypass const in function structure.
      Getting address of the const members in local mutable pointers.
      */
-    char **name_not_const = (char **)&(tc->name);
+    const char **name_not_const = (const char **)&(tc->name);
     symbol_t **params     = (symbol_t **) &tc->params;
     symbol_t **locals     = (symbol_t **) &tc->locals;
     expr_t **priority     = (expr_t **)&tc->priority;
@@ -2301,7 +2296,7 @@ parsec_dtd_create_task_class( parsec_dtd_taskpool_t *__tp, parsec_dtd_funcptr_t*
     *priority                 = NULL;
     tc->flags                 = 0x0 | PARSEC_HAS_IN_IN_DEPENDENCIES | PARSEC_USE_DEPS_MASK;
     tc->dependencies_goal     = 0;
-    tc->key                   = (parsec_functionkey_fn_t *)DTD_identity_hash;
+    tc->key                   = (parsec_functionkey_fn_t *)DTD_taskclass_task_key;
     tc->fini                  = NULL;
     *incarnations             = (__parsec_chore_t *)dtd_chore;
     tc->find_deps             = NULL;
@@ -2747,9 +2742,9 @@ parsec_insert_dtd_task( parsec_dtd_task_t *this_task )
             /* parentless */
             /* Create Fake output_task */
             parsec_dtd_taskpool_insert_task( this_task->super.taskpool,
-                                &fake_first_out_body,   0, "Fake_FIRST_OUT",
-                                PASSED_BY_REF,         tile,       INOUT | AFFINITY,
-                                0 );
+                                             &fake_first_out_body, 0, "Fake_FIRST_OUT",
+                                             PASSED_BY_REF, tile, INOUT | AFFINITY,
+                                             0 );
 
             parsec_dtd_last_user_lock( &(tile->last_user) );
 
@@ -3042,8 +3037,8 @@ parsec_insert_dtd_task( parsec_dtd_task_t *this_task )
  */
 void
 parsec_dtd_taskpool_insert_task( parsec_taskpool_t  *tp,
-                    parsec_dtd_funcptr_t *fpointer, int priority,
-                    char *name_of_kernel, ... )
+                                 parsec_dtd_funcptr_t *fpointer, int priority,
+                                 const char *name_of_kernel, ... )
 {
     parsec_dtd_taskpool_t *dtd_tp = (parsec_dtd_taskpool_t *)tp;
 
@@ -3066,7 +3061,8 @@ parsec_dtd_taskpool_insert_task( parsec_taskpool_t  *tp,
     va_start(args, name_of_kernel);
 
 #if defined(PARSEC_PROF_TRACE)
-    parsec_profiling_trace(dtd_tp->super.context->virtual_processes[0]->execution_streams[0]->es_profile, insert_task_trace_keyin, 0, dtd_tp->super.taskpool_id, NULL );
+    parsec_profiling_trace(dtd_tp->super.context->virtual_processes[0]->execution_streams[0]->es_profile,
+                           insert_task_trace_keyin, 0, dtd_tp->super.taskpool_id, NULL );
 #endif
 
     /* extracting the rank of the task */
@@ -3115,8 +3111,8 @@ parsec_dtd_taskpool_insert_task( parsec_taskpool_t  *tp,
     uint64_t fkey = (uint64_t)(uintptr_t)fpointer + flow_count_of_template;
     /* Creating master function structures */
     /* Hash table lookup to check if the function structure exists or not */
-    parsec_task_class_t *tc = (parsec_task_class_t *)parsec_dtd_find_task_class
-                                                       (dtd_tp, fkey);
+    parsec_task_class_t *tc = (parsec_task_class_t *)
+                                  parsec_dtd_find_task_class(dtd_tp, fkey);
 
     if( NULL == tc ) {
         /* calculating the size of parameters for each task class*/

parsec/interfaces/superscalar/insert_function.h
@@ -236,8 +236,8 @@ parsec_dtd_tile_of( parsec_data_collection_t *dc, parsec_data_key_t key );
  */
 void
 parsec_dtd_taskpool_insert_task( parsec_taskpool_t  *tp,
-                    parsec_dtd_funcptr_t *fpointer, int priority,
-                    char *name_of_kernel, ... );
+                                 parsec_dtd_funcptr_t *fpointer, int priority,
+                                 const char *name_of_kernel, ... );
 
 /*
  * This macros should be called anytime users

parsec/interfaces/superscalar/insert_function_internal.h
@@ -186,23 +186,23 @@ typedef struct parsec_dtd_tile_user_s {
 }parsec_dtd_tile_user_t;
 
 struct parsec_dtd_tile_s {
-    parsec_list_item_t       super;
-    parsec_hash_table_item_t ht_item;
-    parsec_thread_mempool_t *mempool_owner;
-    int16_t                  arena_index;
-    int16_t                  flushed;
-    int32_t                  rank;
-    uint64_t                 key;
-    parsec_data_copy_t      *data_copy;
-    parsec_data_collection_t          *dc;
-    parsec_dtd_tile_user_t   last_user;
-    parsec_dtd_tile_user_t   last_writer;
+    parsec_list_item_t        super;
+    parsec_hash_table_item_t  ht_item;
+    parsec_thread_mempool_t  *mempool_owner;
+    int16_t                   arena_index;
+    int16_t                   flushed;
+    int32_t                   rank;
+    uint64_t                  key;
+    parsec_data_copy_t       *data_copy;
+    parsec_data_collection_t *dc;
+    parsec_dtd_tile_user_t    last_user;
+    parsec_dtd_tile_user_t    last_writer;
 };
 /* For creating objects of class parsec_dtd_tile_t */
 PARSEC_DECLSPEC OBJ_CLASS_DECLARATION(parsec_dtd_tile_t);
 
 /* for testing abstraction for PaRsec */
-struct hook_info{
+struct hook_info {
     parsec_hook_t *hook;
 };
 
@@ -333,7 +333,7 @@ parsec_dtd_find_task_class( parsec_dtd_taskpool_t  *tp,
 
 parsec_task_class_t*
 parsec_dtd_create_task_class( parsec_dtd_taskpool_t *__tp, parsec_dtd_funcptr_t* fpointer,
-                              char* name, int count_of_params, long unsigned int size_of_param,
+                              const char* name, int count_of_params, long unsigned int size_of_param,
                               int flow_count );
 
 void

parsec/mca/pins/ptg_to_dtd/pins_ptg_to_dtd_module.c
@@ -72,7 +72,7 @@ static void
 copy_chores(parsec_taskpool_t *tp, parsec_dtd_taskpool_t *dtd_tp)
 {
     int i, j;
-    for( i = 0; i < (int)tp->nb_task_classes; i++) {
+    for( i = 0; NULL != tp->task_classes_array[i]; i++) {
         for( j = 0; NULL != tp->task_classes_array[i]->incarnations[j].hook; j++) {
             parsec_hook_t **hook_not_const = (parsec_hook_t **)&(tp->task_classes_array[i]->incarnations[j].hook);
 

parsec/parsec.c
@@ -2343,7 +2343,7 @@ void parsec_debug_taskpool_local_tasks( parsec_taskpool_t *tp,
     if( tp->dependencies_array == NULL )
         return;
 
-    for(fi = 0; fi < tp->nb_task_classes; fi++) {
+    for(fi = 0; NULL != tp->task_classes_array[fi]; fi++) {
         parsec_debug_verbose(0, parsec_debug_output, " Tasks of Class %u (%s):\n", fi, tp->task_classes_array[fi]->name);
         parsec_debug_taskpool_count_local_tasks( tp, tp->task_classes_array[fi],
                                                  show_remote, show_startup, show_complete,

commit e7effa9eb58e770e2689a51b5d2142a2edfcff66

Typo introduced in d699b38

parsec/mca/pins/task_profiler/pins_task_profiler_module.c
@@ -153,7 +153,7 @@ task_profiler_release_deps_begin(struct parsec_execution_stream_s*   es,
 
     PARSEC_PROFILING_TRACE(es->es_profile,
                            release_deps_trace_keyin,
-                           (*task->task_class->key)(task->taskpool, task->locals)
+                           (*task->task_class->key)(task->taskpool, task->locals),
                            task->taskpool->taskpool_id,
                            (void *)&rd_fid);
 