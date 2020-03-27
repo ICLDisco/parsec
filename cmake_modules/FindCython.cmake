668e35d517c2c0993b95b305a6a3af6ec95e4816
Aurelien Bouteiller <bouteill@icl.utk.edu>
2020-03-27 03:11:54 -0400

    we missed some PYTHON->Python and that causes finding the wrong cython/python version with virtualenvs
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- cmake_modules/FindCython.cmake
+++ cmake_modules/FindCython.cmake
@@ -25,8 +25,8 @@
 # Use the Cython executable that lives next to the Python executable
 # if it is a local installation.
 find_package (Python COMPONENTS Interpreter Development)
-if( PYTHONINTERP_FOUND )
-  get_filename_component( _python_path ${PYTHON_EXECUTABLE} PATH )
+if( Python_FOUND )
+  get_filename_component( _python_path ${Python_EXECUTABLE} PATH )
   find_program( CYTHON_EXECUTABLE
     NAMES cython cython.bat
     HINTS ${_python_path}
