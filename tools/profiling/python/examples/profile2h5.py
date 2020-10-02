62fe1436ef6b02eb650b8d13a3aabb772f86cc5a
Thomas Herault <herault@icl.utk.edu>
2020-10-02 19:00:55 -0400

    Create a profiling test for the ASYNC and AGAIN return codes in PTG.
    Remove abusive shortcut that would disregard events with ID == 0 ending on a thread different from the one they started on
    
    Add command-line parsing for async.jdf test
    
    Start automatizing the tests.
    
    Adapt test to new interface for two_dim_block_cyclic_init
    
    Add arguments to profile2h5:
      - allow to enable/disable multiprocessing and process tracking
      - allow to specify output filename
    
    When the output filename is specified, the script does not try to group input files by execution, allowing to bypass the checks that those belong to the same run.
    
    Working tests for the profiling (for async and again specifically).

--- tools/profiling/python/examples/profile2h5.py
+++ tools/profiling/python/examples/profile2h5.py
@@ -3,15 +3,31 @@ from __future__ import print_function
 import sys
 import ptt_utils
 import pbt2ptt
+import argparse
 
 if __name__ == '__main__':
-    if len(sys.argv[1:]) == 0:
-        print("Usage: %s profile-file1 profile-file2 ..." % (sys.argv[0]), file=sys.stderr)
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Convert a set of PaRSEC Binary Profile files into an HDF5 file")
+    parser.add_argument('--output', dest='output', help='Output file name')
+    parser.add_argument('--report-progress', dest='report_progress', action='store_const',
+                        const=True, default=False,
+                        help='Report progress of conversion to stderr')
+    parser.add_argument('--single-process', dest='multiprocess', action='store_const',
+                        const=False, default=True,
+                        help='Deactivate multiprocess parallelism')
+    parser.add_argument('inputs', metavar='INPUT', type=str, nargs='+',
+                        help='PaRSEC Binary Profile Input files')
+    args = parser.parse_args()
 
-    groups = ptt_utils.group_trace_filenames(sys.argv[1:])
-    for f in groups:
-        print("Processing %s" % f)
-        name = pbt2ptt.convert(f, multiprocess=True, report_progress=False)
-        print("Generated: %s" % (name))
+    if args.output is None:
+        groups = ptt_utils.group_trace_filenames(args.inputs)
+        for f in groups:
+            print("Processing {}".format(f))
+            name = pbt2ptt.convert(f, multiprocess=args.multiprocess, report_progress=args.report_progress)
+            print("Generated: {}".format(name))
+    else:
+        f = args.inputs
+        print("Processing {}".format(f))
+        name = pbt2ptt.convert(f, multiprocess=args.multiprocess, report_progress=args.report_progress,
+                               out=args.output)
+        print("Generated {}".format(name))
     sys.exit(0)
