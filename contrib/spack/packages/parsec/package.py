49db3a06ae69bd95661be143c36750ef47d3f06d
Aurelien Bouteiller <bouteill@icl.utk.edu>
2020-12-04 16:05:34 -0500

    spack: comments from Spack reviewers
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- contrib/spack/packages/parsec/package.py
+++ contrib/spack/packages/parsec/package.py
@@ -3,15 +3,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 #
-# Copyright (c) 2017-2020  The University of Tennessee and the University
-#                          of Tennessee Research Foundation.  All rights
-#                          reserved.
-#
-
 from spack import *
 
 
-class Parsec(CMakePackage):
+class Parsec(CMakePackage, CudaPackage):
     """PaRSEC: the Parallel Runtime Scheduler and Execution Controller
 
     PaRSEC is a runtime and a programming toolbox that support the design and
@@ -28,35 +23,30 @@ class Parsec(CMakePackage):
     version('3.0.2012-rc1', sha256='a0f013bd5a2c44c61d3d76bab102e3ca3bab68ef2e89d7b5f544b9c1a6fde475')
     version('1.1.0', sha256='d2928033c121000ae0a554f1e7f757c1f22274a8b74457ecd52744ae1f70b95a', url='https://bitbucket.org/icldistcomp/parsec/get/v1.1.0.tar.bz2')
 
-    variant('build_type', default='RelWithDebInfo', description='CMake build type', values=('Debug', 'Release', ' RelWithDebInfo'))
+    variant('build_type', default='RelWithDebInfo', description='CMake build type', values=('Debug', 'Release', 'RelWithDebInfo'))
     variant('shared', default=True, description='Build a shared library')
-    # We always need MPI for now.
-    # variant('mpi', default=True,
-    #         description='Use MPI for dependency transport between nodes')
-    variant('cuda', default=True, description='Use CUDA for GPU acceleration')
+    variant('cuda', default=True, description='Build with CUDA')
     variant('profile', default=False, description='Generate profiling data')
     variant('debug_verbose', default=False, description='Debug version with verbose and paranoid (incurs performance overhead!)')
     conflicts('+debug_verbose build_type=Release', msg='You need to set build_type=Debug for +debug_verbose')
     conflicts('+debug_verbose build_type=RelWithDebInfo', msg='You need to set build_type=Debug for +debug_verbose')
-    # SPack does not handle cross-compilation atm
+    # TODO: Spack does not handle cross-compilation atm
     # variant('xcompile', default=False, description='Cross compile')
 
     depends_on('cmake@3.16.0:', type='build')
     depends_on('python', type='build')
     depends_on('hwloc')
-    depends_on('mpi')  # depends_on('mpi', when='+mpi')
-    depends_on('cuda', when='+cuda')
+    depends_on('mpi')
     depends_on('papi', when='+profile')
-    depends_on('python', when='+profile')
-    depends_on('py-cython', when='+profile')
-    depends_on('py-pandas', when='+profile')
-    depends_on('py-matplotlib', when='+profile')
-    depends_on('py-tables', when='+profile')
+    depends_on('python', type=('build', 'run'), when='+profile')
+    depends_on('py-cython', type=('build', 'run'), when='+profile')
+    depends_on('py-pandas', type=('build', 'run'), when='+profile')
+    depends_on('py-matplotlib', type=('build', 'run'),  when='+profile')
+    depends_on('py-tables', type=('build', 'run'), when='+profile')
 
     def cmake_args(self):
         args = [
             self.define_from_variant('BUILD_SHARED_LIBS', 'shared'),
-            # self.define_from_variant('PARSEC_DIST_WITH_MPI', 'mpi'),
             self.define_from_variant('PARSEC_GPU_WITH_CUDA', 'cuda'),
             self.define_from_variant('PARSEC_PROF_TRACE', 'profile'),
             self.define_from_variant('PARSEC_DEBUG_HISTORY', 'debug_verbose'),
