ce0d4a7468d8e87b8742a1adbb99b488dc95285e
Thomas Herault <herault@icl.utk.edu>
2021-02-05 19:20:28 -0500

    There was two problems with the Testings of profiling   1- if we used Python-2.x, the profile2h5.py file was not in the build/      directory, it has been added to the files to copy there, and moved      from the examples/ subdir to the main python tools subdir.
      2- if the profiling is enabled, and python is detected, but cython
         is not, the python scripts (and trace tests) cannot work. This
         version of Testings.cmake takes that into account and define the
         tests only if everything is present. To do so, it creates a new
         cache variable, PARSEC_PYTHON_TOOLS that is ON iff the python tools
         are available.
    
    This also reduces the length of the checks during
    tests/interfaces/ptg/profiling

--- tests/profiling/Testings.cmake
+++ tests/profiling/Testings.cmake
@@ -1,22 +1,21 @@
 find_package (Python COMPONENTS Interpreter)
+if(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUbND)
+  execute_process(COMMAND ${Python_EXECUTABLE} -c
+    "from __future__ import print_function; import sysconfig; import sys; print('{}-{}.{}'.format(sysconfig.get_platform(),sys.version_info[0],sys.version_info[1]), end='')"
+    OUTPUT_VARIABLE SYSCONF)
+  parsec_addtest_cmd(generate_bw_profile ${MPI_TEST_CMD_LIST} 2 ../pingpong/bw_test -n 10 -f 10 -l 2097152 -- --mca profile_filename bw  --mca mca_pins task_profiler)
 
-if(PARSEC_PROF_TRACE AND MPI_C_FOUND AND Python_FOUND)
-    execute_process(COMMAND ${Python_EXECUTABLE} -c
-            "import sysconfig; import sys; print('{}-{}.{}'.format(sysconfig.get_platform(),sys.version_info[0],sys.version_info[1]), end='')"
-            OUTPUT_VARIABLE SYSCONF)
-    parsec_addtest_cmd(generate_bw_profile ${MPI_TEST_CMD_LIST} 2 ../pingpong/bw_test -n 10 -f 10 -l 2097152 -- --mca profile_filename bw  --mca mca_pins task_profiler)
-
-    parsec_addtest_cmd(generate_hdf5_bw_file ${SHM_TEST_CMD_LIST}
-            ${Python_EXECUTABLE}
-            ${CMAKE_BINARY_DIR}/tools/profiling/python/examples/profile2h5.py --output=bw.h5 bw-0.prof bw-1.prof)
-    set_property(TEST generate_hdf5_bw_file APPEND PROPERTY DEPENDS generate_bw_profile)
-    set_property(TEST generate_hdf5_bw_file APPEND PROPERTY ENVIRONMENT
-            LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/temp.${SYSCONF}:$ENV{LD_LIBRARY_PATH})
-    set_property(TEST generate_hdf5_bw_file APPEND PROPERTY ENVIRONMENT
-            PYTHONPATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/lib.${SYSCONF}/:$ENV{PYTHONPATH})
-
-    parsec_addtest_cmd(check_hdf5_bw_file ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/check_comms.py)
-    set_property(TEST check_hdf5_bw_file APPEND PROPERTY DEPENDS generate_hdf5_bw_file)
-
-    parsec_addtest_cmd(cleanup_profile_files ${SHM_TEST_CMD_LIST} rm -f bw-0.prof bw-1.prof bw.h5)
-endif(PARSEC_PROF_TRACE AND MPI_C_FOUND AND Python_FOUND)
+  parsec_addtest_cmd(generate_hdf5_bw_file ${SHM_TEST_CMD_LIST}
+    ${Python_EXECUTABLE}
+    ${PROJECT_BINARY_DIR}/tools/profiling/python/profile2h5.py --output=bw.h5 bw-0.prof bw-1.prof)
+  set_property(TEST generate_hdf5_bw_file APPEND PROPERTY DEPENDS generate_bw_profile)
+  set_property(TEST generate_hdf5_bw_file APPEND PROPERTY ENVIRONMENT
+    LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/temp.${SYSCONF}:$ENV{LD_LIBRARY_PATH})
+  set_property(TEST generate_hdf5_bw_file APPEND PROPERTY ENVIRONMENT
+    PYTHONPATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/lib.${SYSCONF}/:$ENV{PYTHONPATH})
+  
+  parsec_addtest_cmd(check_hdf5_bw_file ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/check_comms.py)
+  set_property(TEST check_hdf5_bw_file APPEND PROPERTY DEPENDS generate_hdf5_bw_file)
+  
+  parsec_addtest_cmd(cleanup_profile_files ${SHM_TEST_CMD_LIST} rm -f bw-0.prof bw-1.prof bw.h5)
+endif(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUND)
