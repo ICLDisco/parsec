f5fb1552c73302b51a38e6ab44aaf6c5e55832b4
Aurelien Bouteiller <bouteill@icl.utk.edu>
2023-01-18 13:46:48 -0500

    Make the 'parsec/' test label prefix conditional on being a submodule
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- tests/dsl/dtd/Testings.cmake
+++ tests/dsl/dtd/Testings.cmake
@@ -2,28 +2,28 @@
 #
 # Shared Memory Testings
 #
-parsec_addtest_cmd(parsec/dsl/dtd/task_generation ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_task_generation)
-parsec_addtest_cmd(parsec/dsl/dtd/task_inserting_task ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_task_inserting_task)
-parsec_addtest_cmd(parsec/dsl/dtd/task_insertion ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_task_insertion)
-parsec_addtest_cmd(parsec/dsl/dtd/war ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_war)
-parsec_addtest_cmd(parsec/dsl/dtd/new_tile:cpu ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 0)
+parsec_addtest_cmd(${parsec/}dsl/dtd/task_generation ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_task_generation)
+parsec_addtest_cmd(${parsec/}dsl/dtd/task_inserting_task ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_task_inserting_task)
+parsec_addtest_cmd(${parsec/}dsl/dtd/task_insertion ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_task_insertion)
+parsec_addtest_cmd(${parsec/}dsl/dtd/war ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_war)
+parsec_addtest_cmd(${parsec/}dsl/dtd/new_tile:cpu ${SHM_TEST_CMD_LIST} dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 0)
 if(PARSEC_HAVE_CUDA AND CMAKE_CUDA_COMPILER)
 # How do we run CUDA tests? Is there a SHM_TEST_CMD_LIST_CUDA?
-  parsec_addtest_cmd(parsec/dsl/dtd/new_tile:gpu ${SHM_TEST_CMD_LIST} ${CTEST_CUDA_LAUNCHER_OPTIONS} dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 1 --mca device cuda)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/new_tile:gpu ${SHM_TEST_CMD_LIST} ${CTEST_CUDA_LAUNCHER_OPTIONS} dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 1 --mca device cuda)
 endif(PARSEC_HAVE_CUDA AND CMAKE_CUDA_COMPILER)
 
 #
 # Distributed Memory Testings
 #
 if( MPI_C_FOUND )
-  parsec_addtest_cmd(parsec/dsl/dtd/pingpong:mp ${MPI_TEST_CMD_LIST} 2 dsl/dtd/dtd_test_pingpong)
-  parsec_addtest_cmd(parsec/dsl/dtd/task_inserting_task:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_task_inserting_task)
-  parsec_addtest_cmd(parsec/dsl/dtd/task_insertion:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_task_insertion)
-  parsec_addtest_cmd(parsec/dsl/dtd/war:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_war)
-  parsec_addtest_cmd(parsec/dsl/dtd/interleave_actions:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_interleave_actions)
-  parsec_addtest_cmd(parsec/dsl/dtd/allreduce:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_allreduce)
-  parsec_addtest_cmd(parsec/dsl/dtd/new_tile:mp:cpu ${MPI_TEST_CMD_LIST} 2 dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 0)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/pingpong:mp ${MPI_TEST_CMD_LIST} 2 dsl/dtd/dtd_test_pingpong)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/task_inserting_task:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_task_inserting_task)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/task_insertion:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_task_insertion)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/war:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_war)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/interleave_actions:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_interleave_actions)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/allreduce:mp ${MPI_TEST_CMD_LIST} 4 dsl/dtd/dtd_test_allreduce)
+  parsec_addtest_cmd(${parsec/}dsl/dtd/new_tile:mp:cpu ${MPI_TEST_CMD_LIST} 2 dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 0)
   if(PARSEC_HAVE_CUDA AND CMAKE_CUDA_COMPILER)
-    parsec_addtest_cmd(parsec/dsl/dtd/new_tile:mp:gpu ${MPI_TEST_CMD_LIST} 2 ${CTEST_CUDA_LAUNCHER_OPTIONS} dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 1 --mca device cuda)
+    parsec_addtest_cmd(${parsec/}dsl/dtd/new_tile:mp:gpu ${MPI_TEST_CMD_LIST} 2 ${CTEST_CUDA_LAUNCHER_OPTIONS} dsl/dtd/dtd_test_new_tile --mca device_cuda_enabled 1 --mca device cuda)
   endif(PARSEC_HAVE_CUDA AND CMAKE_CUDA_COMPILER)
 endif( MPI_C_FOUND )
--- tests/profiling/Testings.cmake
+++ tests/profiling/Testings.cmake
@@ -3,9 +3,9 @@ if(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUND)
   execute_process(COMMAND ${Python_EXECUTABLE} -c
     "from __future__ import print_function; import sysconfig; import sys; print('{}-{}.{}'.format(sysconfig.get_platform(),sys.version_info[0],sys.version_info[1]), end='')"
     OUTPUT_VARIABLE SYSCONF)
-  parsec_addtest_cmd(parsec/profiling/generate_profile_bw:mp ${MPI_TEST_CMD_LIST} 2 apps/pingpong/bw_test -n 10 -f 10 -l 2097152 -- --mca profile_filename bw  --mca mca_pins task_profiler)
+  parsec_addtest_cmd(${parsec/}profiling/generate_profile_bw:mp ${MPI_TEST_CMD_LIST} 2 apps/pingpong/bw_test -n 10 -f 10 -l 2097152 -- --mca profile_filename bw  --mca mca_pins task_profiler)
 
-  parsec_addtest_cmd(parsec/profiling/generate_hdf5 ${SHM_TEST_CMD_LIST}
+  parsec_addtest_cmd(${parsec/}profiling/generate_hdf5 ${SHM_TEST_CMD_LIST}
     ${Python_EXECUTABLE}
     ${PROJECT_BINARY_DIR}/tools/profiling/python/profile2h5.py --output=bw.h5 bw-0.prof bw-1.prof)
   set_property(TEST parsec/profiling/generate_hdf5 APPEND PROPERTY DEPENDS profiling/generate_profile_bw:mp)
@@ -14,12 +14,12 @@ if(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUND)
   set_property(TEST parsec/profiling/generate_hdf5 APPEND PROPERTY ENVIRONMENT
     PYTHONPATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/lib.${SYSCONF}/:$ENV{PYTHONPATH})
 
-  parsec_addtest_cmd(parsec/profiling/check_hdf5 ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/profiling/check-comms.py)
+  parsec_addtest_cmd(${parsec/}profiling/check_hdf5 ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/profiling/check-comms.py)
   set_property(TEST parsec/profiling/check_hdf5 APPEND PROPERTY DEPENDS profiling/generate_hdf5)
 
-  parsec_addtest_cmd(parsec/profiling/generate_profile_async ${SHM_TEST_CMD_LIST} profiling/async 100 -- --mca profile_filename async  --mca mca_pins task_profiler)
+  parsec_addtest_cmd(${parsec/}profiling/generate_profile_async ${SHM_TEST_CMD_LIST} profiling/async 100 -- --mca profile_filename async  --mca mca_pins task_profiler)
 
-  parsec_addtest_cmd(parsec/profiling/generate_hdf5_async ${SHM_TEST_CMD_LIST}
+  parsec_addtest_cmd(${parsec/}profiling/generate_hdf5_async ${SHM_TEST_CMD_LIST}
                      ${Python_EXECUTABLE}
                      ${PROJECT_BINARY_DIR}/tools/profiling/python/profile2h5.py --output=async.h5 async-0.prof)
   set_property(TEST parsec/profiling/generate_hdf5_async APPEND PROPERTY DEPENDS profiling/generate_profile_async)
@@ -28,15 +28,15 @@ if(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUND)
   set_property(TEST parsec/profiling/generate_hdf5_async APPEND PROPERTY ENVIRONMENT
                PYTHONPATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/lib.${SYSCONF}/:$ENV{PYTHONPATH})
 
-  parsec_addtest_cmd(parsec/profiling/check_hdf5_async ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/profiling/check-async.py async.h5)
+  parsec_addtest_cmd(${parsec/}profiling/check_hdf5_async ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/profiling/check-async.py async.h5)
     set_property(TEST parsec/profiling/check_hdf5_async APPEND PROPERTY DEPENDS profiling/generate_hdf5_async)
 
-  parsec_addtest_cmd(parsec/profiling/cleanup_profile_files ${SHM_TEST_CMD_LIST} rm -f bw-0.prof bw-1.prof bw.h5 async-0.prof async.h5)
+  parsec_addtest_cmd(${parsec/}profiling/cleanup_profile_files ${SHM_TEST_CMD_LIST} rm -f bw-0.prof bw-1.prof bw.h5 async-0.prof async.h5)
 
   if(PARSEC_PROF_GRAPHER)
-    parsec_addtest_cmd(parsec/profiling/generate_profile_and_dot_bw:mp ${MPI_TEST_CMD_LIST} 2 apps/pingpong/bw_test -n 3 -f 2 -l 2097152 -- --mca profile_filename bw  --mca mca_pins task_profiler --dot bw)
+    parsec_addtest_cmd(${parsec/}profiling/generate_profile_and_dot_bw:mp ${MPI_TEST_CMD_LIST} 2 apps/pingpong/bw_test -n 3 -f 2 -l 2097152 -- --mca profile_filename bw  --mca mca_pins task_profiler --dot bw)
 
-    parsec_addtest_cmd(parsec/profiling/generate_hdf5_for_dag_and_dot ${SHM_TEST_CMD_LIST}
+    parsec_addtest_cmd(${parsec/}profiling/generate_hdf5_for_dag_and_dot ${SHM_TEST_CMD_LIST}
             ${Python_EXECUTABLE}
             ${PROJECT_BINARY_DIR}/tools/profiling/python/profile2h5.py --output=bw.h5 bw-0.prof bw-1.prof)
     set_property(TEST parsec/profiling/generate_hdf5_for_dag_and_dot APPEND PROPERTY DEPENDS profiling/generate_profile_and_dot_bw:mp)
@@ -45,9 +45,9 @@ if(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUND)
     set_property(TEST parsec/profiling/generate_hdf5_for_dag_and_dot APPEND PROPERTY ENVIRONMENT
             PYTHONPATH=${CMAKE_BINARY_DIR}/tools/profiling/python/build/lib.${SYSCONF}/:$ENV{PYTHONPATH})
 
-    parsec_addtest_cmd(parsec/profiling/check_DAG_and_Trace ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${PROJECT_SOURCE_DIR}/tools/profiling/python/examples/example-DAG-and-Trace.py --dot bw-0.dot --dot bw-1.dot --h5 bw.h5)
+    parsec_addtest_cmd(${parsec/}profiling/check_DAG_and_Trace ${SHM_TEST_CMD_LIST} ${Python_EXECUTABLE} ${PROJECT_SOURCE_DIR}/tools/profiling/python/examples/example-DAG-and-Trace.py --dot bw-0.dot --dot bw-1.dot --h5 bw.h5)
     set_property(TEST parsec/profiling/check_DAG_and_Trace APPEND PROPERTY DEPENDS profiling/generate_hdf5_for_dag_and_dot)
 
-    parsec_addtest_cmd(parsec/profiling/cleanup_dot_files ${SHM_TEST_CMD_LIST} rm -f bw-0.prof bw-1.prof bw.h5 bw-0.dot bw-1.dot)
+    parsec_addtest_cmd(${parsec/}profiling/cleanup_dot_files ${SHM_TEST_CMD_LIST} rm -f bw-0.prof bw-1.prof bw.h5 bw-0.dot bw-1.dot)
   endif(PARSEC_PROF_GRAPHER)
 endif(Python_FOUND AND PARSEC_PYTHON_TOOLS AND PARSEC_PROF_TRACE AND MPI_C_FOUND)
