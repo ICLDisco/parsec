ICLDisco/parsec

commit 07f1e20d0659be272cb9197851458124352e3cc8

Remove parsec_profiling_ts_trace_flags that was used only in a couple of places

parsec/profiling.h
@@ -390,7 +390,7 @@ int parsec_profiling_trace_flags_info_fn(parsec_profiling_stream_t* context, int
  * @return 0 if success, negative otherwise.
  * @remark thread safe
  */
-int parsec_profiling_ts_trace_flags(int key, uint64_t event_id, uint32_t taskpool_id,
+int a(int key, uint64_t event_id, uint32_t taskpool_id,
                                     const void *info, uint16_t flags );
 
 /**
@@ -415,14 +415,6 @@ int parsec_profiling_ts_trace_flags(int key, uint64_t event_id, uint32_t taskpoo
  */
 int parsec_profiling_ts_trace_flags_info_fn(int key, uint64_t event_id, uint32_t taskpool_id,
                                             parsec_profiling_info_fn_t *info_fn, const void *info_data, uint16_t flags );
-
-/**
- * @brief Convenience macro when no flag needs to be passed
- */
-#define parsec_profiling_ts_trace(key, event_id, object_id, info) \
-    parsec_profiling_ts_trace_flags((key), (event_id), (object_id), (info), 0)
-#define parsec_profiling_ts_trace_info_fn(key, event_id, object_id, info_fn, info_fn_data) \
-    parsec_profiling_ts_trace_flags_info_fn((key), (event_id), (object_id), (info_fn), (info_fn_data), 0)
 
 /**
  * @brief Creates the profile file given as a parameter to store the

parsec/arena.c
@@ -24,13 +24,13 @@
 extern int arena_memory_alloc_key, arena_memory_free_key;
 extern int arena_memory_used_key, arena_memory_unused_key;
 #define TRACE_MALLOC(key, size, ptr) do {                               \
-        parsec_profiling_ts_trace_flags(key, (uint64_t)ptr, PROFILE_OBJECT_ID_NULL, \
-                                        &size, PARSEC_PROFILING_EVENT_COUNTER|PARSEC_PROFILING_EVENT_HAS_INFO); \
+        parsec_profiling_ts_trace_flags_info_fn(key, (uint64_t)ptr, PROFILE_OBJECT_ID_NULL, \
+                                                memcpy, &size, PARSEC_PROFILING_EVENT_COUNTER|PARSEC_PROFILING_EVENT_HAS_INFO); \
         PARSEC_PAPI_SDE_COUNTER_ADD(PARSEC_PAPI_SDE_MEM_ALLOC, size);   \
     } while(0)
 #define TRACE_FREE(key, size, ptr) do {                                 \
-        parsec_profiling_ts_trace_flags(key, (uint64_t)ptr, PROFILE_OBJECT_ID_NULL, \
-                                        NULL, PARSEC_PROFILING_EVENT_COUNTER); \
+        parsec_profiling_ts_trace_flags_info_fn(key, (uint64_t)ptr, PROFILE_OBJECT_ID_NULL, \
+                                                NULL, NULL, PARSEC_PROFILING_EVENT_COUNTER); \
         PARSEC_PAPI_SDE_COUNTER_ADD(PARSEC_PAPI_SDE_MEM_ALLOC, -size);  \
     } while(0)
 

parsec/interfaces/dtd/insert_function.c
@@ -2621,7 +2621,7 @@ parsec_dtd_set_descendant(parsec_dtd_task_t *parent_task, uint8_t parent_flow_in
                 parsec_dtd_untrack_remote_dep(tp, key);
 #if defined(PARSEC_PROF_TRACE)
                 if( parsec_dtd_profile_verbose )
-                    parsec_profiling_ts_trace(hashtable_trace_keyin, 0, tp->super.taskpool_id, NULL);
+                    parsec_profiling_ts_trace_flags_info_fn(hashtable_trace_keyin, 0, tp->super.taskpool_id, NULL, NULL, 0);
 #endif
                 parsec_dtd_track_task(tp, key, real_parent_task);
                 remote_dep_dequeue_delayed_dep_release(dep);
@@ -3142,7 +3142,7 @@ parsec_insert_dtd_task(parsec_task_t *__this_task)
 
 #if defined(PARSEC_PROF_TRACE)
     if( parsec_dtd_profile_verbose )
-        parsec_profiling_ts_trace(insert_task_trace_keyout, 0, dtd_tp->super.taskpool_id, NULL);
+        parsec_profiling_ts_trace_flags_info_fn(insert_task_trace_keyout, 0, dtd_tp->super.taskpool_id, NULL, NULL, 0);
 #endif
 
     if( parsec_dtd_task_is_local(this_task)) {
@@ -3181,7 +3181,7 @@ __parsec_dtd_taskpool_create_task(parsec_taskpool_t *tp,
 
 #if defined(PARSEC_PROF_TRACE)
     if( parsec_dtd_profile_verbose )
-        parsec_profiling_ts_trace(insert_task_trace_keyin, 0, dtd_tp->super.taskpool_id, NULL);
+        parsec_profiling_ts_trace_flags_info_fn(insert_task_trace_keyin, 0, dtd_tp->super.taskpool_id, NULL, NULL, 0);
 #endif
 
     /* We parse arguments a first time (mostly skipping the tile),

parsec/profiling.c
@@ -956,12 +956,6 @@ static int switch_event_buffer( parsec_profiling_stream_t *context )
     return 0;
 }
 
-int parsec_profiling_ts_trace_flags(int key, uint64_t event_id, uint32_t taskpool_id,
-                                    const void *info, uint16_t flags )
-{
-    return parsec_profiling_ts_trace_flags_info_fn(key, event_id, taskpool_id, memcpy, info, flags);
-}
-
 int parsec_profiling_ts_trace_flags_info_fn(int key, uint64_t event_id, uint32_t taskpool_id,
                                             parsec_profiling_info_fn_t *info_fn, const void *info_data, uint16_t flags )
 {
@@ -975,7 +969,7 @@ int parsec_profiling_ts_trace_flags_info_fn(int key, uint64_t event_id, uint32_t
     if( NULL != ctx )
         return parsec_profiling_trace_flags_info_fn(ctx, key, event_id, taskpool_id, info_fn, info_data, flags);
 
-    set_last_error("Profiling system: error: called parsec_profiling_ts_trace_flags"
+    set_last_error("Profiling system: error: called parsec_profiling_ts_trace_flags_info_fn"
                    " from a thread that did not call parsec_profiling_stream_init\n");
     return PARSEC_ERR_NOT_SUPPORTED;
 }

parsec/profiling_otf2.c
@@ -689,12 +689,6 @@ int parsec_profiling_dictionary_flush( void )
     return 0;
 }
 
-int parsec_profiling_ts_trace_flags(int key, uint64_t event_id, uint32_t taskpool_id,
-                                    const void *info, uint16_t flags )
-{
-    return parsec_profiling_ts_trace_flags_info_fn(key, event_id, taskpool_id, memcpy, info, flags);
-}
-
 int parsec_profiling_ts_trace_flags_info_fn(int key, uint64_t event_id, uint32_t taskpool_id,
                                             parsec_profiling_info_fn_t *info_fn, const void *info_data, uint16_t flags )
 {
@@ -708,7 +702,7 @@ int parsec_profiling_ts_trace_flags_info_fn(int key, uint64_t event_id, uint32_t
     if( NULL != ctx )
         return parsec_profiling_trace_flags_info_fn(ctx, key, event_id, taskpool_id, info_fn, info_data, flags);
 
-    set_last_error("Profiling system: error: called parsec_profiling_ts_trace_flags"
+    set_last_error("Profiling system: error: called parsec_profiling_ts_trace_flags_info_fn"
                    " from a thread that did not call parsec_profiling_stream_init\n");
     return PARSEC_ERR_NOT_SUPPORTED;
 }

commit 9fc74b6f165605a133125d8a5b62cf55642c1907

Fix typo introduced in 07f1e20d06

parsec/profiling.h
@@ -371,30 +371,6 @@ int parsec_profiling_trace_flags_info_fn(parsec_profiling_stream_t* context, int
 
 /**
  * @brief Trace one event on the implicit thread context.
- * @details This uses a TLS variable to lookup the last context created on the calling
- * thread. If the calling thread did not create a a profiling context, this
- * will generate an error. This function is significantly more costly than
- * parsec_profiling_trace_flags, as it includes the cost of TLS lookup.
- *
- * @param[in] key     the key (as returned by add_dictionary_keyword) of the event to log
- * @param[in] event_id a (possibly unique) event identifier. Events are coupled together: start/end.
- *                      a couple (start, end) has
- *                        - the same key
- *                        - end is the next "end" event with the same key and the same non-null event_id and
- *                          non OBJECT_ID_NULL taskpool_id as start in the event buffer of the thread context
- *                        - if no matching end is found, this is an error
- * @param[in] taskpool_id unique object/handle identifier (use PROFILE_OBJECT_ID_NULL if N/A)
- * @param[in] info    a pointer to an area of size info_length for this key (see
- *                        parsec_profiling_add_dictionary_keyword)
- * @param[in] flags   flags related to the event
- * @return 0 if success, negative otherwise.
- * @remark thread safe
- */
-int a(int key, uint64_t event_id, uint32_t taskpool_id,
-                                    const void *info, uint16_t flags );
-
-/**
- * @brief Trace one event on the implicit thread context.
  *
  * @details Event is added to the series of events related to the context passed as argument.
  *