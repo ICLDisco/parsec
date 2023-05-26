7bf88baeac08d05068c01decd68a3da95079b8d0
Aurelien Bouteiller <bouteill@icl.utk.edu>
2023-05-26 17:10:09 -0400

    Move cuda tests to runtime category
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- tests/dsl/ptg/Testings.cmake
+++ tests/dsl/ptg/Testings.cmake
@@ -6,5 +6,3 @@ parsec_addtest_cmd(dsl/ptg/startup1 ${SHM_TEST_CMD_LIST} dsl/ptg/startup -i=10 -
 parsec_addtest_cmd(dsl/ptg/startup2 ${SHM_TEST_CMD_LIST} dsl/ptg/startup -i=10 -j=20 -k=30 -v=5)
 parsec_addtest_cmd(dsl/ptg/startup3 ${SHM_TEST_CMD_LIST} dsl/ptg/startup -i=30 -j=30 -k=30 -v=5)
 parsec_addtest_cmd(dsl/ptg/strange ${SHM_TEST_CMD_LIST} dsl/ptg/strange)
-
-include(${CMAKE_CURRENT_LIST_DIR}/cuda/Testings.cmake)
