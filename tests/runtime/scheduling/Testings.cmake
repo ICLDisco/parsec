eb1e4111caf17a2e986ca519fb7f49bae57d55e1
Aurelien Bouteiller <bouteill@icl.utk.edu>
2023-05-26 16:34:09 -0400

    Assign proper markers/labels to all ctests
    Signed-off-by: Aurelien Bouteiller <bouteill@icl.utk.edu>

--- tests/runtime/scheduling/Testings.cmake
+++ tests/runtime/scheduling/Testings.cmake
@@ -1,5 +1,5 @@
 foreach(_sched ${MCA_sched})
-    parsec_addtest_cmd(runtime/scheduling:sp:${_sched} ${MPI_TEST_CMD_LIST} 1 runtime/scheduling/schedmicro -t 10 -l 8 -n 512 -- --mca mca_sched ${_sched})
+    parsec_addtest_cmd(runtime/scheduling:${_sched} ${MPI_TEST_CMD_LIST} 1 runtime/scheduling/schedmicro -t 10 -l 8 -n 512 -- --mca mca_sched ${_sched})
 endforeach()
 
 if( MPI_C_FOUND )
