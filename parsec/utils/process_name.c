125b29eb235f1e372fcd1ad21e2a1c2747a02d92
George Bosilca <bosilca@icl.utk.edu>
2024-01-25 19:08:41 -0500

    Fix the argument _NSGetExecutablePath.
    It is not an size_t but an uint32_t.
    
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>

--- parsec/utils/process_name.c
+++ parsec/utils/process_name.c
@@ -11,15 +11,16 @@
 char *parsec_process_name(void) {
     char name[PATH_MAX];
     char *sname = NULL;
-    size_t len = PATH_MAX;
     int ret;
 #if defined(__APPLE__)
+    uint32_t len = PATH_MAX;
     ret = _NSGetExecutablePath(name, &len);
     if(0 != ret) {
         snprintf(name, len, "parsec-app");
         return strdup(name);
     }
 #else
+    size_t len = PATH_MAX;
     ret = readlink("/proc/self/exe", name, len);
     if(-1 == ret) {
         snprintf(name, len, "parsec-app" );
