bf14d46e0643dc308b59b32a9955e3e190be922c
G-Ragghianti <33492707+G-Ragghianti@users.noreply.github.com>
2025-03-24 09:38:04 -0400

    Refactoring CI (#746)
    * Refactoring CI

--- .github/CI/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-source .github/CI/env_setup.sh
-
-cmake $GITHUB_WORKSPACE $BUILD_CONFIG
-
-
--- .github/CI/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-source .github/CI/env_setup.sh
-
-cmake -E make_directory $BUILD_DIRECTORY
